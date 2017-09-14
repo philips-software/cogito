528e1ac7d7303d8d62c4da38ab981f6f4c29e2a9
Stefan van den Oord <stefan.van.den.oord@philips.com>
2017-09-14 16:44:59 +0200

    Created new single view application

---
+++ Cogito/AppDelegate.swift
@@ -0,0 +1,11 @@
+//  Copyright © 2017 Konkinklijke Philips Nederland N.V. All rights reserved.
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
---
+++ CogitoTests/CogitoTests.swift
@@ -0,0 +1,7 @@
+//  Copyright © 2017 Konkinklijke Philips Nederland N.V. All rights reserved.
+
+import XCTest
+@testable import Cogito
+
+class CogitoTests: XCTestCase {
+}
