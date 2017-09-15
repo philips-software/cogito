8dba4233ac83262fed38c3cc0949341c91127225
Stefan van den Oord <stefan.van.den.oord@philips.com>
2017-09-15 16:58:36 +0200

    Added MarqueeLabel pod

--- Pods/Target Support Files/Pods-Cogito/Pods-Cogito-frameworks.sh
+++ Pods/Target Support Files/Pods-Cogito/Pods-Cogito-frameworks.sh
@@ -102,9 +102,11 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/MarqueeLabel/MarqueeLabel.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ReSwift/ReSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/MarqueeLabel/MarqueeLabel.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ReSwift/ReSwift.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
