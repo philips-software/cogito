933e3781958e65e65e37860bcd554f3eddf8f342
Stefan van den Oord <stefan.van.den.oord@philips.com>
2017-09-15 14:43:34 +0200

    Added ReSwift

--- Pods/Target Support Files/Pods-Cogito/Pods-Cogito-frameworks.sh
+++ Pods/Target Support Files/Pods-Cogito/Pods-Cogito-frameworks.sh
@@ -100,6 +100,13 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/ReSwift/ReSwift.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/ReSwift/ReSwift.framework"
+fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
 fi
