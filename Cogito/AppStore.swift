e7da26fb17c1474a5eb5b96c577d22e4decd0407
Stefan van den Oord <stefan.van.den.oord@philips.com>
2017-09-18 16:38:01 +0200

    Configured store for thunk middleware

--- Cogito/AppStore.swift
+++ Cogito/AppStore.swift
@@ -1,8 +1,10 @@
 //  Copyright © 2017 Konkinklijke Philips Nederland N.V. All rights reserved.
 
 import ReSwift
+import ReSwiftThunk
 
 let appStore = Store<AppState>(
     reducer: appReducer,
-    state: nil
+    state: nil,
+    middleware: [ThunkMiddleware()]
 )
