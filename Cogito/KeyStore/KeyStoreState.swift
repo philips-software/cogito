c0eaa5b2ec85a7c0008d033c304b076222c03a8d
Stefan van den Oord <stefan.van.den.oord@philips.com>
2017-09-19 17:02:12 +0200

    Reducer for KeyStoreActions.Fulfilled

---
+++ Cogito/KeyStore/KeyStoreState.swift
@@ -0,0 +1,7 @@
+//  Copyright © 2017 Konkinklijke Philips Nederland N.V. All rights reserved.
+
+struct KeyStoreState {
+    var keyStore: KeyStore?
+}
+
+let initialKeyStoreState = KeyStoreState(keyStore: nil)
