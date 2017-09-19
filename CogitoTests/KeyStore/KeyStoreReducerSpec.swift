ef6a4bdff27b3caf6fed0f35f859255610a1cd58
Stefan van den Oord <stefan.van.den.oord@philips.com>
2017-09-19 17:02:12 +0200

    Reducer for KeyStoreActions.Fulfilled

--- CogitoTests/KeyStore/KeyStoreReducerSpec.swift
+++ CogitoTests/KeyStore/KeyStoreReducerSpec.swift
@@ -4,8 +4,13 @@ import Quick
 import Nimble
 @testable import Cogito
 
-class KeyStoreActionsSpec: QuickSpec {
+class KeyStoreReducerSpec: QuickSpec {
     override func spec() {
-
+        it("handles create key store fulfilled") {
+            let keyStore = KeyStoreMock(path: "", scryptN: 0, scryptP: 0)
+            let action = KeyStoreActions.Fulfilled(keyStore: keyStore)
+            let nextState = keyStoreReducer(action: action, state: nil)
+            expect(nextState.keyStore) === keyStore
+        }
     }
 }
