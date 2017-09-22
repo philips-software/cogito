33177657a9c511754e7b1f29ef7853c5f6cdb265
Spanbroek, Mark <mark@spanbroek.net>
2017-09-22 16:45:13 +0200

    Refactored test: split into two separate specs

--- CogitoTests/KeyStore/KeyStoreActionsSpec.swift
+++ CogitoTests/KeyStore/KeyStoreActionsSpec.swift
@@ -9,23 +9,30 @@ import Geth
 
 class KeyStoreActionsSpec: QuickSpec {
     override func spec() {
-        it("creates a key store") {
-            var dispatchedAction: Action?
-            let dispatch: DispatchFunction = { (action) in
-                dispatchedAction = action
+        context("when creating a key store") {
+            var fulfilled: KeyStoreActions.Fulfilled!
+
+            beforeEach {
+                var dispatchedAction: Action?
+                let dispatch: DispatchFunction = { (action) in
+                    dispatchedAction = action
+                }
+                let action = KeyStoreActions.create()
+                action.action(dispatch, { return nil })
+                fulfilled = dispatchedAction as? KeyStoreActions.Fulfilled
             }
-            let action = KeyStoreActions.create()
-            action.action(dispatch, { return nil })
-            guard let fulfilled = dispatchedAction as? KeyStoreActions.Fulfilled else {
-                fail("incorrect type of \(dispatchedAction!)")
-                return
+
+            it("stores it in the document directory") {
+                let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory,
+                                                                            .userDomainMask,
+                                                                            true)[0]
+                expect(fulfilled.keyStore.path) == documentDirectory + "/main.keystore"
             }
-            let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory,
-                                                                        .userDomainMask,
-                                                                        true)[0]
-            expect(fulfilled.keyStore.path) == documentDirectory + "/main.keystore"
-            expect(fulfilled.keyStore.scryptN) == GethStandardScryptN
-            expect(fulfilled.keyStore.scryptP) == GethStandardScryptP
+
+            it("uses the standard key derivation parameters") {
+                expect(fulfilled.keyStore.scryptN) == GethStandardScryptN
+                expect(fulfilled.keyStore.scryptP) == GethStandardScryptP
+            }
         }
     }
 }
