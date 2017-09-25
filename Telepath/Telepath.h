ccb31645e23fc66207418ad6fcd6dfd0d4c9d630
Spanbroek, Mark <mark@spanbroek.net>
2017-09-25 10:38:43 +0200

    Empty project

---
+++ Telepath/Telepath.h
@@ -0,0 +1,13 @@
+//  Copyright © 2017 Philips. All rights reserved.
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Telepath.
+FOUNDATION_EXPORT double TelepathVersionNumber;
+
+//! Project version string for Telepath.
+FOUNDATION_EXPORT const unsigned char TelepathVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Telepath/PublicHeader.h>
+
+
