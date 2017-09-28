f6121dba23e37c173fbe4678c800ebf3d3c2f928
Spanbroek, Mark <mark@spanbroek.net>
2017-09-28 12:15:27 +0200

    Add parsing of URL parameters

---
+++ Telepath/URLParameterParser.swift
@@ -0,0 +1,28 @@
+//Copyright © 2017 Philips. All rights reserved.
+
+import Foundation
+
+func parseUrlParameters(_ query: String) -> [String: String] {
+    var result: [String: String] = [:]
+    for keyValuePair in query.split(separator: "&") {
+        guard let (key, value) = parseKeyValuePair(keyValuePair) else {
+            continue
+        }
+        result[key] = value
+    }
+    return result
+}
+
+private func parseKeyValuePair(_ pair: Substring) -> (key: String, value: String)? {
+    let components = pair.split(separator: "=", maxSplits: 1)
+    guard components.count == 2 else {
+        return nil
+    }
+    guard let key = components.first?.removingPercentEncoding else  {
+        return nil
+    }
+    guard let value = components.last?.removingPercentEncoding else {
+        return nil
+    }
+    return (key, value)
+}
---
+++ TelepathTests/UrlParameterParserSpec.swift
@@ -0,0 +1,41 @@
+//Copyright © 2017 Philips. All rights reserved.
+
+import Quick
+import Nimble
+@testable import Telepath
+
+class UrlParameterParserSpec: QuickSpec {
+    override func spec() {
+        it("parses url parameters") {
+            expect(parseUrlParameters("A=1&B=2")) == [ "A": "1", "B": "2"]
+        }
+
+        it("ignores empty parameters") {
+            expect(parseUrlParameters("")) == [:]
+            expect(parseUrlParameters("A=1&")) == [ "A": "1" ]
+            expect(parseUrlParameters("&B=2")) == [ "B": "2" ]
+        }
+
+        it("ignores empty values") {
+            expect(parseUrlParameters("A=&B=2")) == [ "B": "2" ]
+        }
+
+        it("ignores empty keys") {
+            expect(parseUrlParameters("=")) == [:]
+            expect(parseUrlParameters("=1&B=2")) == [ "B" : "2" ]
+        }
+
+        it("handles parameter values containing '='") {
+            expect(parseUrlParameters("A=1=1&B=2")) == [ "A": "1=1", "B": "2" ]
+        }
+
+        it("removes percent encoding") {
+            expect(parseUrlParameters("A%20B=1%202")) == [ "A B": "1 2" ]
+        }
+
+        it("ignores incorrect percent encoding") {
+            expect(parseUrlParameters("A=1&B=%2")) == [ "A": "1" ]
+            expect(parseUrlParameters("A=1&B%=2")) == [ "A": "1" ]
+        }
+    }
+}
