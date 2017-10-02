0a7f08bd5dc885c008254a2081c1778011fbab2c
Spanbroek, Mark <mark@spanbroek.net>
2017-10-02 15:13:35 +0200

    Remove padding from base64url encoding

--- Telepath/URL/Base64url.swift
+++ Telepath/URL/Base64url.swift
@@ -5,6 +5,9 @@ extension Data {
         var base64 = input
         base64 = base64.replacingOccurrences(of: "-", with: "+")
         base64 = base64.replacingOccurrences(of: "_", with: "/")
+        while base64.count % 4 != 0 {
+            base64 = base64.appending("=")
+        }
         self.init(base64Encoded: base64)
     }
 
@@ -12,6 +15,7 @@ extension Data {
         var result = self.base64EncodedString()
         result = result.replacingOccurrences(of: "+", with: "-")
         result = result.replacingOccurrences(of: "/", with: "_")
+        result = result.replacingOccurrences(of: "=", with: "")
         return result
     }
 }
