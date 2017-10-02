80347e77a9c4debe166d1d32e41d2220ea0c386a
Stefan van den Oord <stefan.van.den.oord@philips.com>
2017-10-02 11:07:06 +0200

    [BLA-159] It can dispatch rejected and calls newAccount (WIP)

--- Cogito/Create identity/CreateIdentityActions.swift
+++ Cogito/Create identity/CreateIdentityActions.swift
@@ -1,6 +1,12 @@
 //  Copyright © 2017 Koninklijke Philips Nederland N.V. All rights reserved.
 
 import ReSwift
+import ReSwiftThunk
+
+// In this file we disable SwiftLint `identifier_name`, because we don't want
+// to make a syntactic difference between normal actions and thunks; from the
+// outside it doesn't matter which it is.
+// swiftlint:disable identifier_name
 
 struct CreateIdentityActions {
     struct Reset: Action {}
@@ -9,5 +15,20 @@ struct CreateIdentityActions {
         let description: String
     }
 
-    struct CreateIdentity: Action {}
+    static func CreateIdentity() -> ThunkAction<AppState> {
+        return ThunkAction(action: { (dispatch, getState) in
+            dispatch(Pending())
+            guard let state = getState(),
+                  let keyStore = state.keyStore.keyStore else {
+                dispatch(Rejected(message: "key store not found"))
+                return
+            }
+            keyStore.newAccount()
+        })
+    }
+
+    struct Pending: Action {}
+    struct Rejected: Action {
+        let message: String
+    }
 }
