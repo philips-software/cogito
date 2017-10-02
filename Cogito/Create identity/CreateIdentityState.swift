0fe95ee187710390b48bb50eadf5bd373bc07e00
Stefan van den Oord <stefan.van.den.oord@philips.com>
2017-10-02 16:44:22 +0200

    [BLA-159] Extended CreateIdentityState with pending/fulfilled/rejected

--- Cogito/Create identity/CreateIdentityState.swift
+++ Cogito/Create identity/CreateIdentityState.swift
@@ -1,7 +1,41 @@
 //  Copyright © 2017 Koninklijke Philips Nederland N.V. All rights reserved.
 
+import Geth
+
 struct CreateIdentityState: Codable {
     var description: String
+    var pending: Bool
+    var newAccount: GethAccount?
+    var error: String?
+
+    init(description: String, pending: Bool, newAccount: GethAccount?, error: String?) {
+        self.description = description
+        self.pending = pending
+        self.newAccount = newAccount
+        self.error = error
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        description = try container.decode(String.self, forKey: .description)
+        pending = false
+        newAccount = nil
+        error = nil
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case description
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(description, forKey: .description)
+    }
 }
 
-let initialCreateIdentityState = CreateIdentityState(description: "")
+let initialCreateIdentityState = CreateIdentityState(
+    description: "",
+    pending: false,
+    newAccount: nil,
+    error: nil
+)
--- CogitoTests/Create identity/CreateIdentityReducerSpec.swift
+++ CogitoTests/Create identity/CreateIdentityReducerSpec.swift
@@ -2,6 +2,7 @@
 
 import Quick
 import Nimble
+import Geth
 
 class CreateIdentityReducerSpec: QuickSpec {
     override func spec() {
@@ -12,10 +13,52 @@ class CreateIdentityReducerSpec: QuickSpec {
         }
 
         it("handles Reset") {
-            let state = CreateIdentityState(description: "me")
+            let state = CreateIdentityState(description: "me",
+                                            pending: true,
+                                            newAccount: GethAccount(),
+                                            error: "some error")
             let action = CreateIdentityActions.Reset()
             let nextState = createIdentityReducer(action: action, state: state)
             expect(nextState.description) == ""
+            expect(nextState.pending).to(beFalse())
+            expect(nextState.newAccount).to(beNil())
+            expect(nextState.error).to(beNil())
+        }
+
+        it("handles Pending") {
+            let state = CreateIdentityState(description: "me",
+                                            pending: false,
+                                            newAccount: GethAccount(),
+                                            error: "some error")
+            let action = CreateIdentityActions.Pending()
+            let nextState = createIdentityReducer(action: action, state: state)
+            expect(nextState.pending).to(beTrue())
+            expect(nextState.newAccount).to(beNil())
+            expect(nextState.error).to(beNil())
+        }
+
+        it("handles Fulfilled") {
+            let state = CreateIdentityState(description: "me",
+                                            pending: true,
+                                            newAccount: nil,
+                                            error: nil)
+            let account = GethAccount()!
+            let action = CreateIdentityActions.Fulfilled(account: account)
+            let nextState = createIdentityReducer(action: action, state: state)
+            expect(nextState.pending).to(beFalse())
+            expect(nextState.newAccount) === account
+        }
+
+        it("handles Rejected") {
+            let state = CreateIdentityState(description: "me",
+                                            pending: true,
+                                            newAccount: nil,
+                                            error: nil)
+            let error = "some error"
+            let action = CreateIdentityActions.Rejected(message: error)
+            let nextState = createIdentityReducer(action: action, state: state)
+            expect(nextState.pending).to(beFalse())
+            expect(nextState.error) == error
         }
     }
 }
