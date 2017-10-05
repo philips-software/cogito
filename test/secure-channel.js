11b0e0ad564944f74ce4c215a7f22bcde2911065
Spanbroek, Mark <mark@spanbroek.net>
2017-10-05 17:45:59 +0200

    Decrypt messages when receiving

--- test/secure-channel.js
+++ test/secure-channel.js
@@ -5,6 +5,7 @@ const random = sodium.randombytes_buf
 const nonceSize = sodium.crypto_secretbox_NONCEBYTES
 const keySize = sodium.crypto_secretbox_KEYBYTES
 const decrypt = sodium.crypto_secretbox_open_easy
+const encrypt = sodium.crypto_secretbox_easy
 const { SecureChannel } = require('../lib/secure-channel')
 
 describe('secure-channel', function () {
@@ -37,6 +38,25 @@ describe('secure-channel', function () {
 
     it('uses the red queue', function () {
       td.verify(queuing.send(channelId + '.red', td.matchers.anything()))
+    })
+  })
+
+  context('when receiving a message (on the blue queue)', function () {
+    const message = 'a message'
+    const blueQueue = `${channelId}.blue`
+
+    let receivedMessage
+
+    beforeEach(async function () {
+      const nonce = Buffer.from(random(nonceSize))
+      const cypherText = encrypt(Buffer.from(message), nonce, key)
+      const nonceAndCypherText = Buffer.concat([nonce, cypherText])
+      td.when(queuing.receive(blueQueue)).thenResolve(nonceAndCypherText)
+      receivedMessage = await channel.receive()
+    })
+
+    it('decrypts the message', function () {
+      expect(receivedMessage).to.equal(message)
     })
   })
 })
