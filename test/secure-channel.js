34788083e17d5391ef24d4beab3dab3879ef2c75
Spanbroek, Mark <mark@spanbroek.net>
2017-10-05 15:53:18 +0200

    Encrypt messages when sending

--- test/secure-channel.js
+++ test/secure-channel.js
@@ -1,19 +1,42 @@
+const expect = require('chai').expect
 const td = require('testdouble')
+const sodium = require('libsodium-wrappers')
+const random = sodium.randombytes_buf
+const nonceSize = sodium.crypto_secretbox_NONCEBYTES
+const keySize = sodium.crypto_secretbox_KEYBYTES
+const decrypt = sodium.crypto_secretbox_open_easy
 const { SecureChannel } = require('../lib/secure-channel')
 
 describe('secure-channel', function () {
   const channelId = 'channel_id'
+  const key = random(keySize)
 
   let channel
   let queuing
 
   beforeEach(function () {
     queuing = td.object()
-    channel = new SecureChannel({ queuing, id: channelId })
+    channel = new SecureChannel({ queuing, id: channelId, key })
   })
 
-  it('uses the red queue for sending', function () {
-    channel.send()
-    td.verify(queuing.send(channelId + '.red'))
+  context('when sending a message', function () {
+    const message = 'a message'
+
+    beforeEach(function () {
+      channel.send(message)
+    })
+
+    it('encrypts the message', function () {
+      const captor = td.matchers.captor()
+      td.verify(queuing.send(td.matchers.anything(), captor.capture()))
+      const nonceAndCypherText = captor.value
+      const nonce = nonceAndCypherText.slice(0, nonceSize)
+      const cypherText = nonceAndCypherText.slice(nonceSize)
+      expect(decrypt(cypherText, nonce, key, 'text')).to.equal(message)
+    })
+
+    it('uses the red queue', function () {
+      td.verify(queuing.send(channelId + '.red', td.matchers.anything()))
+    })
   })
 })
