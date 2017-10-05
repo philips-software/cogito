d4993d5e5fa2549d2dd4691ae681d523ccd5b2da
Stefan van den Oord <stefan.van.den.oord@philips.com>
2017-10-05 17:05:53 +0200

    [wip failing test] Add identity facet when new identity is created

--- CogitoTests/Diamond/DiamondReducerSpec.swift
+++ CogitoTests/Diamond/DiamondReducerSpec.swift
@@ -2,8 +2,43 @@
 
 import Quick
 import Nimble
+import Geth
 
 class DiamondReducerSpec: QuickSpec {
     override func spec() {
+        var keyStoreDir: String!
+        var keyStore: GethKeyStore!
+        var account0: GethAccount!
+
+        beforeEach {
+            keyStoreDir = NSSearchPathForDirectoriesInDomains(.cachesDirectory,
+                                                              .userDomainMask,
+                                                              true)[0] + "/test.keystore"
+            keyStore = GethKeyStore(keyStoreDir,
+                                    scryptN: GethLightScryptN,
+                                    scryptP: GethLightScryptP)
+            expect {
+                account0 = try keyStore.newAccount("some pass")
+                }.toNot(throwError())
+        }
+
+        afterEach {
+            do {
+                try FileManager.default.removeItem(atPath: keyStoreDir)
+            } catch let e {
+                print(e)
+                abort()
+            }
+        }
+
+        context("when a new identity is created") {
+            it("adds the correspoding facet to the diamond") {
+                let action = CreateIdentityActions.Fulfilled(account: account0)
+                let state = diamondReducer(action: action, state: nil)
+                expect(state.facets.count) == 1
+                expect(state.facets[0]) == Identity(description: "my id",
+                                                    gethAddress: account0.getAddress()!)
+            }
+        }
     }
 }
