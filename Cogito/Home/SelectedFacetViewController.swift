e63f9e9619bde6ac5c1377660c74d4b03d0ab501
Stefan van den Oord <stefan.van.den.oord@philips.com>
2017-10-09 14:13:56 +0200

    Added animations after initial setup

--- Cogito/Home/SelectedFacetViewController.swift
+++ Cogito/Home/SelectedFacetViewController.swift
@@ -4,6 +4,7 @@ import UIKit
 import ReSwift
 import ReRxSwift
 import RxSwift
+import Geth
 
 class SelectedFacetViewController: UIViewController, Connectable {
 
@@ -11,20 +12,82 @@ class SelectedFacetViewController: UIViewController, Connectable {
                                 mapStateToProps: mapStateToProps,
                                 mapDispatchToActions: mapDispatchToActions)
     @IBOutlet weak var headerButton: UIButton!
+    @IBOutlet weak var facetLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        connection.bind(\Props.selectedFacet, to: headerButton.rx.title(for: .normal)) {
-            ($0 != nil) ? "I am." : "Who am I?"
+        headerButton.titleLabel?.textAlignment = .center
+        connection.bind(\Props.selectedFacet, to: headerButton.rx.title(for: .normal)) { [unowned self] in
+            ($0 != nil && !self.firstFacetWasCreated()) ? "I am." : "Who am I?"
+        }
+        connection.bind(\Props.selectedFacet, to: facetLabel.rx.isHidden) { [unowned self] in
+            !($0 != nil && !self.firstFacetWasCreated())
         }
         connection.bind(\Props.selectedFacet, to: headerButton.rx.isUserInteractionEnabled) {
             $0 == nil
         }
+        connection.bind(\Props.selectedFacet, to: facetLabel.rx.text) { $0?.description ?? "" }
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         connection.connect()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        if firstFacetWasCreated() {
+            transitionToSelectedFacet()
+        }
+    }
+
+    func firstFacetWasCreated() -> Bool {
+        return props.createdNewAccount != nil && props.selectedFacet != nil && props.facets.count == 1
+    }
+
+    private func transitionToSelectedFacet() {
+        connection.disconnect()
+
+        let text = "Who am I?"
+        var endIndex = text.endIndex
+        var delay: Double = 0
+        let delayStep = 0.2
+        repeat {
+            endIndex = text.index(endIndex, offsetBy: -1)
+            delay += delayStep
+            setHeaderTitle(String(text[..<endIndex]), afterDelay: delay)
+        } while endIndex > text.startIndex
+
+        let newText = "I am."
+        endIndex = newText.startIndex
+        repeat {
+            endIndex = newText.index(endIndex, offsetBy: 1)
+            delay += delayStep
+            setHeaderTitle(String(newText[..<endIndex]), afterDelay: delay)
+        } while endIndex < newText.endIndex
+
+        delay += delayStep
+        facetLabel.alpha = 0
+        facetLabel.isHidden = false
+        UIView.animate(withDuration: 2, delay: delay,
+                       usingSpringWithDamping: 1, initialSpringVelocity: 0,
+                       options: [],
+                       animations: { [weak self] in
+            self?.facetLabel.alpha = 1
+        })
+
+        delay += delayStep
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
+            self?.connection.connect()
+            self?.actions.resetCreateIdentity()
+        }
+    }
+
+    private func setHeaderTitle(_ title: String, afterDelay delay: Double) {
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + delay) { [weak self] in
+            self?.headerButton.titleLabel?.text = title
+        }
     }
 
     override func viewDidDisappear(_ animated: Bool) {
@@ -41,6 +104,8 @@ class SelectedFacetViewController: UIViewController, Connectable {
 
     struct Props {
         let selectedFacet: Identity?
+        let createdNewAccount: GethAccount?
+        let facets: [Identity]
     }
 
     struct Actions {
@@ -55,7 +120,11 @@ private func mapStateToProps(state: AppState) -> SelectedFacetViewController.Pro
     } else {
         selectedFacet = nil
     }
-    return SelectedFacetViewController.Props(selectedFacet: selectedFacet)
+    return SelectedFacetViewController.Props(
+        selectedFacet: selectedFacet,
+        createdNewAccount: state.createIdentity.newAccount,
+        facets: state.diamond.facets
+    )
 }
 
 private func mapDispatchToActions(dispatch: @escaping DispatchFunction)
