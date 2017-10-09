e7b52e4313f2770a6c3232e8016aa042d27f1dbb
Stefan van den Oord <stefan.van.den.oord@philips.com>
2017-10-09 23:17:54 +0200

    Added app reducer test for reset action

---
+++ CogitoTests/Global/AppReducerSpec.swift
@@ -0,0 +1,21 @@
+//  Copyright © 2017 Koninklijke Philips Nederland N.V. All rights reserved.
+
+import Quick
+import Nimble
+import Geth
+
+class AppReducerSpec: QuickSpec {
+    override func spec() {
+        it("returns initial state when reset") {
+            let state = appState(
+                keyStore: KeyStoreState(keyStore: KeyStore(name: "test", scryptN: 0, scryptP: 0)),
+                geth: GethState(peersCount: 1, syncProgress: nil),
+                createIdentity: CreateIdentityState(description: "test", pending: true, newAccount: nil, error: "test"),
+                diamond: DiamondState(facets: [Identity(description: "test", gethAddress: GethAddress())])
+            )
+            let action = ResetAppState()
+            let nextState = appReducer(action: action, state: state)
+            expect(nextState) == initialAppState
+        }
+    }
+}
