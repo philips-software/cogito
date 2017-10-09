6729f6c8e24a34bd622919637250bb7857a0bcd5
Spanbroek, Mark <mark@spanbroek.net>
2017-10-09 15:22:03 +0200

    Allow creation of secure channels with random id and key

---
+++ test/telepath.js
@@ -0,0 +1,47 @@
+/* eslint-env mocha */
+const chai = require('chai')
+const expect = chai.expect
+const td = require('testdouble')
+const base64url = require('base64url')
+const crypto = td.replace('../lib/crypto')
+const Telepath = require('../lib/telepath')
+
+describe('telepath', function () {
+  let telepath
+  let queuing
+
+  beforeEach(function () {
+    queuing = td.object()
+    telepath = new Telepath({ queuing })
+  })
+
+  afterEach(function () {
+    td.reset()
+  })
+
+  context('when creating a new channel', function () {
+    const randomId = [1, 2, 3]
+    const randomKey = [4, 5, 6]
+
+    let channel
+
+    beforeEach(function () {
+      const idSize = 18
+      td.when(crypto.random(idSize)).thenReturn(randomId)
+      td.when(crypto.random(crypto.keySize)).thenReturn(randomKey)
+      channel = telepath.createChannel()
+    })
+
+    it('uses the queuing service', function () {
+      expect(channel.queuing).to.equal(queuing)
+    })
+
+    it('has a random id', function () {
+      expect(channel.id).to.equal(base64url.encode(randomId))
+    })
+
+    it('has a random key', function () {
+      expect(channel.key).to.equal(randomKey)
+    })
+  })
+})
