ef6fb029c4f9724d46cb025509011b8ac79a324b
Stefan van den Oord <stefan.van.den.oord@philips.com>
2017-10-09 11:03:14 +0200

    Use existing CreateIdentityViewController

--- Cogito/Initial setup wizard/InitialExplanationViewController.swift
+++ Cogito/Initial setup wizard/InitialExplanationViewController.swift
@@ -3,9 +3,14 @@
 import UIKit
 
 class InitialExplanationViewController: UIViewController {
-    
+
     @IBAction func cancel(_ sender: UIBarButtonItem) {
         dismiss(animated: true)
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let nextVC = segue.destination as? CreateIdentityViewController {
+            nextVC.onDone = { nextVC.dismiss(animated: true) }
+        }
+    }
 }
