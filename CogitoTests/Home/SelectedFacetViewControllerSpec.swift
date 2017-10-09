c6b3cf1a7fa0d2f53fe9502cf46f79abcbe4567f
Stefan van den Oord <stefan.van.den.oord@philips.com>
2017-10-09 14:13:56 +0200

    Added animations after initial setup

--- CogitoTests/Home/SelectedFacetViewControllerSpec.swift
+++ CogitoTests/Home/SelectedFacetViewControllerSpec.swift
@@ -8,6 +8,9 @@ import ReSwift
 
 class SelectedFacetViewControllerSpec: QuickSpec {
     override func spec() {
+        let testAddress = GethAddress(fromHex: "0x0000000000000000000000000000000000000000")!
+        let testIdentity = Identity(description: "testdesc",
+                                    gethAddress: testAddress)
         var viewController: SelectedFacetViewController!
 
         beforeEach {
@@ -18,18 +21,40 @@ class SelectedFacetViewControllerSpec: QuickSpec {
         }
 
         describe("map state to props") {
-            it("maps selected facet when there is no facet") {
+            it("maps selectedFacet when there is no facet") {
                 let state = appState(diamond: DiamondState(facets: []))
                 viewController.connection.newState(state: state)
                 expect(viewController.props.selectedFacet).to(beNil())
             }
 
-            it("maps selected facet when there is one") {
-                let address = GethAddress(fromHex: "0x0000000000000000000000000000000000000000")!
-                let identity = Identity(description: "test", gethAddress: address)
-                let state = appState(diamond: DiamondState(facets: [identity]))
+            it("maps selectedFacet when there is one") {
+                let state = appState(diamond: DiamondState(facets: [testIdentity]))
                 viewController.connection.newState(state: state)
-                expect(viewController.props.selectedFacet) == identity
+                expect(viewController.props.selectedFacet) == testIdentity
+            }
+
+            it("maps createdNewAccount when there is none") {
+                let state = appState(createIdentity: CreateIdentityState(description: "testdesc",
+                                                                         pending: true,
+                                                                         newAccount: nil,
+                                                                         error: nil))
+                viewController.connection.newState(state: state)
+                expect(viewController.props.createdNewAccount).to(beNil())
+            }
+
+            it("maps createdNewAcount when there is one") {
+                let state = appState(createIdentity: CreateIdentityState(description: "testdesc",
+                                                                         pending: false,
+                                                                         newAccount: GethAccount()!,
+                                                                         error: nil))
+                viewController.connection.newState(state: state)
+                expect(viewController.props.createdNewAccount).toNot(beNil())
+            }
+
+            it("maps facets") {
+                let state = appState(diamond: DiamondState(facets: [testIdentity]))
+                viewController.connection.newState(state: state)
+                expect(viewController.props.facets) == [testIdentity]
             }
         }
 
@@ -51,25 +76,39 @@ class SelectedFacetViewControllerSpec: QuickSpec {
 
         context("when selectedFacet is nil") {
             beforeEach {
-                viewController.props = SelectedFacetViewController.Props(selectedFacet: nil)
+                viewController.props = SelectedFacetViewController.Props(
+                    selectedFacet: nil,
+                    createdNewAccount: nil,
+                    facets: [])
             }
 
             it("has correct header button title and is touchable") {
                 expect(viewController.headerButton.title(for: .normal)) == "Who am I?"
                 expect(viewController.headerButton.isUserInteractionEnabled).to(beTrue())
             }
+
+            it("has empty facet label and facet label is hidden") {
+                expect(viewController.facetLabel.text) == ""
+                expect(viewController.facetLabel.isHidden).to(beTrue())
+            }
         }
 
         context("when selectedFacet is not nil") {
             beforeEach {
-                let identity = Identity(description: "test",
-                                        gethAddress: GethAddress())
-                viewController.props = SelectedFacetViewController.Props(selectedFacet: identity)
+                viewController.props = SelectedFacetViewController.Props(
+                    selectedFacet: testIdentity,
+                    createdNewAccount: nil,
+                    facets: [testIdentity, testIdentity])
             }
 
             it("has correct header button title and is not touchable") {
                 expect(viewController.headerButton.title(for: .normal)) == "I am."
                 expect(viewController.headerButton.isUserInteractionEnabled).to(beFalse())
+            }
+
+            it("has facet label and facet label is visible") {
+                expect(viewController.facetLabel.text) == "testdesc"
+                expect(viewController.facetLabel.isHidden).to(beFalse())
             }
         }
 
@@ -80,6 +119,28 @@ class SelectedFacetViewControllerSpec: QuickSpec {
             })
             viewController.whoAmITouched()
             expect(resetCalled).to(beTrue())
+        }
+
+        it("knows if the first facets was just created") {
+            viewController.props = SelectedFacetViewController.Props(selectedFacet: nil,
+                                                                     createdNewAccount: nil,
+                                                                     facets: [])
+            expect(viewController.firstFacetWasCreated()).to(beFalse())
+
+            viewController.props = SelectedFacetViewController.Props(selectedFacet: testIdentity,
+                                                                     createdNewAccount: nil,
+                                                                     facets: [testIdentity])
+            expect(viewController.firstFacetWasCreated()).to(beFalse())
+
+            viewController.props = SelectedFacetViewController.Props(selectedFacet: testIdentity,
+                                                                     createdNewAccount: GethAccount()!,
+                                                                     facets: [testIdentity])
+            expect(viewController.firstFacetWasCreated()).to(beTrue())
+
+            viewController.props = SelectedFacetViewController.Props(selectedFacet: testIdentity,
+                                                                     createdNewAccount: GethAccount()!,
+                                                                     facets: [testIdentity, testIdentity])
+            expect(viewController.firstFacetWasCreated()).to(beFalse())
         }
     }
 }
