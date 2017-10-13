d41210177999078f3f4b5b3be949269b4165fa11
Spanbroek, Mark <mark@spanbroek.net>
2017-10-13 15:32:17 +0200

    Handle queues correctly

--- test/server.js
+++ test/server.js
@@ -4,7 +4,6 @@ const createServer = require('../lib/server')
 
 describe('Server', function () {
   const queueId = 'a_queue_id'
-  const message = 'a message'
 
   let server
 
@@ -12,19 +11,10 @@ describe('Server', function () {
     server = createServer()
   })
 
-  context('when a message has been sent', function () {
-    beforeEach(async function () {
-      await request(server)
-        .post(`/${queueId}`)
-        .send(message)
-    })
-
-    it('can be received', async function () {
-      await request(server)
-        .get(`/${queueId}`)
-        .expect(200)
-        .expect(message)
-    })
+  it('receives messages that have been sent', async function () {
+    const message = 'a message'
+    await request(server).post(`/${queueId}`).send(message)
+    await request(server).get(`/${queueId}`).expect(200).expect(message)
   })
 
   it('delivers messages in fifo order', async function () {
@@ -32,5 +22,22 @@ describe('Server', function () {
     await request(server).post(`/${queueId}`).send('message 2')
     await request(server).get(`/${queueId}`).expect('message 1')
     await request(server).get(`/${queueId}`).expect('message 2')
+  })
+
+  it('delivers messages to the correct queue', async function () {
+    await request(server).post(`/queueA`).send('messageA')
+    await request(server).post(`/queueB`).send('messageB')
+    await request(server).get(`/queueB`).expect('messageB')
+    await request(server).get(`/queueA`).expect('messageA')
+  })
+
+  it('returns status code 204 when queue is empty', async function () {
+    await request(server).post(`/${queueId}`).send('message')
+    await request(server).get(`/${queueId}`)
+    await request(server).get(`/${queueId}`).expect(204)
+  })
+
+  it('returns status code 204 when queue is non-existent', async function () {
+    await request(server).get('/non-existent').expect(204)
   })
 })
