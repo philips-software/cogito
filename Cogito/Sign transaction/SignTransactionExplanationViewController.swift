771a7907297c04fdb2ed72dfd0badfbe464649ee
Stefan van den Oord <stefan.van.den.oord@philips.com>
2017-10-23 17:04:05 +0200

    Initial empty 'sign transaction' screen

---
+++ Cogito/Sign transaction/SignTransactionExplanationViewController.swift
@@ -0,0 +1,10 @@
+//  Copyright © 2017 Koninklijke Philips Nederland N.V. All rights reserved.
+
+import UIKit
+
+class SignTransactionExplanationViewController: UIViewController {
+
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: {})
+    }
+}
