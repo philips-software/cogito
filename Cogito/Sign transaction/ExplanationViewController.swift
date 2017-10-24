4fb3e7545bc87781d6cd7bb2e087da4807f81efd
Stefan van den Oord <stefan.van.den.oord@philips.com>
2017-10-24 10:40:51 +0200

    Configure action buttons based on props

--- Cogito/Sign transaction/ExplanationViewController.swift
+++ Cogito/Sign transaction/ExplanationViewController.swift
@@ -6,16 +6,44 @@ import ReSwift
 
 class ExplanationViewController: UIViewController, Connectable {
 
+    static let signColor = UIColor(red: 0.816, green: 0.259, blue: 0.196, alpha: 1)
+    static let rejectColor = UIColor(red: 0.111, green: 0.749, blue: 0.247, alpha: 1)
+    static let signDisabledColor = UIColor.lightGray
+    static let rejectDisabledColor = UIColor.gray
+
     @IBOutlet weak var explanationLabel: UILabel!
+    @IBOutlet weak var startAttestationButton: UIButton!
+    @IBOutlet weak var signButton: UIButton!
+    @IBOutlet weak var rejectButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         connection.subscribe(\Props.appName) { _ in self.configureExplanation() }
         connection.subscribe(\Props.actionDescription) { _ in  self.configureExplanation() }
+        connection.subscribe(\Props.hasValidAttestation) { _ in self.configureButtons() }
     }
 
     func configureExplanation() {
-        explanationLabel.text = "\(props.appName) requires your signature to \(props.actionDescription)."
+        var text = "\(props.appName) requires your signature to \(props.actionDescription)."
+        if let org = props.organization {
+            text += "\n\nIn addition, \(props.appName) requires proof that you have an account"
+            text += " with \(org)."
+        }
+        explanationLabel.text = text
+    }
+
+    func configureButtons() {
+        let canSign = props.hasValidAttestation ?? true
+        if canSign {
+            signButton.backgroundColor = ExplanationViewController.signColor
+            rejectButton.backgroundColor = ExplanationViewController.rejectColor
+        } else {
+            signButton.backgroundColor = ExplanationViewController.signDisabledColor
+            rejectButton.backgroundColor = ExplanationViewController.rejectDisabledColor
+        }
+        signButton.isEnabled = canSign
+        rejectButton.isEnabled = canSign
+        startAttestationButton.isHidden = canSign
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -32,11 +60,26 @@ class ExplanationViewController: UIViewController, Connectable {
         dismiss(animated: true, completion: {})
     }
 
+    @IBAction func startAttestation() {
+    }
+
+    @IBAction func sign() {
+    }
+
+    @IBAction func reject() {
+    }
+
     struct Props {
         let appName: String
         let actionDescription: String
+        let organization: String?
+        let hasValidAttestation: Bool?
     }
-    struct Actions {}
+    struct Actions {
+        let sign: () -> Void
+        let reject: () -> Void
+        let startAttestation: () -> Void
+    }
     let connection = Connection(store: appStore,
                                 mapStateToProps: mapStateToProps,
                                 mapDispatchToActions: mapDispatchToActions)
@@ -45,10 +88,16 @@ class ExplanationViewController: UIViewController, Connectable {
 private func mapStateToProps(state: AppState) -> ExplanationViewController.Props {
     return ExplanationViewController.Props(
         appName: "Sample App",
-        actionDescription: "perform some sample action"
+        actionDescription: "perform some sample action",
+        organization: "Sample Organization",
+        hasValidAttestation: false
     )
 }
 
 private func mapDispatchToActions(dispatch: DispatchFunction) -> ExplanationViewController.Actions {
-    return ExplanationViewController.Actions()
+    return ExplanationViewController.Actions(
+        sign: {},
+        reject: {},
+        startAttestation: {}
+    )
 }
