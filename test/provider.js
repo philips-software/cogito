c10e361ad7e1750cd99e13e90fa058d7dd09475f
Spanbroek, Mark <mark@spanbroek.net>
2017-10-25 14:13:54 +0200

    Sign transactions using Cogito

--- test/provider.js
+++ test/provider.js
@@ -2,6 +2,7 @@
 const expect = require('chai').expect
 const td = require('testdouble')
 const anything = td.matchers.anything
+const contains = td.matchers.contains
 const Web3 = require('web3')
 const CogitoProvider = require('../lib/provider')
 const promisify = require('util.promisify')
@@ -29,6 +30,37 @@ describe('provider', function () {
       td.when(telepathChannel.receive(), { times: 1 }).thenResolve(response)
     })
     expect(await web3.eth.getAccounts()).to.eql(accounts)
+  })
+
+  context('when using cogito for signatures', function () {
+    const transaction = { from: '0x1234567890123456789012345678901234567890' }
+    const signed = '0xSignedTransaction'
+
+    beforeEach(function () {
+      const request = JSON.stringify({ method: 'sign', params: [ transaction ] })
+      const response = JSON.stringify({ result: signed })
+      td.when(telepathChannel.send(request)).thenDo(function () {
+        td.when(telepathChannel.receive(), { times: 1 }).thenResolve(response)
+      })
+    })
+
+    context('when using original provider for raw transactions', function () {
+      const hash = '0xTransactionHash'
+
+      beforeEach(function () {
+        const request = { method: 'eth_sendRawTransaction', params: [ signed ] }
+        const response = { jsonrpc: '2.0', result: hash, id: 1 }
+        td.when(originalProvider.sendAsync(contains(request)))
+          .thenCallback(null, response)
+      })
+
+      it('sends a cogito signed transaction', function (done) {
+        web3.eth.sendTransaction(transaction, function (_, result) {
+          expect(result).to.equal(hash)
+          done()
+        })
+      })
+    })
   })
 
   it('passes requests to the original provider', async function () {
