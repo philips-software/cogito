51cc861974e64e59efaef4fdb7fc04dd98d02ecb
Spanbroek, Mark <mark@spanbroek.net>
2017-10-27 11:40:58 +0200

    Action for connecting to telepath channel

--- Cogito/Telepath/TelepathActions.swift
+++ Cogito/Telepath/TelepathActions.swift
@@ -2,9 +2,31 @@
 
 import Foundation
 import ReSwift
+import ReSwiftThunk
+
+let queuingServiceUrl = URL(string: "https://telepath.cogito.mobi")!
 
 struct TelepathActions {
+    // swiftlint:disable identifier_name
+    static func Connect(url: URL) -> ThunkAction<AppState> {
+        return ThunkAction(action: { dispatch, _ in
+            do {
+                let channel = try TelepathChannel(
+                    queuingServiceUrl: queuingServiceUrl,
+                    connectUrl: url
+                )
+                dispatch(Connected(channel: channel))
+            } catch let error {
+                dispatch(ConnectionFailed(error: error))
+            }
+        })
+    }
+
     struct Connected: Action {
         let channel: TelepathChannel
+    }
+
+    struct ConnectionFailed: Action {
+        let error: Error
     }
 }
