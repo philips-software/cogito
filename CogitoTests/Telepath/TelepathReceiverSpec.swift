814af3e11102fd5549aa54cb8be370a50a6a2439
Spanbroek, Mark <mark@spanbroek.net>
2017-10-30 16:50:04 +0100

    Extract StoreSpy into separate file

--- CogitoTests/Telepath/TelepathReceiverSpec.swift
+++ CogitoTests/Telepath/TelepathReceiverSpec.swift
@@ -64,31 +64,6 @@ class TelepathReceiverSpec: QuickSpec {
 private typealias ReceiveFulfilled = TelepathActions.ReceiveFulfilled
 private typealias ReceiveRejected = TelepathActions.ReceiveRejected
 
-class StoreSpy: Store<AppState> {
-    var latestSubscriber: AnyStoreSubscriber?
-    var latestUnsubscriber: AnyStoreSubscriber?
-    var actions = [Action]()
-
-    convenience init() {
-        self.init(reducer: { _, _ in return initialAppState }, state: nil)
-    }
-
-    override func subscribe<SelectedState, S>(
-        _ subscriber: S, transform:((Subscription<AppState>) -> Subscription<SelectedState>)?
-    ) where SelectedState == S.StoreSubscriberStateType, S: StoreSubscriber {
-        latestSubscriber = subscriber
-        super.subscribe(subscriber, transform: transform)
-    }
-
-    override func unsubscribe(_ subscriber: AnyStoreSubscriber) {
-        latestUnsubscriber = subscriber
-    }
-
-    override func dispatch(_ action: Action) {
-        actions.append(action)
-    }
-}
-
 class TelepathChannelSpy: TelepathChannelType {
     var receiveMessage: String?
     var receiveError: Error?
