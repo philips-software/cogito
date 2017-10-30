1a8eb809a5675e7c0f8d6633ef98f3c607ca3c7f
Spanbroek, Mark <mark@spanbroek.net>
2017-10-30 16:04:11 +0100

    Connect to Telepath channel when QR code is scanned

--- CogitoTests/Home/HomeViewControllerSpec.swift
+++ CogitoTests/Home/HomeViewControllerSpec.swift
@@ -2,8 +2,45 @@
 
 import Quick
 import Nimble
+import ReSwift
+import ReSwiftThunk
 
 class HomeViewControllerSpec: QuickSpec {
     override func spec() {
+        var home: HomeViewController!
+        var store: Store<AppState>!
+
+        beforeEach {
+            let reducer: Reducer<AppState> = { _, _ in return initialAppState }
+            store = Store<AppState>(reducer: reducer, state: nil)
+            home = self.instantiateViewController()
+            home.connection.store = store
+        }
+
+        describe("connecting to a telepath url") {
+            var recorder: DispatchRecorder<ThunkAction<AppState>>!
+
+            beforeEach {
+                recorder = DispatchRecorder<ThunkAction<AppState>>()
+                store.dispatchFunction = recorder.dispatch
+            }
+
+            it("dispatches the connect action") {
+                home.actions.connectToTelepathChannel("http://valid.url")
+                expect(recorder.count) == 1
+            }
+
+            it("does not dispatch when the URL is incorrect") {
+                home.actions.connectToTelepathChannel("invalid url")
+                expect(recorder.count) == 0
+            }
+        }
+    }
+
+    func instantiateViewController() -> HomeViewController? {
+        let bundle = Bundle(for: type(of: self))
+        let storyboard = UIStoryboard(name: "Home", bundle: bundle)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "Home")
+        return viewController as? HomeViewController
     }
 }
