e3406066a65e534b47c43449d68638da7d3416bf
Spanbroek, Mark <mark@spanbroek.net>
2017-10-31 10:22:12 +0100

    Dispatch GetAccounts action when an account request is received via Telepath

---
+++ CogitoTests/Accounts/AccountServiceSpec.swift
@@ -0,0 +1,72 @@
+//Copyright © 2017 Koninklijke Philips Nederland N.V. All rights reserved.
+
+import Quick
+import Nimble
+import ReSwiftThunk
+
+class AccountServiceSpec: QuickSpec {
+    override func spec() {
+        var service: AccountService!
+        var store: StoreSpy!
+
+        beforeEach {
+            store = StoreSpy()
+            service = AccountService(store: store)
+        }
+
+        it("subscribes to incoming Telepath messages") {
+            service.start()
+            expect(store.latestSubscriber) === service
+        }
+
+        it("unsubscribes") {
+            service.stop()
+            expect(store.latestUnsubscriber) === service
+        }
+
+        describe("incoming messages") {
+            let accountRequest = "{\"method\":\"accounts\"}"
+            let otherRequest = "{\"method\":\"other\"}"
+
+            func whenReceiving(messages: [String]) {
+                beforeEach {
+                    service.newState(state: messages)
+                }
+            }
+
+            func itDispatchesGetAccounts() {
+                it("dispatches the GetAccounts action") {
+                    expect(store.actions.last as? ThunkAction<AppState>)
+                        .toNot(beNil())
+                }
+            }
+
+            func itDoesNotDispatchGetAccounts() {
+                it("does not dispatch the GetAccount action") {
+                    expect(store.actions.last as? ThunkAction<AppState>)
+                        .to(beNil())
+                }
+            }
+
+            context("when an account requests comes in") {
+                whenReceiving(messages: [accountRequest])
+                itDispatchesGetAccounts()
+            }
+
+            context("when a different request comes in") {
+                whenReceiving(messages: [otherRequest])
+                itDoesNotDispatchGetAccounts()
+            }
+
+            context("when the list of messages is empty") {
+                whenReceiving(messages: [])
+                itDoesNotDispatchGetAccounts()
+            }
+
+            context("when the account request is not the oldest message") {
+                whenReceiving(messages: [otherRequest, accountRequest])
+                itDoesNotDispatchGetAccounts()
+            }
+        }
+    }
+}
