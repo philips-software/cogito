fc25b7c7833c6dd34cd751d657d26ba5643f99ea
Spanbroek, Mark <mark@spanbroek.net>
2017-10-31 17:00:27 +0100

    Cleanup

--- Cogito/Telepath/TelepathActions.swift
+++ Cogito/Telepath/TelepathActions.swift
@@ -7,14 +7,14 @@ import ReSwiftThunk
 struct TelepathActions {
     // swiftlint:disable identifier_name
     static func Connect(url: URL) -> ThunkAction<AppState> {
-        return ThunkAction(action: { dispatch, _ in
+        return ThunkAction { dispatch, _ in
             do {
                 let channel = try TelepathChannel(connectUrl: url)
                 dispatch(ConnectFulfilled(channel: channel))
             } catch let error {
                 dispatch(ConnectRejected(error: error))
             }
-        })
+        }
     }
 
     struct ConnectFulfilled: Action {
