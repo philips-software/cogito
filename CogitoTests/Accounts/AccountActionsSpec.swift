da10b2b2fee170516038e80ddb39421f278b01a7
Spanbroek, Mark <mark@spanbroek.net>
2017-11-01 14:50:36 +0100

    Refactor: simplify tests by introducing RecordingStore
    RecordingStore uses ReSwiftThunk middleware to execute thunks.

--- CogitoTests/Accounts/AccountActionsSpec.swift
+++ CogitoTests/Accounts/AccountActionsSpec.swift
@@ -2,44 +2,38 @@
 
 import Quick
 import Nimble
-import ReSwiftThunk
 
 class AccountActionsSpec: QuickSpec {
     override func spec() {
+        var store: RecordingStore!
+
+        beforeEach {
+            store = RecordingStore()
+        }
+
         context("given that a facet was selected") {
             let address = Address.testAddress
-            var state: AppState!
 
             beforeEach {
                 let identity = Identity(description: "me", address: address)
-                state = appState(diamond: DiamondState(facets: [identity]))
+                store.state = appState(diamond: DiamondState(facets: [identity]))
             }
 
             it("sends out its Ethereum address via Telepath") {
-                let thunkRecorder = DispatchRecorder<ThunkAction<AppState>>()
-                let sendRecorder = DispatchRecorder<TelepathActions.SendPending>()
-                let action = AccountActions.GetAccounts()
-                action.action(thunkRecorder.dispatch, { return state })
-                thunkRecorder.actions[0].action(sendRecorder.dispatch, { return state })
-                let send = sendRecorder.actions.last
+                store.dispatch(AccountActions.GetAccounts())
+                let send = store.actions.last as? TelepathActions.SendPending
                 expect(send?.message) == "{\"result\":[\"\(address)\"]}"
             }
         }
 
         context("when no facet was selected") {
-            var state: AppState!
-
             beforeEach {
-                state = appState(diamond: DiamondState(facets: []))
+                store.state = appState(diamond: DiamondState(facets: []))
             }
 
             it("returns an empty list") {
-                let thunkRecorder = DispatchRecorder<ThunkAction<AppState>>()
-                let sendRecorder = DispatchRecorder<TelepathActions.SendPending>()
-                let action = AccountActions.GetAccounts()
-                action.action(thunkRecorder.dispatch, { return state })
-                thunkRecorder.actions[0].action(sendRecorder.dispatch, { return state })
-                let send = sendRecorder.actions.last
+                store.dispatch(AccountActions.GetAccounts())
+                let send = store.actions.last as? TelepathActions.SendPending
                 expect(send?.message) == "{\"result\":[]}"
             }
         }
