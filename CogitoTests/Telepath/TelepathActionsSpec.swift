78694a783644aedc723df78fd46e26e90bbfae21
Spanbroek, Mark <mark@spanbroek.net>
2017-11-01 15:05:39 +0100

    Refactor: simplify tests using RecordingStore

--- CogitoTests/Telepath/TelepathActionsSpec.swift
+++ CogitoTests/Telepath/TelepathActionsSpec.swift
@@ -5,87 +5,72 @@ import Nimble
 
 class TelepathActionsSpec: QuickSpec {
     override func spec() {
+        var store: RecordingStore!
+
+        beforeEach {
+            store = RecordingStore()
+        }
+
         describe("connection") {
             let connectUrl = URL(
                 string: "https://cogito.example.com/telepath/connect#I=1234&E=abcd"
             )!
 
             it("connects to a channel") {
-                let recorder = DispatchRecorder<TelepathActions.ConnectFulfilled>()
-                let action = TelepathActions.Connect(url: connectUrl)
-                action.action(recorder.dispatch, { return nil })
-                expect(recorder.count) == 1
+                store.dispatch(TelepathActions.Connect(url: connectUrl))
+                let fulfilled = store.actions.last as? TelepathActions.ConnectFulfilled
+                expect(fulfilled).toNot(beNil())
             }
 
             it("reports an error when connecting to a channel fails") {
-                let recorder = DispatchRecorder<TelepathActions.ConnectRejected>()
-                let action = TelepathActions.Connect(url: URL(string: "http://invalid")!)
-                action.action(recorder.dispatch, { return nil })
-                expect(recorder.count) == 1
+                store.dispatch(TelepathActions.Connect(url: URL(string: "http://invalid")!))
+                let rejected = store.actions.last as? TelepathActions.ConnectRejected
+                expect(rejected).toNot(beNil())
             }
         }
 
         describe("sending and receiving") {
-            var state: AppState!
             var channel: TelepathChannelSpy!
 
             beforeEach {
                 channel = TelepathChannelSpy()
-                state = appState(telepath: TelepathState(channel: channel))
+                store.state = appState(telepath: TelepathState(channel: channel))
             }
 
             it("receives messages") {
-                let recorder = DispatchRecorder<TelepathActions.ReceiveFulfilled>()
                 let message = "a message"
                 channel.receiveMessage = message
-
-                let action = TelepathActions.Receive()
-                action.action(recorder.dispatch, { return state })
-
-                expect(recorder.actions.last?.message)
-                    .toEventually(equal(message))
+                store.dispatch(TelepathActions.Receive())
+                let fulfilled = store.actions.last as? TelepathActions.ReceiveFulfilled
+                expect(fulfilled?.message) == message
             }
 
             it("reports errors while receiving") {
-                let recorder = DispatchRecorder<TelepathActions.ReceiveRejected>()
                 let error = ExampleError(message: "an error")
                 channel.receiveError = error
-
-                let action = TelepathActions.Receive()
-                action.action(recorder.dispatch, { return state })
-
-                expect(recorder.actions.last?.error as? ExampleError)
-                    .toEventually(equal(error))
+                store.dispatch(TelepathActions.Receive())
+                let rejected = store.actions.last as? TelepathActions.ReceiveRejected
+                expect(rejected?.error as? ExampleError) == error
             }
 
             it("sends messages") {
                 let message = "a message"
-
-                let action = TelepathActions.Send(message: message)
-                action.action({ _ in }, { return state })
-
+                store.dispatch(TelepathActions.Send(message: message))
                 expect(channel.sentMessage) == message
             }
 
             it("reports sending success") {
-                let recorder = DispatchRecorder<TelepathActions.SendFulfilled>()
-
-                let action = TelepathActions.Send(message: "")
-                action.action(recorder.dispatch, { return state })
-
-                expect(recorder.count).toEventually(equal(1))
+                store.dispatch(TelepathActions.Send(message: ""))
+                let fulfilled = store.actions.last as? TelepathActions.SendFulfilled
+                expect(fulfilled).toNot(beNil())
             }
 
             it("reports errors while sending") {
-                let recorder = DispatchRecorder<TelepathActions.SendRejected>()
                 let error = ExampleError(message: "an error")
                 channel.sendError = error
-
-                let action = TelepathActions.Send(message: "")
-                action.action(recorder.dispatch, { return state })
-
-                expect(recorder.actions.last?.error as? ExampleError)
-                    .toEventually(equal(error))
+                store.dispatch(TelepathActions.Send(message: ""))
+                let rejected = store.actions.last as? TelepathActions.SendRejected
+                expect(rejected?.error as? ExampleError) == error
             }
         }
     }
