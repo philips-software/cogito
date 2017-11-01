85d8184a81de55bb0e5e7d67f93c78dbd3356707
Spanbroek, Mark <mark@spanbroek.net>
2017-11-01 14:50:36 +0100

    Refactor: simplify tests by introducing RecordingStore
    RecordingStore uses ReSwiftThunk middleware to execute thunks.

---
+++ CogitoTests/RecordingStore.swift
@@ -0,0 +1,20 @@
+//  Copyright © 2017 Koninklijke Philips Nederland N.V. All rights reserved.
+
+import ReSwift
+import ReSwiftThunk
+
+class RecordingStore: Store<AppState> {
+    var actions = [Action]()
+
+    convenience init(state: AppState = initialAppState) {
+        let reducer: Reducer<AppState> = { _, currentState in
+            return currentState ?? state
+        }
+        self.init(reducer: reducer, state: nil, middleware: [ThunkMiddleware()])
+    }
+
+    override func dispatch(_ action: Action) {
+        actions.append(action)
+        super.dispatch(action)
+    }
+}
