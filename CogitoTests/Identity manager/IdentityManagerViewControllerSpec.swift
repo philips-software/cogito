698ec738be735ef1f3f7593a900b4420fa0aea67
Stefan van den Oord <stefan.van.den.oord@philips.com>
2017-11-02 11:04:52 +0100

    Added (empty) screen to manage identities

---
+++ CogitoTests/Identity manager/IdentityManagerViewControllerSpec.swift
@@ -0,0 +1,10 @@
+//  Copyright © 2017 Koninklijke Philips Nederland N.V. All rights reserved.
+
+import Quick
+import Nimble
+
+class IdentityManagerViewControllerSpec: QuickSpec {
+    override func spec() {
+        
+    }
+}
