6421b4aad1c517f39e78bb81bda41c96a9345c03
Stefan van den Oord <stefan.van.den.oord@philips.com>
2017-11-02 15:15:21 +0100

    Show facets

--- Cogito/Identity manager/IdentityManagerViewController.swift
+++ Cogito/Identity manager/IdentityManagerViewController.swift
@@ -4,16 +4,100 @@ import UIKit
 import RxSwift
 import RxCocoa
 import RxDataSources
+import ReSwift
+import ReRxSwift
 
 class IdentityManagerViewController: UITableViewController {
+
+    var dataSource: RxTableViewSectionedAnimatedDataSource<ViewModel.FacetGroup>!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.tableView.dataSource = nil
+        dataSource = RxTableViewSectionedAnimatedDataSource(configureCell: { _, tableView, indexPath, item in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Facet", for: indexPath)
+            if let facetCell = cell as? FacetTableVieCell {
+                // space compensates for slanted font, gets clipped otherwise
+                facetCell.textLabel?.text = " " + item.facet.description
+                let addr = item.facet.address.description
+                let range = addr.startIndex ..< addr.index(addr.startIndex, offsetBy: 10)
+                facetCell.detailTextLabel?.text = addr[range] + "..."
+            }
+            return cell
+        })
+        connection.bind(\Props.facetGroups, to: tableView.rx.items(dataSource: dataSource))
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        connection.connect()
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        connection.disconnect()
+    }
 
     @IBAction func done(_ sender: UIBarButtonItem) {
         dismiss(animated: true)
     }
 
+    struct Props {
+        let facetGroups: [ViewModel.FacetGroup]
+    }
+    struct Actions {}
+
+    let connection = Connection(store: appStore,
+                                mapStateToProps: mapStateToProps,
+                                mapDispatchToActions: mapDispatchToActions)
 }
 
+private func mapStateToProps(state: AppState) -> IdentityManagerViewController.Props {
+    let facets = state.diamond.facets.values.map { IdentityManagerViewController.ViewModel.Facet(facet: $0) }
+    let groups = [IdentityManagerViewController.ViewModel.FacetGroup(items: facets)]
+    return IdentityManagerViewController.Props(facetGroups: groups)
+}
+
+private func mapDispatchToActions(dispatch: @escaping DispatchFunction) -> IdentityManagerViewController.Actions {
+    return IdentityManagerViewController.Actions()
+}
+
+typealias CogitoIdentity = Identity
+
 extension IdentityManagerViewController {
-    private struct FacetGroup {
+    struct ViewModel {
+        // swiftlint:disable nesting
+
+        struct FacetGroup: AnimatableSectionModelType, IdentifiableType, Equatable {
+            typealias Identity = String
+            typealias Item = Facet
+
+            var identity = "only one"
+            var items: [Facet]
+
+            init(items: [Facet]) {
+                self.items = items
+            }
+
+            init(original: FacetGroup, items: [Facet]) {
+                self = original
+                self.items = items
+            }
+
+            static func == (lhs: FacetGroup, rhs: FacetGroup) -> Bool {
+                return lhs.items == rhs.items
+            }
+        }
+
+        struct Facet: IdentifiableType, Equatable {
+            typealias Identity = UUID
+            var identity: UUID { return facet.identifier }
+            var facet: CogitoIdentity
+
+            static func == (lhs: Facet, rhs: Facet) -> Bool {
+                return lhs.facet == rhs.facet
+            }
+        }
     }
 }
