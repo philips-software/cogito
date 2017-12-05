8d0d47506ccea026df53048c1f9e5cd554354e00
Mark Spanbroek <mark@spanbroek.net>
2017-12-05 13:44:47 +0100

    Update Cogito provider for use with web3 1.0.0 beta
    Use 'send' instead of 'sendAsync'. Bind 'send' method because this version
    of web3 doesn't call with the correct `this` pointer.

--- test/provider.js
+++ test/provider.js
@@ -25,76 +25,88 @@ describe('provider', function () {
     web3 = new Web3(cogitoProvider)
   })
 
-  context('when cogito provides accounts', function () {
-    const accounts = [ '0x1234', '0xabcd' ]
+  describe('accounts', function () {
+    const accounts = [
+      '0x1234567890123456789012345678901234567890',
+      '0x0123456789012345678901234567890123456789'
+    ]
 
-    beforeEach(function () {
-      const request = JSON.stringify({ method: 'accounts' })
-      const response = JSON.stringify({ result: accounts })
-      td.when(telepathChannel.send(request)).thenDo(function () {
-        td.when(telepathChannel.receive(), { times: 1 }).thenResolve(response)
+    context('when cogito provides accounts', function () {
+      beforeEach(function () {
+        const request = JSON.stringify({ method: 'accounts' })
+        const response = JSON.stringify({ result: accounts })
+        td.when(telepathChannel.send(request)).thenDo(function () {
+          td.when(telepathChannel.receive(), { times: 1 }).thenResolve(response)
+        })
+      })
+
+      it('returns the cogito accounts', function (done) {
+        web3.eth.getAccounts(function (_, result) {
+          expect(result).to.eql(accounts)
+          done()
+        })
       })
     })
 
-    it('returns the cogito accounts', function (done) {
-      web3.eth.getAccounts(function (_, result) {
-        expect(result).to.eql(accounts)
+    it('throws when requesting accounts via telepath fails', function (done) {
+      td.when(telepathChannel.send(anything())).thenReject(new Error('an error'))
+      web3.eth.getAccounts(function (error, _) {
+        expect(error).to.not.be.null()
         done()
       })
     })
   })
 
-  it('throws when requesting accounts via telepath fails', function (done) {
-    td.when(telepathChannel.send(anything())).thenReject(new Error('an error'))
-    web3.eth.getAccounts(function (error, _) {
-      expect(error).to.not.be.null()
-      done()
-    })
-  })
+  describe('signed transactions', function () {
+    const transaction = { from: '0x1234567890123456789012345678901234567890', gasPrice: '0x123' }
 
-  context('when cogito provides signatures', function () {
-    const transaction = { from: '0x1234567890123456789012345678901234567890' }
-    const signed = '0xSignedTransaction'
-
-    beforeEach(function () {
-      const request = JSON.stringify({ method: 'sign', params: [ transaction ] })
-      const response = JSON.stringify({ result: signed })
-      td.when(telepathChannel.send(request)).thenDo(function () {
-        td.when(telepathChannel.receive(), { times: 1 }).thenResolve(response)
-      })
-    })
-
-    context('when using original provider for raw transactions', function () {
-      const hash = '0xTransactionHash'
+    context('when cogito provides signatures', function () {
+      const signed = '0xSignedTransaction'
 
       beforeEach(function () {
-        const request = { method: 'eth_sendRawTransaction', params: [ signed ] }
-        const response = { jsonrpc: '2.0', result: hash, id: 1 }
-        td.when(originalProvider.sendAsync(contains(request)))
-          .thenCallback(null, response)
+        const request = JSON.stringify({ method: 'sign', params: [ transaction ] })
+        const response = JSON.stringify({ result: signed })
+        td.when(telepathChannel.send(request)).thenDo(function () {
+          td.when(telepathChannel.receive(), { times: 1 }).thenResolve(response)
+        })
       })
 
-      it('sends a cogito signed transaction', function (done) {
-        web3.eth.sendTransaction(transaction, function (_, result) {
-          expect(result).to.equal(hash)
-          done()
+      context('when using original provider for raw transactions', function () {
+        const hash = '0xTransactionHash'
+
+        beforeEach(function () {
+          const sendRaw = { method: 'eth_sendRawTransaction', params: [ signed ] }
+          td.when(originalProvider.send(contains(sendRaw), anything()))
+            .thenDo(function (request, callback) {
+              const response = { jsonrpc: '2.0', result: hash, id: request.id }
+              callback(null, response)
+            })
+        })
+
+        it('sends a cogito signed transaction', function (done) {
+          web3.eth.sendTransaction(transaction, function (_, result) {
+            expect(result).to.equal(hash)
+            done()
+          })
         })
       })
     })
-  })
 
-  it('throws when signing via telepath fails', function (done) {
-    const transaction = { from: '0x1234567890123456789012345678901234567890' }
-    td.when(telepathChannel.send(anything())).thenReject(new Error('an error'))
-    web3.eth.sendTransaction(transaction, function (error, _) {
-      expect(error).to.not.be.null()
-      done()
+    it('throws when signing via telepath fails', function (done) {
+      td.when(telepathChannel.send(anything())).thenReject(new Error('an error'))
+      web3.eth.sendTransaction(transaction, function (error, _) {
+        expect(error).to.not.be.null()
+        done()
+      })
     })
   })
 
   it('passes requests to the original provider', function (done) {
-    td.when(originalProvider.sendAsync(anything()))
-      .thenCallback(null, { jsonrpc: '2.0', result: 42, id: 1 })
+    td.when(originalProvider.send(anything(), anything()))
+      .thenDo(function (request, callback) {
+        const response = { jsonrpc: '2.0', result: 42, id: request.id }
+        callback(null, response)
+      })
     web3.eth.getBlockNumber(function (_, result) {
       expect(result).to.equal(42)
       done()
