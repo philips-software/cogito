dbb707416e7616c586339639f10842b9af7dfb5d
Mark Spanbroek <mark@spanbroek.net>
2017-12-12 13:12:06 +0100

    Use base64url encoding instead of binary buffers to communicate with the queuing service

--- test/queuing-service.js
+++ test/queuing-service.js
@@ -1,12 +1,14 @@
 /* eslint-env mocha */
 const expect = require('chai').expect
 const nock = require('nock')
+const base64url = require('base64url')
 const QueuingService = require('../lib/queuing-service')
 
 describe('Queuing Service', function () {
   const baseUrl = 'https://queuing.example.com'
   const queueId = 'a_queue_id'
-  const message = 'a message'
+  const message = Buffer.from('a message')
+  const encodedMessage = base64url.encode(message)
 
   let queuing
 
@@ -19,29 +21,28 @@ describe('Queuing Service', function () {
   })
 
   it('can send a message', async function () {
-    const post = nock(`${baseUrl}`).post(`/${queueId}`, message).reply(200)
+    const post = nock(baseUrl).post(`/${queueId}`, encodedMessage).reply(200)
     await queuing.send(queueId, message)
     expect(post.isDone()).to.be.true()
   })
 
   it('throws when sending fails', async function () {
-    nock(`${baseUrl}`).post(`/${queueId}`, message).reply(500)
+    nock(baseUrl).post(`/${queueId}`, encodedMessage).reply(500)
     await expect(queuing.send(queueId, message)).to.be.rejected()
   })
 
   it('can receive a message', async function () {
-    nock(`${baseUrl}`).get(`/${queueId}`).reply(200, Buffer.from(message))
-    await expect(queuing.receive(queueId))
-      .to.eventually.eql(Buffer.from(message))
+    nock(baseUrl).get(`/${queueId}`).reply(200, encodedMessage)
+    await expect(queuing.receive(queueId)).to.eventually.eql(message)
   })
 
   it('returns null when queue is empty', async function () {
-    nock(`${baseUrl}`).get(`/${queueId}`).reply(204)
+    nock(baseUrl).get(`/${queueId}`).reply(204)
     await expect(queuing.receive(queueId)).to.eventually.be.null()
   })
 
   it('throws when receiving fails', async function () {
-    nock(`${baseUrl}`).get(`/${queueId}`).reply(500)
+    nock(baseUrl).get(`/${queueId}`).reply(500)
     await expect(queuing.receive(queueId)).to.be.rejected()
   })
 })
