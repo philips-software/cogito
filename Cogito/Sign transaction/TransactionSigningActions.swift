037554d263a1ef8a4bd67f206d1cfd41cb949e6c
Mark Spanbroek <mark@spanbroek.net>
2017-12-18 15:30:15 +0100

    Introduce TransactionSigningService

---
+++ Cogito/Sign transaction/TransactionSigningActions.swift
@@ -0,0 +1,12 @@
+//  Copyright © 2017 Koninklijke Philips Nederland N.V. All rights reserved.
+
+import ReSwiftThunk
+
+struct TransactionSigningActions {
+    // swiftlint:disable:next identifier_name
+    static func Sign() -> ThunkAction<AppState> {
+        return ThunkAction(action: { _, _ in
+
+        })
+    }
+}
