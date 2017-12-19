9c95551690068a790e2a9fdc3de1539a9d45bae4
Mark Spanbroek <mark@spanbroek.net>
2017-12-19 11:11:23 +0100

    Refactor: remove duplication from tests

--- CogitoTests/Sign transaction/TransactionSigningServiceSpec.swift
+++ CogitoTests/Sign transaction/TransactionSigningServiceSpec.swift
@@ -6,6 +6,8 @@ import ReSwiftThunk
 
 class TransactionSigningServiceSpec: QuickSpec {
     override func spec() {
+        let signRequest = "{\"method\":\"signTransaction\",\"params\":{}}"
+
         var service: TransactionSigningService!
         var store: StoreSpy!
 
@@ -14,67 +16,13 @@ class TransactionSigningServiceSpec: QuickSpec {
             service = TransactionSigningService(store: store)
         }
 
-        it("subscribes to incoming Telepath messages") {
-            service.start()
-            expect(store.latestSubscriber) === service
-        }
-
-        it("unsubscribes") {
-            service.stop()
-            expect(store.latestUnsubscriber) === service
-        }
-
-        describe("incoming messages") {
-            let signRequest = "{\"method\":\"signTransaction\",\"params\":{}}"
-            let otherRequest = "{\"method\":\"other\"}"
-
-            func whenReceiving(messages: [String]) {
-                beforeEach {
-                    service.newState(state: messages)
-                }
+        context("when a sign requests comes in") {
+            beforeEach {
+                service.newState(state: [signRequest])
             }
 
-            func itDispatchesSignTransaction() {
-                it("dispatches the SignTransaction action") {
-                    expect(store.actions.last as? ThunkAction<AppState>)
-                        .toNot(beNil())
-                }
-            }
-
-            func itDoesNotDispatchSignTransaction() {
-                it("does not dispatch the GetAccount action") {
-                    expect(store.actions.last as? ThunkAction<AppState>)
-                        .to(beNil())
-                }
-            }
-
-            func itHandlesTheMessage() {
-                it("handles the message") {
-                    expect(store.actions).to(containElementSatisfying({
-                        $0 is TelepathActions.ReceivedMessageHandled
-                    }))
-                }
-            }
-
-            context("when an sign requests comes in") {
-                whenReceiving(messages: [signRequest])
-                itHandlesTheMessage()
-                itDispatchesSignTransaction()
-            }
-
-            context("when a different request comes in") {
-                whenReceiving(messages: [otherRequest])
-                itDoesNotDispatchSignTransaction()
-            }
-
-            context("when the list of messages is empty") {
-                whenReceiving(messages: [])
-                itDoesNotDispatchSignTransaction()
-            }
-
-            context("when the account request is not the oldest message") {
-                whenReceiving(messages: [otherRequest, signRequest])
-                itDoesNotDispatchSignTransaction()
+            it("dispatches the SignTransaction action") {
+                expect(store.actions.last as? ThunkAction<AppState>).toNot(beNil())
             }
         }
     }
