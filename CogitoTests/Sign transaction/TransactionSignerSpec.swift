873bc3f340bc8e697336d8625bb2d6227fff5520
Stefan van den Oord <stefan.van.den.oord@philips.com>
2018-01-11 16:07:02 +0100

    Added new files that are going to hold the transaction signing logic

---
+++ CogitoTests/Sign transaction/TransactionSignerSpec.swift
@@ -0,0 +1,9 @@
+//  Copyright © 2018 Koninklijke Philips Nederland N.V. All rights reserved.
+
+import Quick
+import Nimble
+
+class TrnasactionSignerSpec: QuickSpec {
+    override func spec() {
+    }
+}
