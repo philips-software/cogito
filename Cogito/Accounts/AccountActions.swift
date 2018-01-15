84df1656181eae337ec4f7d78042a82af5865519
Mark Spanbroek <mark@spanbroek.net>
2018-01-15 15:28:45 +0100

    Use JSON-RPC sending instead of raw message sending

--- Cogito/Accounts/AccountActions.swift
+++ Cogito/Accounts/AccountActions.swift
@@ -7,27 +7,12 @@ struct AccountActions {
     // swiftlint:disable identifier_name
     static func GetAccounts(requestId: JsonRpcId) -> ThunkAction<AppState> {
         return ThunkAction(action: { dispatch, getState in
-            var accounts: [Address] = []
+            var accounts: [String] = []
             if let diamond = getState()?.diamond,
                let selectedFacet = diamond.selectedFacet() {
-                   accounts.append(selectedFacet.address)
+                   accounts.append("\(selectedFacet.address)")
             }
-            let response = AccountsResult(id: requestId, result: accounts)
-            dispatch(TelepathActions.Send(message: response.json))
+            dispatch(TelepathActions.Send(id: requestId, result: accounts))
         })
-    }
-}
-
-private struct AccountsResult {
-    let id: JsonRpcId
-    let result: [Address]
-
-    var json: String {
-        let accountStrings = result.map { account in "\(account)" }
-        return JSON([
-            "jsonrpc": "2.0",
-            "result": accountStrings,
-            "id": id.object
-        ]).rawString()!
     }
 }
