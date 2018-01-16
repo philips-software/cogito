3b2e9920c844b57a649894e01f29e9d02ed35dc7
Stefan van den Oord <stefan.van.den.oord@philips.com>
2018-01-16 09:02:59 +0100

    Store initialized properties and verify them

--- Cogito/Geth/Transaction.swift
+++ Cogito/Geth/Transaction.swift
@@ -3,15 +3,23 @@
 import Foundation
 
 struct Transaction {
+    let from: Address
+    let to: Address // swiftlint:disable:this identifier_name
+    let data: Data
+
     init?(from txDict: [String: Any]) {
-        guard let fromString = txDict["from"] as? String, let _ = Address(fromHex: fromString),
-              let toString = txDict["to"] as? String, let _ = Address(fromHex: toString),
+        guard let fromString = txDict["from"] as? String, let from = Address(fromHex: fromString),
+              let toString = txDict["to"] as? String, let to = Address(fromHex: toString),
               txDict["gasPrice"] != nil,
               txDict["gasLimit"] != nil,
               txDict["value"] != nil,
               txDict["nonce"] != nil,
-              let dataString = txDict["data"] as? String, let _ = Data(fromHex: dataString) else {
+              let dataString = txDict["data"] as? String, let data = Data(fromHex: dataString) else {
             return nil
         }
+
+        self.from = from
+        self.to = to
+        self.data = data
     }
 }
