2dbe6ebc7dc71235bf81a962aa563225d14dc285
Stefan van den Oord <stefan.van.den.oord@philips.com>
2018-02-19 16:46:28 +0100

    Dispatch delete action on swipe-to-delete

--- CogitoTests/Identity manager/IdentityManagerViewControllerSpec.swift
+++ CogitoTests/Identity manager/IdentityManagerViewControllerSpec.swift
@@ -17,7 +17,8 @@ class IdentityManagerViewControllerSpec: QuickSpec {
         it("resets create identity when showing the create identity form") {
             var reset = false
             identityManagerController.actions = IdentityManagerViewController.Actions(
-                resetCreateIdentity: { reset = true }
+                resetCreateIdentity: { reset = true },
+                deleteIdentity: { _ in }
             )
             let createIdentityController = CreateIdentityViewController()
             let segue = UIStoryboardSegue(identifier: "CreateIdentity",
@@ -26,6 +27,25 @@ class IdentityManagerViewControllerSpec: QuickSpec {
             identityManagerController.prepare(for: segue, sender: nil)
             createIdentityController.onDone()
             expect(reset).toEventually(beTrue())
+        }
+
+        it("triggers delete identity action") {
+            let testAddress = Address.testAddress
+            let testIdentity = Identity(description: "testdesc",
+                                        address: testAddress)
+            let model = [IdentityManagerViewController.ViewModel.FacetGroup(items: [
+                IdentityManagerViewController.ViewModel.Facet(facet: testIdentity)
+            ])]
+            identityManagerController.props = IdentityManagerViewController.Props(facetGroups: model)
+            var deletedUuid: UUID? = nil
+            identityManagerController.actions = IdentityManagerViewController.Actions(
+                resetCreateIdentity: {},
+                deleteIdentity: { uuid in deletedUuid = uuid }
+            )
+            identityManagerController.tableView.dataSource?.tableView?(identityManagerController.tableView,
+                                                           commit: .delete,
+                                                           forRowAt: IndexPath(row: 0, section: 0))
+            expect(deletedUuid) == testIdentity.identifier
         }
 
         describe("Map dispatch to actions") {
@@ -39,6 +59,12 @@ class IdentityManagerViewControllerSpec: QuickSpec {
             it("maps resetCreateIdentity") {
                 identityManagerController.actions.resetCreateIdentity()
                 expect(store.firstAction(ofType: CreateIdentityActions.ResetForm.self)).toNot(beNil())
+            }
+
+            it("maps deleteIdentity") {
+                let uuid = UUID()
+                identityManagerController.actions.deleteIdentity(uuid)
+                expect(store.firstAction(ofType: DiamondActions.DeleteFacet.self)?.uuid) == uuid
             }
         }
     }
