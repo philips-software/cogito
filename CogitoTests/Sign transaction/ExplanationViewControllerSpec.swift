fcb5e34c9a6ae587b2ba2c7b9bf0669bffabe0ef
Stefan van den Oord <stefan.van.den.oord@philips.com>
2018-02-20 16:03:50 +0100

    Fixed tests

--- CogitoTests/Sign transaction/ExplanationViewControllerSpec.swift
+++ CogitoTests/Sign transaction/ExplanationViewControllerSpec.swift
@@ -22,83 +22,13 @@ class ExplanationViewControllerSpec: QuickSpec {
             beforeEach {
                 viewController.props = ExplanationViewController.Props(
                     appName: appName,
-                    actionDescription: actionDescription,
-                    organization: nil,
-                    hasValidAttestation: nil
+                    actionDescription: actionDescription
                 )
             }
 
             it("shows explanatory text") {
-                let expectedText = "\(appName) requires your signature to \(actionDescription)."
+                let expectedText = "\(appName) requests your signature to \(actionDescription)."
                 expect(viewController.explanationLabel.text) == expectedText
-            }
-
-            it("does not show startAttestation button") {
-                expect(viewController.startAttestationButton.isHidden).to(beTrue())
-            }
-
-            it("configures the sign and reject buttons") {
-                expect(viewController.signButton.isEnabled).to(beTrue())
-                expect(viewController.signButton.backgroundColor) == ExplanationViewController.signColor
-                expect(viewController.rejectButton.isEnabled).to(beTrue())
-                expect(viewController.rejectButton.backgroundColor) == ExplanationViewController.rejectColor
-            }
-        }
-
-        context("when attestation is needed") {
-            let appName = "Test App"
-            let actionDescription = "perform some test action"
-            let organization = "Philips"
-
-            beforeEach {
-                viewController.props = ExplanationViewController.Props(
-                    appName: appName,
-                    actionDescription: actionDescription,
-                    organization: organization,
-                    hasValidAttestation: false
-                )
-            }
-
-            it("shows requirement to prove identity with IAM") {
-                let expectedText = """
-                                   \(appName) requires your signature to \(actionDescription).
-
-                                   In addition, \(appName) requires proof that you have an account with \(organization).
-                                   """
-                expect(viewController.explanationLabel.text) == expectedText
-            }
-
-            it("shows startAttestation button") {
-                expect(viewController.startAttestationButton.isHidden).to(beFalse())
-            }
-
-            it("configures the sign and reject buttons") {
-                expect(viewController.signButton.isEnabled).to(beFalse())
-                expect(viewController.signButton.backgroundColor) == ExplanationViewController.signDisabledColor
-                expect(viewController.rejectButton.isEnabled).to(beFalse())
-                expect(viewController.rejectButton.backgroundColor) == ExplanationViewController.rejectDisabledColor
-            }
-
-            context("when attestation available") {
-                beforeEach {
-                    viewController.props = ExplanationViewController.Props(
-                        appName: appName,
-                        actionDescription: actionDescription,
-                        organization: organization,
-                        hasValidAttestation: true
-                    )
-                }
-
-                it("does not show startAttestation button") {
-                    expect(viewController.startAttestationButton.isHidden).to(beTrue())
-                }
-
-                it("configures the sign and reject buttons") {
-                    expect(viewController.signButton.isEnabled).to(beTrue())
-                    expect(viewController.signButton.backgroundColor) == ExplanationViewController.signColor
-                    expect(viewController.rejectButton.isEnabled).to(beTrue())
-                    expect(viewController.rejectButton.backgroundColor) == ExplanationViewController.rejectColor
-                }
             }
         }
     }
