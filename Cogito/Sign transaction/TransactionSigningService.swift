72696189ef73d841511c8c2895479f99f56615b5
Stefan van den Oord <stefan.van.den.oord@philips.com>
2018-02-20 15:14:24 +0100

    Take the transaction parameters from the JSON RPC data

--- Cogito/Sign transaction/TransactionSigningService.swift
+++ Cogito/Sign transaction/TransactionSigningService.swift
@@ -1,15 +1,17 @@
 //  Copyright © 2017 Koninklijke Philips Nederland N.V. All rights reserved.
 
 import ReSwift
+import SwiftyJSON
 
 struct TransactionSigningService: TelepathService {
     let store: Store<AppState>
 
     func onRequest(_ request: JsonRpcRequest) {
-        guard request.method == "sign" else {
+        guard request.method == "sign",
+              let txDict = request.params[0].dictionaryObject else {
             return
         }
-        store.dispatch(TransactionSigningActions.Sign(tx: [:],
-                                                      responseId: request.id)) // todo: take from request
+        store.dispatch(TransactionSigningActions.Sign(tx: txDict,
+                                                      responseId: request.id))
     }
 }
