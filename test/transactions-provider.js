24a91c33589f26a77aaa5ffeb619c90dee926703
Mark Spanbroek <mark@spanbroek.net>
2018-02-22 10:28:16 +0100

    Handle cogito errors

--- test/transactions-provider.js
+++ test/transactions-provider.js
@@ -61,6 +61,15 @@ describe('sending transactions', function () {
     })
   })
 
+  it('throws when cogito returns an error', function (done) {
+    const response = { error: { message: 'some error', code: -42 } }
+    td.when(telepathChannel.send(anything())).thenResolve(response)
+    web3.eth.sendTransaction(transaction, function (error, _) {
+      expect(error).to.not.be.null()
+      done()
+    })
+  })
+
   it('sets transaction defaults', function (done) {
     const transactionWithDefaults = Object.assign({ value: '0x0' }, transaction)
     const expectedRequest = { method: 'sign', params: [transactionWithDefaults] }
