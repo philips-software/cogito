7b2ae4ad8149ef5cf85cb70cd0b284b1ad82307e
Czenko, Marcin <marcin.czenko@philips.com>
2018-02-23 16:59:21 +0100

    Making actual transpilation

--- distribution/lib/delay.js
+++ distribution/lib/delay.js
@@ -1,9 +1,30 @@
 "use strict";
 
-async function delay(milliseconds) {
-  return new Promise(function (resolve) {
-    setTimeout(() => resolve(), milliseconds);
-  });
-}
+var delay = function () {
+  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(milliseconds) {
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            return _context.abrupt("return", new Promise(function (resolve) {
+              setTimeout(function () {
+                return resolve();
+              }, milliseconds);
+            }));
+
+          case 1:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee, this);
+  }));
+
+  return function delay(_x) {
+    return _ref.apply(this, arguments);
+  };
+}();
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
 module.exports = delay;
