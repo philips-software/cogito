b205d1a90cd7907290d7b843aa42981e172daf3c
Marcin Czenko <marcin@everydayproductive.com>
2018-03-09 10:45:27 +0100

    accounts-provider tests rewritten

--- packages/cogito-web3/test/accounts-provider.js
+++ packages/cogito-web3/test/accounts-provider.js
@@ -1,9 +1,5 @@
-const expect = require('chai').expect
-const td = require('testdouble')
-const anything = td.matchers.anything
-const contains = td.matchers.contains
-const Web3 = require('web3')
-const CogitoProvider = require('../source/lib/cogito-provider')
+import Web3 from 'web3'
+import CogitoProvider from '../source/lib/cogito-provider'
 
 describe('accounts', function () {
   const accounts = [
@@ -15,32 +11,47 @@ describe('accounts', function () {
   let telepathChannel
   let web3
 
-  beforeEach(function () {
-    telepathChannel = td.object()
-    cogitoProvider = new CogitoProvider({ telepathChannel })
-    web3 = new Web3(cogitoProvider)
-  })
-
-  context('when cogito provides accounts', function () {
-    beforeEach(function () {
-      const request = { method: 'accounts' }
-      const response = { result: accounts }
-      td.when(telepathChannel.send(contains(request))).thenResolve(response)
+  describe('when cogito provides accounts', () => {
+    const request = { method: 'accounts' }
+    const response = { result: accounts }
+    beforeEach(() => {
+      telepathChannel = {
+        send: jest.fn().mockReturnValue(Promise.resolve(response))
+      }
+      cogitoProvider = new CogitoProvider({ telepathChannel })
+      web3 = new Web3(cogitoProvider)
     })
 
-    it('returns the cogito accounts', function (done) {
-      web3.eth.getAccounts(function (_, result) {
-        expect(result).to.eql(accounts)
+    it('call telepath send with proper request argument', (done) => {
+      web3.eth.getAccounts((_, result) => {
+        expect(telepathChannel.send.mock.calls[0][0]).toMatchObject(request)
+        done()
+      })
+    })
+
+    it('returns the cogito accounts', (done) => {
+      web3.eth.getAccounts((_, result) => {
+        expect(result).toEqual(accounts)
         done()
       })
     })
   })
 
-  it('throws when requesting accounts via telepath fails', function (done) {
-    td.when(telepathChannel.send(anything())).thenReject(new Error('an error'))
-    web3.eth.getAccounts(function (error, _) {
-      expect(error).to.not.be.null()
-      done()
+  describe('on failure', () => {
+    const error = new Error('an error')
+    beforeEach(() => {
+      telepathChannel = {
+        send: jest.fn().mockReturnValue(Promise.reject(error))
+      }
+      cogitoProvider = new CogitoProvider({ telepathChannel })
+      web3 = new Web3(cogitoProvider)
+    })
+
+    it('throws when requesting accounts via telepath fails', (done) => {
+      web3.eth.getAccounts((error, _) => {
+        expect(error).toEqual(error)
+        done()
+      })
     })
   })
 })
