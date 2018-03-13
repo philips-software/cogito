fc5e865b769f68ae5f4496ac4e880d85dbc1f088
Czenko, Marcin <marcin.czenko@philips.com>
2018-03-13 17:10:02 +0100

    Distribution updated

--- packages/cogito-web3/distribution/index.js
+++ packages/cogito-web3/distribution/index.js
@@ -1,3 +1,13 @@
-var CogitoProvider = require('./lib/cogito-provider');
+"use strict";
 
-module.exports = CogitoProvider;
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "CogitoProvider", {
+  enumerable: true,
+  get: function get() {
+    return _cogitoProvider.CogitoProvider;
+  }
+});
+
+var _cogitoProvider = require("./cogito-provider");
