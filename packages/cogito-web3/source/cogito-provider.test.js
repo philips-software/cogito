c270fb4ca85ce7c6e578ef297bb0f5dbb383a2ce
Marcin Czenko <marcin@everydayproductive.com>
2018-03-13 17:09:07 +0100

    cogito-web3: tests moved to source directory

--- packages/cogito-web3/source/cogito-provider.test.js
+++ packages/cogito-web3/source/cogito-provider.test.js
@@ -1,5 +1,5 @@
 import Web3 from 'web3'
-import { CogitoProvider } from '../source/cogito-provider'
+import { CogitoProvider } from './cogito-provider'
 
 describe('provider', () => {
   let cogitoProvider
