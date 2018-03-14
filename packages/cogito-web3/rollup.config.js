223b6462c789ccf42b2da7a71d6dcad4a0b036ca
Czenko, Marcin <marcin.czenko@philips.com>
2018-03-14 18:04:13 +0100

    Updated babel, created new build configuration for cogito-web3 including 'commonjs', 'es', and 'umd' modules

---
+++ packages/cogito-web3/rollup.config.js
@@ -0,0 +1,29 @@
+import babel from 'rollup-plugin-babel'
+import uglify from 'rollup-plugin-uglify'
+import replace from 'rollup-plugin-replace'
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
+
+const config = {
+  input: 'source/index.js',
+  name: 'cogito-web3',
+  external: ['web3'],
+  plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    resolve(),
+    commonjs({
+      include: /node_modules/
+    })
+  ]
+}
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(uglify())
+}
+
+export default config
