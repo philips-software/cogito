01b4b4f7fda3e66829cee01127a0ce7a93eaf31a
Mark Spanbroek <mark@spanbroek.net>
2018-03-28 15:40:37 +0200

    Extract callback URL for OpenID into a separate Configuration struct
    Purpose is to provide an easy way to configure the associated domain,
    that can also be referenced to from the documentation.

---
+++ packages/cogito-ios-app/Cogito/Configuration.swift
@@ -0,0 +1,5 @@
+//  Copyright © 2018 Koninklijke Philips Nederland N.V. All rights reserved.
+
+struct Configuration {
+    static let openIdCallbackUrl = "https://cogito.mobi/applinks/openid-callback"
+}
