1ce5b98c006ae8258e7f0057a3428025e60d8655
Marcin Czenko <marcin@everydayproductive.com>
2018-04-14 00:40:05 +0200

    Adds demo-app to validate that the setup is correct

---
+++ workspaces/demo-app/src/pages/home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { HomeGrid, SidebarGridItem, ContentGridItem } from './HomeGrid'
+
+const Home = () =>
+  <HomeGrid>
+    <SidebarGridItem>
+      SidebarGridItem
+    </SidebarGridItem>
+    <ContentGridItem>
+      ContentGridItem
+    </ContentGridItem>
+  </HomeGrid>
+
+export { Home }
