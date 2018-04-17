9cf1dcda727ca47a5b4b44ba4b16e53be7df35c3
Stefan van den Oord <stefan.van.den.oord@philips.com>
2018-04-17 15:09:45 +0200

    Verify that the requested tag is part of the correct identity

--- workspaces/cogito-ios-app/CogitoTests/Encryption/EncryptionServiceSpec.swift
+++ workspaces/cogito-ios-app/CogitoTests/Encryption/EncryptionServiceSpec.swift
@@ -8,17 +8,20 @@ class EncryptionServiceSpec: QuickSpec {
         var service: EncryptionService!
         var store: RecordingStore!
         var channel: TelepathChannel!
-        let identity = Identity(description: "example", address: Address.testAddress)
+        var identity: Identity!
 
         beforeEach {
             store = RecordingStore()
             service = EncryptionService(store: store)
             channel = TelepathChannelSpy()
-            store.state = appState(telepath: TelepathState(channels: [channel: identity]))
+            identity = Identity(description: "example", address: Address.testAddress)
         }
 
         context("when a create encryption key pair request comes in") {
             beforeEach {
+                store.state = appState(
+                    telepath: TelepathState(channels: [channel: identity])
+                )
                 let request = JsonRpcRequest(
                     id: JsonRpcId(1),
                     method: "createEncryptionKeyPair",
@@ -50,6 +53,10 @@ class EncryptionServiceSpec: QuickSpec {
 
             context("when the public key is available") {
                 beforeEach {
+                    identity.encryptionKeyPairs = [tag]
+                    store.state = appState(
+                        telepath: TelepathState(channels: [channel: identity])
+                    )
                     service.publicKeyLoader = { requestedTag in
                         if requestedTag == tag {
                             return publicKey
@@ -84,6 +91,10 @@ class EncryptionServiceSpec: QuickSpec {
 
             context("when the public key is not available") {
                 beforeEach {
+                    identity.encryptionKeyPairs = [tag]
+                    store.state = appState(
+                        telepath: TelepathState(channels: [channel: identity])
+                    )
                     service.publicKeyLoader = { _ in return nil }
                 }
 
@@ -99,7 +110,26 @@ class EncryptionServiceSpec: QuickSpec {
                 }
             }
 
-            // TODO: check that correct identity is used
+            context("when the identity does not contain the key pair") {
+                beforeEach {
+                    identity.encryptionKeyPairs = []
+                    store.state = appState(
+                        telepath: TelepathState(channels: [channel: identity])
+                    )
+                    service.publicKeyLoader = { _ in return publicKey }
+                }
+
+                it("returns an error") {
+                    let request = JsonRpcRequest(
+                        id: JsonRpcId(1),
+                        method: "getEncryptionPublicKey",
+                        params: JsonRpcParams(["tag": tag])
+                    )
+                    service.onRequest(request, on: channel)
+                    let sendPendingAction = store.firstAction(ofType: TelepathActions.SendPending.self)
+                    expect(sendPendingAction?.message).to(contain("\"code\" : \(EncryptionError.keyNotFound.rawValue)"))
+                }
+            }
         }
 
         context("when another request comes in") {
