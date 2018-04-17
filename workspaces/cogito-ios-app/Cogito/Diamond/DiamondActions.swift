97d4543757e7b008c03bcaf9dce94963b4eb67cb
Stefan van den Oord <stefan.van.den.oord@philips.com>
2018-04-17 16:26:24 +0200

    Moved KeyPairCreator to separate file

--- workspaces/cogito-ios-app/Cogito/Diamond/DiamondActions.swift
+++ workspaces/cogito-ios-app/Cogito/Diamond/DiamondActions.swift
@@ -2,7 +2,6 @@
 
 import ReSwift
 import Geth
-import Security
 
 struct DiamondActions {
     struct CreateFacet: Action {
@@ -27,40 +26,10 @@ struct DiamondActions {
         let identity: Identity
         let tag: String
 
-        init(identity: Identity, keyPairCreateFunction: @escaping KeyPairCreateFunction = SecKeyCreateRandomKey) {
+        init(identity: Identity) {
             self.identity = identity
             self.tag = UUID().uuidString
-            var creator = KeyPairCreator()
-            creator.keyPairCreateFunction = keyPairCreateFunction
-            creator.create(tag: tag)
+            KeyPairCreator().create(tag: tag)
         }
     }
 }
-
-struct KeyPairCreator {
-    var keyPairCreateFunction: KeyPairCreateFunction = SecKeyCreateRandomKey
-
-    func create(tag: String) {
-        let accessFlags = SecAccessControlCreateWithFlags(kCFAllocatorDefault,
-                                                          kSecAttrAccessibleAfterFirstUnlock,
-                                                          .userPresence,
-                                                          nil)!
-        let parameters: [String:Any] = [
-            kSecAttrKeyType as String: kSecAttrKeyTypeRSA,
-            kSecAttrKeySizeInBits as String: 2048,
-            kSecPrivateKeyAttrs as String: [
-                kSecAttrIsPermanent as String: true,
-                kSecAttrCanEncrypt as String: true,
-                kSecAttrCanDecrypt as String: true,
-                kSecAttrApplicationTag as String: tag.data(using: .utf8)!,
-                kSecAttrAccessControl as String: accessFlags
-            ]
-        ]
-        _ = keyPairCreateFunction(parameters as CFDictionary, nil)
-    }
-}
-
-typealias KeyPairCreateFunction = (
-    _ parameters: CFDictionary,
-    _ error: UnsafeMutablePointer<Unmanaged<CFError>?>?
-    ) -> SecKey?
