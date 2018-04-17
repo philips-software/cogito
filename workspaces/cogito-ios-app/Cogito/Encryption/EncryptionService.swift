bf2027bbe26ed0b6b27de9966c93896d300a3545
Stefan van den Oord <stefan.van.den.oord@philips.com>
2018-04-17 14:10:30 +0200

    Added plumbing for getting public keys

--- workspaces/cogito-ios-app/Cogito/Encryption/EncryptionService.swift
+++ workspaces/cogito-ios-app/Cogito/Encryption/EncryptionService.swift
@@ -1,21 +1,49 @@
-//  Copyright © 2018 Koninklijke Philips Nederland N.V. All rights reserved.
+// Copyright © 2018 Koninklijke Philips Nederland N.V. All rights reserved.
 
 import Foundation
 import ReSwift
 
 struct EncryptionService: TelepathService {
     let store: Store<AppState>
-    func onRequest(_ request: JsonRpcRequest, on channel: TelepathChannel) {
-        guard request.method == "createEncryptionKeyPair" else { return }
+    var publicKeyLoader: ((String) -> PublicKey?)!
 
+    init(store: Store<AppState>) {
+        self.store = store
+        self.publicKeyLoader = loadPublicKeyFromKeychain
+    }
+
+    func onRequest(_ request: JsonRpcRequest, on channel: TelepathChannel) {
         if let identity = store.state.telepath.channels[channel] {
-            let createKeyPairAction = DiamondActions.CreateEncryptionKeyPair(identity: identity)
-            store.dispatch(createKeyPairAction)
-            store.dispatch(TelepathActions.Send(
-                id: request.id,
-                result: createKeyPairAction.tag,
-                on: channel
-            ))
+            switch request.method {
+            case "createEncryptionKeyPair":
+                createKeyPair(request: request, identity: identity, channel: channel)
+            case "getEncryptionPublicKey":
+                requestPublicKey(request: request, identity: identity, channel: channel)
+            default:
+                break
+            }
         }
     }
+
+    private func createKeyPair(request: JsonRpcRequest, identity: Identity, channel: TelepathChannel) {
+        let createKeyPairAction = DiamondActions.CreateEncryptionKeyPair(identity: identity)
+        store.dispatch(createKeyPairAction)
+        store.dispatch(TelepathActions.Send(
+            id: request.id,
+            result: createKeyPairAction.tag,
+            on: channel
+        ))
+    }
+
+    private func requestPublicKey(request: JsonRpcRequest, identity: Identity, channel: TelepathChannel) {
+        let publicKey = publicKeyLoader?(request.params["tag"].stringValue)
+        store.dispatch(TelepathActions.Send(id: request.id, result: publicKey!, on: channel))
+    }
+
+    private func loadPublicKeyFromKeychain(tag: String) -> PublicKey? {
+        assert(false, "to be implemented")
+        return nil
+    }
+
+    typealias PublicKey = Data
 }
