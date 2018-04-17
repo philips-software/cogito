89f77b38588fd54d19835e16b600d1271a3c4bb2
Stefan van den Oord <stefan.van.den.oord@philips.com>
2018-04-17 14:10:30 +0200

    Added plumbing for getting public keys

--- workspaces/cogito-ios-app/CogitoTests/Encryption/EncryptionServiceSpec.swift
+++ workspaces/cogito-ios-app/CogitoTests/Encryption/EncryptionServiceSpec.swift
@@ -44,6 +44,36 @@ class EncryptionServiceSpec: QuickSpec {
             }
         }
 
+        context("when a public key is requested") {
+            let tag = "1234-5678"
+            let publicKey = "some public key".data(using: .utf8)!
+
+            beforeEach {
+                service.publicKeyLoader = { requestedTag in
+                    if requestedTag == tag {
+                        return publicKey
+                    } else {
+                        return nil
+                    }
+                }
+            }
+
+            it("sends response on Telepath channel") {
+                let request = JsonRpcRequest(
+                    id: JsonRpcId(1),
+                    method: "getEncryptionPublicKey",
+                    params: JsonRpcParams(["tag": tag])
+                )
+                service.onRequest(request, on: channel)
+                let sendPendingAction = store.firstAction(ofType: TelepathActions.SendPending.self)
+                expect(sendPendingAction?.message).to(contain(publicKey.hexEncodedString()))
+            }
+
+            // TODO: return error when tag is not present in the request parameters
+            // TODO: return error when public key could not be found
+            // TODO: check that correct identity is used
+        }
+
         context("when another request comes in") {
             it("does not dispatch anything") {
                 let actionCountBefore = store.actions.count
