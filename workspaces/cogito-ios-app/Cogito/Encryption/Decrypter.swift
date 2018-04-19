413d3b004107d4fb274c766b43ba6d21e4d7d27a
Mark Spanbroek <mark@spanbroek.net>
2018-04-19 14:38:31 +0200

    cogito-ios-app: implement decryption

--- workspaces/cogito-ios-app/Cogito/Encryption/Decrypter.swift
+++ workspaces/cogito-ios-app/Cogito/Encryption/Decrypter.swift
@@ -3,9 +3,25 @@
 import Foundation
 
 struct Decrypter: DecrypterType {
-    func decrypt(keyTag: String, cipherText: Data) -> Data? {
-        assert(false, "not implemented yet")
-        return nil
+    func decrypt(keyTag tag: String, cipherText: Data) -> Data? {
+        guard let privateKey = loadPrivateKey(tag: tag) else {
+            return nil
+        }
+
+        return SecKeyCreateDecryptedData(privateKey, .rsaEncryptionOAEPSHA512, cipherText as CFData, nil) as Data?
+    }
+
+    private func loadPrivateKey(tag: String) -> SecKey? {
+        let query: [String:Any] = [
+            kSecClass as String: kSecClassKey,
+            kSecAttrKeyClass as String: kSecAttrKeyClassPrivate,
+            kSecAttrApplicationTag as String: tag.data(using: .utf8)!,
+            kSecMatchLimit as String: kSecMatchLimitOne,
+            kSecReturnRef as String: true
+        ]
+        var queryResult: AnyObject?
+        SecItemCopyMatching(query as CFDictionary, &queryResult)
+        return queryResult as! SecKey? // swiftlint:disable:this force_cast
     }
 }
 
---
+++ workspaces/cogito-ios-app/CogitoTests/Encryption/DecrypterSpec.swift
@@ -0,0 +1,53 @@
+//  Copyright © 2018 Koninklijke Philips Nederland N.V. All rights reserved.
+
+import Quick
+import Nimble
+@testable import Cogito
+
+class DecrypterSepc: QuickSpec {
+    override func spec() {
+        var tag: String!
+        var publicKey: SecKey!
+        var plainText: Data!
+        var cipherText: Data!
+
+        beforeEach {
+            tag = UUID().uuidString
+            KeyPairCreator().create(tag: tag)
+            publicKey = loadPublicKey(tag: tag)
+            plainText = "some data".data(using: .utf8)
+            cipherText = encrypt(key: publicKey, plainText: plainText)
+        }
+
+        it("decrypts data that was encrypted with the public key") {
+            expect(Decrypter().decrypt(keyTag: tag, cipherText: cipherText)) == plainText
+        }
+
+        it("returns nil when key could not be found") {
+            let nonExistent = UUID().uuidString
+            expect(Decrypter().decrypt(keyTag: nonExistent, cipherText: cipherText)).to(beNil())
+        }
+
+        it("returns nil when data could not be decrypted") {
+            let wrongCipherText = "invalid".data(using: .utf8)!
+            expect(Decrypter().decrypt(keyTag: tag, cipherText: wrongCipherText)).to(beNil())
+        }
+    }
+}
+
+private func loadPublicKey(tag: String) -> SecKey? {
+    let query: [String:Any] = [
+        kSecClass as String: kSecClassKey,
+        kSecAttrKeyClass as String: kSecAttrKeyClassPublic,
+        kSecAttrApplicationTag as String: tag.data(using: .utf8)!,
+        kSecReturnRef as String: true
+    ]
+    var queryResult: AnyObject?
+    SecItemCopyMatching(query as CFDictionary, &queryResult)
+    return queryResult as! SecKey? // swiftlint:disable:this force_cast
+}
+
+private func encrypt(key: SecKey, plainText: Data) -> Data? {
+    let plainText = "some data".data(using: .utf8)!
+    return SecKeyCreateEncryptedData(key, .rsaEncryptionOAEPSHA512, plainText as CFData, nil) as Data?
+}
