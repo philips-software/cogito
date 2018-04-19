ecfda0b84821f11f5b2618132560205aaeb3a72a
Mark Spanbroek <mark@spanbroek.net>
2018-04-19 16:25:29 +0200

    Add (empty) workspace for cogito-encryption

---
+++ workspaces/cogito-encryption/rollup.config.js
@@ -0,0 +1,30 @@
+import babel from 'rollup-plugin-babel'
+import uglify from 'rollup-plugin-uglify'
+import replace from 'rollup-plugin-replace'
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
+
+const config = {
+  input: 'source/index.js',
+  output: {
+    name: 'cogito-encryption'
+  },
+  plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    resolve(),
+    commonjs({
+      include: /node_modules/
+    })
+  ]
+}
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(uglify())
+}
+
+export default config
