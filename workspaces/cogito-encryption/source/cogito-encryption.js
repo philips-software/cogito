9ee5b4dc2a7d3b74306bbab256b1009db21343a8
Stefan van den Oord <stefan.van.den.oord@philips.com>
2018-04-20 15:43:35 +0200

    cogito-encryption: decrypt

--- workspaces/cogito-encryption/source/cogito-encryption.js
+++ workspaces/cogito-encryption/source/cogito-encryption.js
@@ -33,6 +33,21 @@ class CogitoEncryption {
     return publicKey
   }
 
+  async decrypt ({ tag, cipherText }) {
+    const request = {
+      jsonrpc: '2.0',
+      id: this.nextRequestId(),
+      method: 'decrypt',
+      params: [{ tag, cipherText }]
+    }
+    const response = await this.channel.send(request)
+    if (response.error) {
+      throw new Error('some error')
+    }
+    const plainText = response.result
+    return plainText
+  }
+
   nextRequestId () {
     return this.requestId++
   }
--- workspaces/cogito-encryption/source/cogito-encryption.test.js
+++ workspaces/cogito-encryption/source/cogito-encryption.test.js
@@ -64,12 +64,47 @@ describe('encryption', () => {
     it('throws when error is returned', async () => {
       const error = { code: -42, message: 'some error' }
       telepathChannel.send.mockResolvedValue({ jsonrpc: '2.0', error })
-      await expect(cogitoEncryption.getPublicKey('nonexisting tag')).rejects.toBeDefined()
+      await expect(cogitoEncryption.getPublicKey({tag: 'nonexisting tag'})).rejects.toBeDefined()
     })
 
     it('uses different JSON-RPC ids for subsequent requests', async () => {
       await cogitoEncryption.getPublicKey({tag})
       await cogitoEncryption.getPublicKey({tag})
+      const id1 = telepathChannel.send.mock.calls[0][0].id
+      const id2 = telepathChannel.send.mock.calls[1][0].id
+      expect(id1).not.toBe(id2)
+    })
+  })
+
+  describe('decrypting data', () => {
+    const tag = 'some tag'
+    const cipherText = '0xsomeencryptedstuff'
+    const request = { jsonrpc: '2.0', method: 'decrypt', params: [{ tag, cipherText }] }
+    const plainText = 'decrypted plain text'
+    const response = { jsonrpc: '2.0', result: plainText }
+
+    beforeEach(() => {
+      telepathChannel.send.mockResolvedValue(response)
+    })
+
+    it('decrypts', async () => {
+      await cogitoEncryption.decrypt({ tag, cipherText })
+      expect(telepathChannel.send.mock.calls[0][0]).toMatchObject(request)
+    })
+
+    it('returns the plain text when decrypting', async () => {
+      expect(await cogitoEncryption.decrypt({ tag, cipherText })).toBe(plainText)
+    })
+
+    it('throws when error is returned', async () => {
+      const error = { code: -42, message: 'some error' }
+      telepathChannel.send.mockResolvedValue({ jsonrpc: '2.0', error })
+      await expect(cogitoEncryption.decrypt({ tag, cipherText })).rejects.toBeDefined()
+    })
+
+    it('uses different JSON-RPC ids for subsequent requests', async () => {
+      await cogitoEncryption.decrypt({ tag, cipherText })
+      await cogitoEncryption.decrypt({ tag, cipherText })
       const id1 = telepathChannel.send.mock.calls[0][0].id
       const id2 = telepathChannel.send.mock.calls[1][0].id
       expect(id1).not.toBe(id2)
