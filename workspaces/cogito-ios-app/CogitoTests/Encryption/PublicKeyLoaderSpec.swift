394e53274ad89f8bea94e69f78efe2789ca05f22
Mark Spanbroek <mark@spanbroek.net>
2018-04-24 12:05:25 +0200

    cogito-ios-app: add conversion to Json Web Key format

--- workspaces/cogito-ios-app/CogitoTests/Encryption/PublicKeyLoaderSpec.swift
+++ workspaces/cogito-ios-app/CogitoTests/Encryption/PublicKeyLoaderSpec.swift
@@ -3,15 +3,19 @@
 import Quick
 import Nimble
 import Security
+import SwiftyJSON
+import base64url
 @testable import Cogito
 
 class PublicKeyLoaderSpec: QuickSpec {
     override func spec() {
         let tag = UUID().uuidString
 
-        it("loads a previously created public key") {
+        beforeEach {
             KeyPairCreator().create(tag: tag)
+        }
 
+        it("loads a previously created public key") {
             let query: [String: Any] = [
                 kSecAttrKeyClass as String: kSecAttrKeyClassPublic,
                 kSecReturnData as String: true as CFBoolean,
@@ -28,6 +32,34 @@ class PublicKeyLoaderSpec: QuickSpec {
         it("returns nil when public key could not be found") {
             let nonExistent = UUID().uuidString
             expect(PublicKeyLoader().load(tag: nonExistent)).to(beNil())
+        }
+
+        context("when converting to Json Web Key format") {
+            var publicKey: Data!
+            var jwk: JSON!
+
+            beforeEach {
+                publicKey = PublicKeyLoader().load(tag: tag)
+                let jwkString = PublicKeyLoader().loadJsonWebKey(tag: tag)!
+                jwk = JSON(parseJSON: jwkString)
+            }
+
+            it("outputs the correct key type") {
+                expect(jwk["kty"].string).to(equal("RSA"))
+            }
+
+            it("outputs the modulus in base64url encoding") {
+                var modulus = publicKey.subdata(in: 8..<(publicKey.count - 5))
+                if modulus.count > 2048 / 8 {
+                    modulus.removeFirst()
+                }
+                expect(jwk["n"].string).to(equal(modulus.base64urlEncodedString()))
+            }
+
+            it("outputs the exponent in base64url encoding") {
+                let exponent = publicKey.subdata(in: (publicKey.count - 3)..<publicKey.count)
+                expect(jwk["e"].string).to(equal(exponent.base64urlEncodedString()))
+            }
         }
     }
 }
