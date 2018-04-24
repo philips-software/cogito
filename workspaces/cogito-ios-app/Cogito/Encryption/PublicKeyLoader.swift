189b9686af36db8f2f0e10406aa1f52004d2dec2
Mark Spanbroek <mark@spanbroek.net>
2018-04-24 12:05:25 +0200

    cogito-ios-app: add conversion to Json Web Key format

--- workspaces/cogito-ios-app/Cogito/Encryption/PublicKeyLoader.swift
+++ workspaces/cogito-ios-app/Cogito/Encryption/PublicKeyLoader.swift
@@ -1,6 +1,7 @@
 //  Copyright © 2018 Koninklijke Philips Nederland N.V. All rights reserved.
 
-import Foundation
+import SwiftyJSON
+import base64url
 
 struct PublicKeyLoader: PublicKeyLoaderType {
     func load(tag: String) -> PublicKey? {
@@ -15,6 +16,30 @@ struct PublicKeyLoader: PublicKeyLoaderType {
         SecItemCopyMatching(query as CFDictionary, &result)
         return result as? Data
     }
+
+    func loadJsonWebKey(tag: String) -> String? {
+        guard let publicKey = load(tag: tag) else {
+            return nil
+        }
+
+        let (modulus, exponent) = extractRSAParameters(publicKeyRSA2048Bits: publicKey)
+
+        return JSON([
+            "kty": "RSA",
+            "n": modulus.base64urlEncodedString(),
+            "e": exponent.base64urlEncodedString()
+        ]).rawString()
+    }
+}
+
+func extractRSAParameters(publicKeyRSA2048Bits key: Data) -> (modulus: Data, exponent: Data) {
+    // Simplified decoding of PKCS#1, see: https://stackoverflow.com/a/43225656
+    var modulus = key.subdata(in: 8..<(key.count - 5))
+    if modulus.count > 2048 / 8 {
+        modulus.removeFirst()
+    }
+    let exponent = key.subdata(in: (key.count - 3)..<key.count)
+    return (modulus, exponent)
 }
 
 protocol PublicKeyLoaderType {
