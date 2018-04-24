bd32899f758ec3e00590810dffe03efd702e75ba
Mark Spanbroek <mark@spanbroek.net>
2018-04-24 14:12:51 +0200

    cogito-ios-app: return Json Web Key as JSON object

--- workspaces/cogito-ios-app/CogitoTests/Encryption/PublicKeyLoaderSpy.swift
+++ workspaces/cogito-ios-app/CogitoTests/Encryption/PublicKeyLoaderSpy.swift
@@ -4,10 +4,16 @@
 
 class PublicKeyLoaderSpy: PublicKeyLoaderType {
     var publicKeyToReturn: PublicKey?
+    var jsonWebKeyToReturn: JsonWebKey?
     var latestTag: String?
 
     func load(tag: String) -> PublicKey? {
         latestTag = tag
         return publicKeyToReturn
+    }
+
+    func loadJsonWebKey(tag: String) -> JsonWebKey? {
+        latestTag = tag
+        return jsonWebKeyToReturn
     }
 }
