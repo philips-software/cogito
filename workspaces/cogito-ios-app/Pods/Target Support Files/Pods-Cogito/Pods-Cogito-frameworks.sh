c1459209d5108b6e7e29cdcc7aae9c9a5936bf5a
Mark Spanbroek <mark@spanbroek.net>
2018-04-24 11:12:59 +0200

    cogito-ios-app: update to latest Telepath

--- workspaces/cogito-ios-app/Pods/Target Support Files/Pods-Cogito/Pods-Cogito-frameworks.sh
+++ workspaces/cogito-ios-app/Pods/Target Support Files/Pods-Cogito/Pods-Cogito-frameworks.sh
@@ -161,6 +161,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Telepath/Telepath.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Toast-Swift/Toast_Swift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/base64url/base64url.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/BigInt/BigInt.framework"
@@ -181,6 +182,7 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Telepath/Telepath.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Toast-Swift/Toast_Swift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/base64url/base64url.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
