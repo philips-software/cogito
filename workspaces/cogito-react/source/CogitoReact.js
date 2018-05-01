718c1273f35a21fc7739a22ade45cc5eb64409a1
Marcin Czenko <marcin@everydayproductive.com>
2018-05-01 17:26:03 +0200

    Adds cogito-react package

---
+++ workspaces/cogito-react/source/CogitoReact.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Cogito } from '@cogitojs/cogito'
+
+export class CogitoReact extends React.Component {
+  state = { web3: null, channel: null, contracts: null }
+  cogito
+
+  constructor (contractsInfo) {
+    super()
+    this.cogito = new Cogito(contractsInfo)
+  }
+
+  updateState = async (props) => {
+    const { channelId, channelKey } = props
+
+    let telepathKey
+
+    if (channelKey instanceof Uint8Array || channelKey === undefined) {
+      telepathKey = channelKey
+    } else {
+      telepathKey = Uint8Array.from(Object.values(channelKey))
+    }
+
+    const { web3, channel, contracts } = await this.cogito.update({ channelId, channelKey: telepathKey })
+
+    this.setState({ web3, channel, contracts })
+
+    this.props.onTelepathChanged && this.props.onTelepathChanged({
+      channelId: channel.id,
+      channelKey: channel.key
+    })
+  }
+
+  componentDidMount () {
+    this.updateState(this.props)
+  }
+
+  componentWillReceiveProps (nextProps) {
+    if (nextProps.channelId !== this.props.channelId ||
+      nextProps.channelKey !== this.props.channelKey) {
+      this.updateState(nextProps)
+    }
+  }
+
+  render () {
+    return this.props.render
+      ? this.props.render(this.state)
+      : this.props.children(this.state)
+  }
+}
---
+++ workspaces/cogito-react/tools/build.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+const execSync = require('child_process').execSync
+const prettyBytes = require('pretty-bytes')
+const gzipSize = require('gzip-size')
+
+const exec = (command, extraEnv) =>
+  execSync(command, {
+    stdio: 'inherit',
+    env: Object.assign({}, process.env, extraEnv)
+  })
+
+console.log('Building CommonJS modules ...')
+
+exec('babel source -d lib --delete-dir-on-start', {
+  BABEL_ENV: 'commonjs'
+})
+
+console.log('\nBuilding ES modules ...')
+
+exec('babel source -d es --delete-dir-on-start', {
+  BABEL_ENV: 'es'
+})
+
+console.log('\nBuilding cogito-react.js ...')
+
+exec('rollup -c -f umd -o umd/cogito-react.js', {
+  BABEL_ENV: 'umd',
+  NODE_ENV: 'development'
+})
+
+console.log('\nBuilding cogito-react.min.js ...')
+
+exec('rollup -c -f umd -o umd/cogito-react.min.js', {
+  BABEL_ENV: 'umd',
+  NODE_ENV: 'production'
+})
+
+const size = gzipSize.sync(
+  fs.readFileSync('umd/cogito-react.min.js')
+)
+
+console.log('\ngzipped, the UMD build is %s', prettyBytes(size))
