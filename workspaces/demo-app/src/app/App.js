cbb9453ed8732382dd80b174bcc9ac296a56282d
Czenko, Marcin <marcin.czenko@philips.com>
2018-05-02 01:07:00 +0200

    react-redux removed from demo-app

--- workspaces/demo-app/src/app/App.js
+++ workspaces/demo-app/src/app/App.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { store } from 'app-state'
 
 import { Main } from './Main'
 
 const App = () =>
-  <Provider store={store}>
-    <Router>
-      <Main />
-    </Router>
-  </Provider>
+  <Router>
+    <Main />
+  </Router>
 
 export { App }
