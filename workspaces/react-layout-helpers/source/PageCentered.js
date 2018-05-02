dc08c98c9d1cfd5b2da71f138e1e2916481c21a2
Czenko, Marcin <marcin.czenko@philips.com>
2018-05-02 18:05:34 +0200

    Extracts common react layout helpers to a separate package

---
+++ workspaces/react-layout-helpers/source/PageCentered.js
@@ -0,0 +1,12 @@
+import glamorous from 'glamorous'
+
+const PageCentered = glamorous.div({
+  display: 'flex',
+  flexFlow: 'row nowrap',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100vh',
+  width: '100vw'
+})
+
+export { PageCentered }
---
+++ workspaces/react-layout-helpers/source/index.js
@@ -0,0 +1,8 @@
+export { Centered } from './Centered'
+export { Row } from './Row'
+export { Spacer } from './Spacer'
+export { PageCentered } from './PageCentered'
+export { FullWidth } from './FullWidth'
+export { DistributedHorizontally } from './DistributedHorizontally'
+export { ValueWrapper } from './ValueWrapper'
+export { FullWidthCentered } from './FullWidthCentered'
