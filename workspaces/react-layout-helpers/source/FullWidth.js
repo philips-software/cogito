1c020ba376af3deccb90a09a3eb0183e0bb65b5e
Czenko, Marcin <marcin.czenko@philips.com>
2018-05-03 13:23:01 +0200

    Removes @react-frontend-developer packages from monorepo and updates the references to published versions

--- workspaces/react-layout-helpers/source/FullWidth.js
+++
@@ -1,8 +0,0 @@
-import glamorous from 'glamorous'
-
-const FullWidth = glamorous.div({
-  width: '100%',
-  marginTop: '20px'
-})
-
-export { FullWidth }
--- workspaces/react-layout-helpers/source/Row.js
+++
@@ -1,8 +0,0 @@
-import glamorous from 'glamorous'
-
-export const Row = glamorous.div({
-  display: 'flex',
-  flexFlow: 'row nowrap',
-  justifyContent: 'center',
-  alignItems: 'center'
-})
