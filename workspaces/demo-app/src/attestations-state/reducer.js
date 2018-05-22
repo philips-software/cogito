7dc19d33a18a6b4deb77660d997fa3337fd3d48c
Mark Spanbroek <mark@spanbroek.net>
2018-05-22 14:42:56 +0200

    demo-app: add action to store a retrieved attestation in the state

--- workspaces/demo-app/src/attestations-state/reducer.js
+++ workspaces/demo-app/src/attestations-state/reducer.js
@@ -1,10 +1,12 @@
 export const attestationsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case 'RETRIEVED_ATTESTATION':
+      return { ...state, retrieved: action.attestation }
     default:
       return { ...state }
   }
 }
 
 const initialState = {
-
+  retrieved: undefined
 }
