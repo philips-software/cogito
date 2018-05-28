fa13a588464a9949d591af79ae30d95cd0482f6b
Mark Spanbroek <mark@spanbroek.net>
2018-05-28 11:50:08 +0200

    cogito-attestations-ios: add failing test for Identity

---
+++ workspaces/cogito-attestations-ios/AttestationsTests/AttestationsSpec.swift
@@ -0,0 +1,13 @@
+import Quick
+import Nimble
+@testable import Attestations
+
+class AttestationsSpec: QuickSpec {
+    override func spec() {
+        it("can create new identities") {
+            let identity1 = Identity()
+            let identity2 = Identity()
+            expect(identity1.address) != identity2.address
+        }
+    }
+}
