810ef4c285fb460fe462033d58423ff710b84981
Mark Spanbroek <mark@spanbroek.net>
2018-05-28 12:56:27 +0200

    cogito-attestations-ios: load javascript file

--- workspaces/cogito-attestations-ios/Attestations/JavaScript.swift
+++ workspaces/cogito-attestations-ios/Attestations/JavaScript.swift
@@ -1,5 +1,16 @@
 import JavaScriptCore
 
-struct Javascript {
-    static let context = JSContext()!
+class Javascript {
+    static let context = JSContext()!.load(filename: "cogito-attestations.min")
 }
+
+private extension JSContext {
+    func load(filename: String) -> JSContext {
+        let bundle = Bundle(for: Javascript.self)
+        let path = bundle.path(forResource: filename, ofType: "js")!
+        let script = try! String(contentsOfFile: path)
+        evaluateScript(script)
+        return self
+    }
+}
+
