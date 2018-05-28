13d8945599051ec872435e243f91dbbe891748dc
Mark Spanbroek <mark@spanbroek.net>
2018-05-28 10:38:58 +0200

    cogito-attestations-ios: created (empty) project

---
+++ workspaces/cogito-attestations-ios/Attestations/Attestations.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for Attestations.
+FOUNDATION_EXPORT double AttestationsVersionNumber;
+
+//! Project version string for Attestations.
+FOUNDATION_EXPORT const unsigned char AttestationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Attestations/PublicHeader.h>
+
+
