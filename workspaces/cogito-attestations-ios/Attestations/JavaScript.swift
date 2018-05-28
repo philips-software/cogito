b24bf62d1fed4459832889b57626b554190b0f2e
Mark Spanbroek <mark@spanbroek.net>
2018-05-28 14:03:19 +0200

    cogito-attestations-ios: ensure that javascript exceptions won't go unnoticed

--- workspaces/cogito-attestations-ios/Attestations/JavaScript.swift
+++ workspaces/cogito-attestations-ios/Attestations/JavaScript.swift
@@ -1,7 +1,9 @@
 import JavaScriptCore
 
 class Javascript {
-    static let context = JSContext()!.load(filename: "cogito-attestations.min")
+    static let context = JSContext()!
+        .setExceptionHandler(onException)
+        .load(filename: "cogito-attestations.min")
 }
 
 private extension JSContext {
@@ -12,5 +14,14 @@ private extension JSContext {
         evaluateScript(script)
         return self
     }
+
+    func setExceptionHandler(_ handler: @escaping (JSContext?, JSValue?) -> Void) -> JSContext {
+        exceptionHandler = handler
+        return self
+    }
 }
 
+private func onException(context: JSContext?, value: JSValue?) {
+    let message = value?.toString() ?? "unknown error"
+    assertionFailure("JavaScript error: \(message)")
+}
