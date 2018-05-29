9dc2bf301a330b6fd2067df792a4fbbed7518aa5
Mark Spanbroek <mark@spanbroek.net>
2018-05-29 14:57:50 +0200

    cogito-attestations-ios: fix indentation

--- workspaces/cogito-attestations-ios/Attestations/crypto/crypto.js
+++ workspaces/cogito-attestations-ios/Attestations/crypto/crypto.js
@@ -1,7 +1,7 @@
 if (!this.crypto.getRandomValues) {
-    const crypto = this.crypto
-    crypto.getRandomValues = function(array) {
-        const randomValues = crypto.randomBytes(array.byteLength)
-        array.set(new Uint8Array(randomValues))
-    }
+  const crypto = this.crypto
+  crypto.getRandomValues = function (array) {
+    const randomValues = crypto.randomBytes(array.byteLength)
+    array.set(new Uint8Array(randomValues))
+  }
 }
