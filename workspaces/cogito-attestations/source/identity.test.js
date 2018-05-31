236388634e3e9eda32c6be758ee8afe2010f9aa4
Mark Spanbroek <mark@spanbroek.net>
2018-05-31 17:09:12 +0200

    cogito-attestations: cleanup

--- workspaces/cogito-attestations/source/identity.test.js
+++ workspaces/cogito-attestations/source/identity.test.js
@@ -2,33 +2,35 @@ import { Identity } from './identity'
 import { generatePrivateKey, privateKeyToAddress, sign, keccak256 } from './primitives'
 
 describe('identity', () => {
-  const identity = new Identity()
+  describe('when constructed without an argument', () => {
+    const identity = new Identity()
 
-  it('has a private key', () => {
-    expect(identity.privateKey).toBeDefined()
-  })
+    it('has a private key', () => {
+      expect(identity.privateKey).toBeDefined()
+    })
 
-  it('has a corresponding address', () => {
-    const address = privateKeyToAddress(identity.privateKey)
-    expect(identity.address).toEqual(address)
-  })
+    it('has a corresponding address', () => {
+      const address = privateKeyToAddress(identity.privateKey)
+      expect(identity.address).toEqual(address)
+    })
 
-  it('generates unique addresses', () => {
-    const identity1 = new Identity()
-    const identity2 = new Identity()
-    expect(identity1.address).not.toEqual(identity2.address)
-  })
+    it('generates unique addresses', () => {
+      const identity1 = new Identity()
+      const identity2 = new Identity()
+      expect(identity1.address).not.toEqual(identity2.address)
+    })
 
-  it('can be serialized', () => {
-    const serialized = `${identity}`
-    expect(serialized).toContain(identity.privateKey)
-    expect(serialized).toContain(identity.address)
-  })
+    it('can be serialized', () => {
+      const serialized = `${identity}`
+      expect(serialized).toContain(identity.privateKey)
+      expect(serialized).toContain(identity.address)
+    })
 
-  it('can be deserialized', () => {
-    const serialized = `${identity}`
-    const deserialized = new Identity(serialized)
-    expect(deserialized).toEqual(identity)
+    it('can be deserialized', () => {
+      const serialized = `${identity}`
+      const deserialized = new Identity(serialized)
+      expect(deserialized).toEqual(identity)
+    })
   })
 
   describe('when constructed with a signing function', () => {
