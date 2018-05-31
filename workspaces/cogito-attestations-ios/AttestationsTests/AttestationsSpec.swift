67bf495d2c0924c210d3b28d50d1006189e97465
Mark Spanbroek <mark@spanbroek.net>
2018-05-31 11:45:32 +0200

    cogito-attestations-ios: issue new attestation

--- workspaces/cogito-attestations-ios/AttestationsTests/AttestationsSpec.swift
+++ workspaces/cogito-attestations-ios/AttestationsTests/AttestationsSpec.swift
@@ -9,5 +9,15 @@ class AttestationsSpec: QuickSpec {
             let identity2 = Identity()
             expect(identity1.address) != identity2.address
         }
+
+        it("can issue new attestations") {
+            let issuer = Identity()
+            let attribute = "email:bob@example.com"
+            let protoAttestation = issue(attribute: attribute, issuer: issuer)
+            expect(protoAttestation.issuer) == issuer.address
+            expect(protoAttestation.attribute) == attribute
+            expect(protoAttestation.attestationKey) != "undefined"
+            expect(protoAttestation.issuingSignature) != "undefined"
+        }
     }
 }
