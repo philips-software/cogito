c8e410070921eb71375b43f9acf710b012d0807b
Czenko, Marcin <marcin.czenko@philips.com>
2018-06-21 13:20:48 +0200

    Updates faucet binary to accept PROVIDER_URL variable

--- workspaces/faucet/bin/cogito-faucet.js
+++ workspaces/faucet/bin/cogito-faucet.js
@@ -2,16 +2,22 @@
 
 require('babel-polyfill')
 var FaucetServer = require('@cogitojs/faucet').default
-console.log('FaucetServer=', FaucetServer)
 var fs = require('fs')
 
 var environment = process.env.NODE_ENV || 'development'
+var providerUrl = process.env.PROVIDER_URL
 
 var configurationFile = fs.readFileSync('faucet-config-' + environment + '.json', {encoding: 'utf-8'})
-console.log('configurationFile=', configurationFile)
 var configuration = JSON.parse(configurationFile)
+
+if (providerUrl) {
+  configuration.providerUrl = 'http://' + providerUrl + ':8545'
+}
+
+console.log('Using configuration:')
+console.log(configuration)
 
 var faucetServer = new FaucetServer(configuration)
 faucetServer.server.listen(3001, function () {
-  console.log(' running on port 3001')
+  console.log('Running on port 3001')
 })
