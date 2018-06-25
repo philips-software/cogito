c99d8b199356c0c4e4f930f9fd60ca80b9ab1a72
Marcin Czenko <marcin@everydayproductive.com>
2018-06-25 16:32:43 +0200

    Adds tests validating queueing mechanism on the faucet

--- workspaces/faucet/source/faucet-server.test.js
+++ workspaces/faucet/source/faucet-server.test.js
@@ -24,6 +24,10 @@ describe('Server', () => {
       console.log = jest.fn()
     })
 
+    afterEach(() => {
+      console.log.mockRestore()
+    })
+
     it('supports donate URL', async () => {
       const address = '0x0000000000000000000000000000000000000001'
       await request(faucetServer.server)
@@ -42,7 +46,10 @@ describe('Server', () => {
   describe('errors', () => {
     beforeEach(() => {
       console.error = jest.fn()
-      console.error.mockClear()
+    })
+
+    afterEach(() => {
+      console.error.mockRestore()
     })
 
     it('returns response status 500', async () => {
@@ -104,6 +111,120 @@ describe('Server', () => {
       await request(faucetServer.server)
         .post(`/donate/${address}`)
         .expect('error message - error status text')
+    })
+  })
+
+  describe('scheduling', () => {
+    class DonationControlledMock {
+      fulfilStatus = false
+      maxExpectedQueueSize
+      counter = 10
+
+      waiter = (resolve, reject) => {
+        setTimeout(() => {
+          console.log('fulfilStatus=', this.fulfilStatus)
+          console.log('counter=', this.counter)
+          console.log('queue=', faucetServer.queue.length)
+          if (this.fulfilStatus) {
+            expect(faucetServer.queue.length).toBe(this.maxExpectedQueueSize)
+            resolve({status: true})
+          } else if (this.counter === 0) {
+            reject(new Error('timeout'))
+          } else {
+            this.counter = this.counter - 1
+            this.waiter(resolve, reject)
+          }
+        }, 100)
+      }
+
+      constructor (maxExpectedQueueSize) {
+        this.maxExpectedQueueSize = maxExpectedQueueSize
+      }
+
+      fulfil () {
+        this.fulfilStatus = true
+      }
+
+      getPromiseExecutor () {
+        return this.waiter
+      }
+    }
+
+    beforeEach(() => {
+      console.log = jest.fn()
+    })
+
+    afterEach(() => {
+      console.log.mockRestore()
+    })
+
+    it('immediately schedules a donator call when no previous call is present', async () => {
+      expect(faucetServer.queue.length).toBe(0)
+
+      donate.mockResolvedValueOnce({status: true})
+
+      const address = '0x0000000000000000000000000000000000000001'
+      await request(faucetServer.server)
+        .post(`/donate/${address}`)
+        .expect(200)
+    })
+
+    it('puts a request on a waiting queue and removes the item from the queue after it is served', done => {
+      const donationControlledMock = new DonationControlledMock(1)
+      donate.mockImplementationOnce(() => {
+        return new Promise(donationControlledMock.getPromiseExecutor())
+      })
+
+      const address = '0x0000000000000000000000000000000000000001'
+      request(faucetServer.server)
+        .post(`/donate/${address}`)
+        .expect(200)
+        .end((err, res) => {
+          if (err) {
+            done(err)
+          } else {
+            expect(faucetServer.queue.length).toBe(0)
+            done()
+          }
+        })
+      donationControlledMock.fulfil()
+    })
+
+    it('keeps request on the waiting queue before previous one is finished', done => {
+      const donationControlledMock1 = new DonationControlledMock(2)
+      const donationControlledMock2 = new DonationControlledMock(1)
+      donate
+        .mockImplementationOnce(() => {
+          return new Promise(donationControlledMock1.getPromiseExecutor())
+        })
+        .mockImplementationOnce(() => {
+          return new Promise(donationControlledMock2.getPromiseExecutor())
+        })
+
+      const address = '0x0000000000000000000000000000000000000001'
+      request(faucetServer.server)
+        .post(`/donate/${address}`)
+        .expect(200)
+        .end((err, res) => {
+          if (err) {
+            done(err)
+          } else {
+            expect(faucetServer.queue.length).toBe(1)
+          }
+        })
+      request(faucetServer.server)
+        .post(`/donate/${address}`)
+        .expect(200)
+        .end((err, res) => {
+          if (err) {
+            done(err)
+          } else {
+            expect(faucetServer.queue.length).toBe(0)
+            done()
+          }
+        })
+      donationControlledMock1.fulfil()
+      donationControlledMock2.fulfil()
     })
   })
 })
