b6081c5fae7e4e1afa5b49b72a59d067be9b2880
Marcin Czenko <marcin@everydayproductive.com>
2018-07-25 16:15:24 +0200

    Gets rid of coy.css for styling inline code in markdown and using glamorous instead

--- workspaces/demo-app/src/components/ui/markdown/InlineCode.js
+++ workspaces/demo-app/src/components/ui/markdown/InlineCode.js
@@ -1,8 +1,25 @@
 import React from 'react'
-import './coy.css'
+import glamorous from 'glamorous'
+
+const Code = glamorous.code({
+  display: 'inline',
+  background: 'none',
+  fontFamily: 'Consolas, Monaco, "Andale Mono", "Ubuntu Mono", monospace',
+  whiteSpace: 'normal',
+  wordSpacing: 'normal',
+  wordBreak: 'normal',
+  wordWrap: 'normal',
+  hyphens: 'none',
+  padding: '.2em',
+  borderRadius: '0.3em',
+  color: '#c92c2c',
+  border: '1px solid rgba(0, 0, 0, 0.1)',
+  lineHeight: 2.0,
+  fontSize: '0.9em'
+})
 
 const InlineCode = ({children}) => (
-  <code style={{lineHeight: 2.0, fontSize: '0.9em'}} className='language-text'>{children}</code>
+  <Code>{children}</Code>
 )
 
 export { InlineCode }
