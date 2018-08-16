7542236b4433524f727c5019b9290f2997f11ff7
Marcin Czenko <marcin@everydayproductive.com>
2018-08-16 15:43:00 +0200

    Removes redundant console.log(s) from the homepage EditFile component

--- workspaces/homepage/src/components/Editing.js
+++ workspaces/homepage/src/components/Editing.js
@@ -3,11 +3,9 @@ import React from 'react'
 const editUrl = 'https://github.com/philips-software/cogito/blob/master'
 
 export const EditFile = ({ fileAbsolutePath }) => {
-  console.log('fileAbsolutePath=', fileAbsolutePath)
   const match = fileAbsolutePath.match(/workspaces.*$/)
   if (match) {
     const fileRelativePath = match[0]
-    console.log('fileRelativePath=', fileRelativePath)
     return (
       <div>
         <a href={`${editUrl}/${fileRelativePath}`}>Edit this page</a>
