cc3902487b124dddc738a5bc00fe699e9bfcfdd8
Marcin Czenko <marcin@everydayproductive.com>
2018-09-07 15:04:08 +0200

    Moves TopLevelNavigationItem into a separate group of related abstractions.

---
+++ workspaces/homepage/src/components/navigation/top-level-navigation-item/arrows/ArrowDown.js
@@ -0,0 +1,9 @@
+import glamorous from 'glamorous'
+
+export const ArrowDown = glamorous.div({
+  width: 0,
+  height: 0,
+  borderLeft: '5px solid transparent',
+  borderRight: '5px solid transparent',
+  borderTop: '8px solid #F486CA'
+})
---
+++ workspaces/homepage/src/components/navigation/top-level-navigation-item/arrows/ArrowUp.js
@@ -0,0 +1,9 @@
+import glamorous from 'glamorous'
+
+export const ArrowUp = glamorous.div({
+  width: 0,
+  height: 0,
+  borderLeft: '5px solid transparent',
+  borderRight: '5px solid transparent',
+  borderBottom: '8px solid #F486CA'
+})
