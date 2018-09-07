72026c2a52a3c8780fa6294e7c48bc8cb09af885
Marcin Czenko <marcin@everydayproductive.com>
2018-09-07 16:35:18 +0200

    Introduced Collapsable to reduce duplication in top-level and mid-level navigation items

--- workspaces/homepage/src/components/navigation/top-level-navigation-item/TopLevelNavigationItem.js
+++ workspaces/homepage/src/components/navigation/top-level-navigation-item/TopLevelNavigationItem.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import glamorous from 'glamorous'
 
+import { Collapsable } from '../Collapsable'
 import { ArrowUp, ArrowDown } from './arrows'
 import { ActiveMarker } from './ActiveMarker'
 
@@ -30,75 +31,21 @@ const ArrowPositioner = glamorous.div({
 })
 
 class TopLevelNavigationItem extends React.Component {
-  state = {
-    folded: true,
-    delta: 0,
-    style: {
-      display: 'flex',
-      flexFlow: 'column nowrap',
-      alignItems: 'flex-start',
-      justifyContent: 'flex-start',
-      maxHeight: 0,
-      overflow: 'hidden',
-      transition: 'max-height 0.2s ease-out'
-    }
-  }
-
-  componentDidUpdate (prevProps) {
-    if (this.props.delta !== prevProps.delta) {
-      console.log('componentDidUpdate', this.props.delta, this.div.scrollHeight)
-      if (!this.state.folded) {
-        this.setState({
-          delta: this.props.delta,
-          style: {
-            ...this.state.style,
-            maxHeight: `${this.div.scrollHeight + this.props.delta}px`
-          }
-        })
-      }
-    }
-  }
-
-  unfold () {
-    if (this.state.folded) {
-      this.setState({
-        folded: false,
-        style: {
-          ...this.state.style,
-          maxHeight: `${this.div.scrollHeight}px`
-        }
-      })
-    } else {
-      this.setState({
-        folded: true,
-        style: {
-          ...this.state.style,
-          maxHeight: 0
-        }
-      })
-    }
-  }
-
-  setDivRef = element => {
-    this.div = element
-  }
-
   render () {
     return (
-      <div>
-        <Wrapper onClick={() => this.unfold()}>
+      <Collapsable delta={this.props.delta} trigger={(unfold, folded) => (
+        <Wrapper onClick={() => unfold()}>
           {this.props.active && <ActiveMarker active={this.props.active} />}
           <Text color='white'>{this.props.title}</Text>
           <ArrowPositioner>
-            { this.state.folded ? <ArrowDown /> : <ArrowUp /> }
+            { folded ? <ArrowDown /> : <ArrowUp /> }
           </ArrowPositioner>
         </Wrapper>
-        <div style={this.state.style} ref={this.setDivRef}>
-          <div style={{backgroundColor: 'white', border: '1px solid #F7F7F7', width: '100%'}}>
-            { this.props.children }
-          </div>
+      )}>
+        <div style={{backgroundColor: 'white', border: '1px solid #F7F7F7', width: '100%'}}>
+          { this.props.children }
         </div>
-      </div>
+      </Collapsable>
     )
   }
 }
