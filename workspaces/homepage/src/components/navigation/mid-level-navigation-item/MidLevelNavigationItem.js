f980644fe60010d874d2a35755e6f923a2faa4b0
Marcin Czenko <marcin@everydayproductive.com>
2018-09-07 16:35:18 +0200

    Introduced Collapsable to reduce duplication in top-level and mid-level navigation items

--- workspaces/homepage/src/components/navigation/mid-level-navigation-item/MidLevelNavigationItem.js
+++ workspaces/homepage/src/components/navigation/mid-level-navigation-item/MidLevelNavigationItem.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import glamorous from 'glamorous'
 
+import { Collapsable } from '../Collapsable'
 import { NavigationLink } from '../NavigationLink'
 
 const Wrapper = glamorous.div({
@@ -11,54 +12,16 @@ const Wrapper = glamorous.div({
 })
 
 class MidLevelNavigationItem extends React.Component {
-  state = {
-    folded: true,
-    style: {
-      display: 'flex',
-      flexFlow: 'column nowrap',
-      alignItems: 'flex-start',
-      justifyContent: 'flex-start',
-      maxHeight: 0,
-      overflow: 'hidden',
-      transition: 'max-height 0.2s ease-out'
-    }
-  }
-
-  unfold () {
-    if (this.state.folded) {
-      this.props.onChange && this.props.onChange(this.div.scrollHeight)
-      this.setState({
-        folded: false,
-        style: {
-          ...this.state.style,
-          maxHeight: `${this.div.scrollHeight}px`
-        }
-      })
-    } else {
-      this.props.onChange && this.props.onChange(0)
-      this.setState({
-        folded: true,
-        style: {
-          ...this.state.style,
-          maxHeight: 0
-        }
-      })
-    }
-  }
-
-  setDivRef = element => {
-    this.div = element
-  }
-
   isActiveGroup = (match, location) => {
     return match && match.isExact
   }
 
   render () {
     const { title, path } = this.props
+
     return (
-      <div>
-        <Wrapper onClick={() => this.unfold()}>
+      <Collapsable onChange={this.props.onChange} trigger={(unfold, folded) => (
+        <Wrapper onClick={() => unfold()}>
           <NavigationLink
             to={path}
             activeStyle={{
@@ -73,12 +36,11 @@ class MidLevelNavigationItem extends React.Component {
             {title}
           </NavigationLink>
         </Wrapper>
-        <div style={this.state.style} ref={this.setDivRef}>
-          <div style={{marginLeft: '1rem'}}>
-            { this.props.children }
-          </div>
+      )}>
+        <div style={{marginLeft: '1rem'}}>
+          { this.props.children }
         </div>
-      </div>
+      </Collapsable>
     )
   }
 }
