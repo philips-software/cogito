8dccccbb60648c3d2d931cf0f889dbfc90003885
Marcin Czenko <marcin@everydayproductive.com>
2018-09-10 12:58:33 +0200

    Updates the styling of the Edit this page link

--- workspaces/homepage/src/components/Editing.js
+++ workspaces/homepage/src/components/Editing.js
@@ -1,15 +1,27 @@
 import React from 'react'
+import glamorous from 'glamorous'
 
 const editUrl = 'https://github.com/philips-software/cogito/blob/master'
+
+const A = glamorous.a({
+  fontFamily: 'Roboto Mono, monospace',
+  fontWeight: '100',
+  fontSize: '0.8rem',
+  color: '#F486CA'
+})
+
+const Wrapper = glamorous.div({
+  paddingLeft: '3px'
+})
 
 export const EditFile = ({ fileAbsolutePath }) => {
   const match = fileAbsolutePath.match(/workspaces.*$/)
   if (match) {
     const fileRelativePath = match[0]
     return (
-      <div>
-        <a href={`${editUrl}/${fileRelativePath}`}>Edit this page</a>
-      </div>
+      <Wrapper>
+        <A href={`${editUrl}/${fileRelativePath}`}>Edit this page</A>
+      </Wrapper>
     )
   }
   return null
