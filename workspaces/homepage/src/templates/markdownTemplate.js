1914ef9014cf6713efe4891c57cccdfe7515f43a
Marcin Czenko <marcin@everydayproductive.com>
2018-09-25 18:19:10 +0200

    Adds support for content filed in the frontmatter

--- workspaces/homepage/src/templates/markdownTemplate.js
+++ workspaces/homepage/src/templates/markdownTemplate.js
@@ -4,13 +4,13 @@ import { EditFile } from 'src/components/Editing'
 import { graphql } from 'gatsby'
 
 const Template = ({ data: { doc }, location }) => {
-  const { html, fileAbsolutePath, frontmatter: { title } } = doc
+  const { html, fileAbsolutePath, frontmatter: { title, content } } = doc
   return (
     <div>
       <Helmet title={title} />
       <EditFile fileAbsolutePath={fileAbsolutePath} />
       <h1>{title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: html }} />
+      <div dangerouslySetInnerHTML={{ __html: content ? content.childMarkdownRemark.html.split('\n').slice(1).join('\n') : html }} />
     </div>
   )
 }
@@ -22,6 +22,11 @@ export const pageQuery = graphql`
       fileAbsolutePath
       frontmatter {
         title
+        content {
+          childMarkdownRemark {
+            html
+          }
+        }
       }
     }
   }
