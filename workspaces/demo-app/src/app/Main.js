d5c46e1e7c517fa1fe014506b6d56e1bab81bf5b
Mark Spanbroek <mark@spanbroek.net>
2018-10-10 13:15:56 +0200

    demo-app: format using prettier

--- workspaces/demo-app/src/app/Main.js
+++ workspaces/demo-app/src/app/Main.js
@@ -24,12 +24,11 @@ const contractsInfo = {
 
 class Main extends React.Component {
   renderComponent = (Component, routeProps, web3Props) => (
-    <Component {...web3Props}
-      {...routeProps} />
+    <Component {...web3Props} {...routeProps} />
   )
 
-  web3IsReady = ({web3, channel, contracts}) => {
-    return (web3 && channel && contracts)
+  web3IsReady = ({ web3, channel, contracts }) => {
+    return web3 && channel && contracts
   }
 
   onTelepathChanged = ({ channelId, channelKey, appName }, dispatch) => {
@@ -43,34 +42,71 @@ class Main extends React.Component {
           channelId: state.userData.channelId,
           channelKey: state.userData.channelKey
         })}
-        render={({channelId, channelKey}, dispatch) =>
-          <CogitoReact contracts={contractsInfo}
+        render={({ channelId, channelKey }, dispatch) => (
+          <CogitoReact
+            contracts={contractsInfo}
             channelId={channelId}
             channelKey={channelKey}
             appName='Cogito Demo App'
-            onTelepathChanged={(telepath) => this.onTelepathChanged(telepath, dispatch)}
+            onTelepathChanged={telepath =>
+              this.onTelepathChanged(telepath, dispatch)}
           >
             {web3Props => {
               if (this.web3IsReady(web3Props)) {
                 return (
                   <Switch>
-                    <Route exact path='/' render={routeProps => this.renderComponent(CogitoId, routeProps, web3Props)} />
-                    <Route exact path='/contracts' render={routeProps => this.renderComponent(Contracts, routeProps, web3Props)} />
-                    <Route exact path='/simple-encryption' render={routeProps => this.renderComponent(SimpleEncryption, routeProps, web3Props)} />
-                    <Route exact path='/stream-encryption' render={routeProps => this.renderComponent(StreamEncryption, routeProps, web3Props)} />
-                    <Route exact path='/attestations' render={routeProps => this.renderComponent(Attestations, routeProps, web3Props)} />
+                    <Route
+                      exact
+                      path='/'
+                      render={routeProps =>
+                        this.renderComponent(CogitoId, routeProps, web3Props)}
+                    />
+                    <Route
+                      exact
+                      path='/contracts'
+                      render={routeProps =>
+                        this.renderComponent(Contracts, routeProps, web3Props)}
+                    />
+                    <Route
+                      exact
+                      path='/simple-encryption'
+                      render={routeProps =>
+                        this.renderComponent(
+                          SimpleEncryption,
+                          routeProps,
+                          web3Props
+                        )}
+                    />
+                    <Route
+                      exact
+                      path='/stream-encryption'
+                      render={routeProps =>
+                        this.renderComponent(
+                          StreamEncryption,
+                          routeProps,
+                          web3Props
+                        )}
+                    />
+                    <Route
+                      exact
+                      path='/attestations'
+                      render={routeProps =>
+                        this.renderComponent(
+                          Attestations,
+                          routeProps,
+                          web3Props
+                        )}
+                    />
                     <Route component={NoMatch404} />
                   </Switch>
                 )
               } else {
                 return (
                   <PageCentered>
-                    <div style={{width: '300px', height: '200px'}}>
-                      <Segment style={{width: '100%', height: '100%'}}>
+                    <div style={{ width: '300px', height: '200px' }}>
+                      <Segment style={{ width: '100%', height: '100%' }}>
                         <Dimmer active inverted>
-                          <Loader inverted content={
-                            <p>loading web3...</p>
-                          } />
+                          <Loader inverted content={<p>loading web3...</p>} />
                         </Dimmer>
                       </Segment>
                     </div>
@@ -79,7 +115,7 @@ class Main extends React.Component {
               }
             }}
           </CogitoReact>
-        }
+        )}
       />
     )
   }
