6c99888c95504c251c1124d4b3c8cf64b97497f8
Marcin Czenko <marcin@everydayproductive.com>
2018-11-06 16:38:31 +0100

    Moves jest configuration to jest.config.js and adds snapshot serializers

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  projects: [
+    'workspaces/cogito-web3',
+    'workspaces/cogito-encryption',
+    'workspaces/cogito-identity',
+    'workspaces/cogito-attestations',
+    'workspaces/telepath-js',
+    'workspaces/telepath-queuing-service',
+    'workspaces/demo-app',
+    'workspaces/crypto',
+    'workspaces/faucet',
+    'workspaces/cogito-ios-app-distribution'
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'source/**.js',
+    '!**/*.test.js',
+    '!**/node_modules/**'
+  ],
+  coverageReporters: [
+    'text-summary',
+    'lcov'
+  ]
+}
