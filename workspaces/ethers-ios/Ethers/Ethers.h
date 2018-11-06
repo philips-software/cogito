fba0677ae0477d4c20c582f3ba8657b319a5df3c
Mark Spanbroek <mark@spanbroek.net>
2018-11-06 16:38:38 +0100

    ethers-ios: empty project

---
+++ workspaces/ethers-ios/Ethers/Ethers.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for Ethers.
+FOUNDATION_EXPORT double EthersVersionNumber;
+
+//! Project version string for Ethers.
+FOUNDATION_EXPORT const unsigned char EthersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ethers/PublicHeader.h>
+
+
