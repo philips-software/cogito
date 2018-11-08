47f7ce0aca0959312ac8e73748c7b34ec958da83
Stefan van den Oord <stefan.van.den.oord@philips.com>
2018-11-08 13:26:11 +0100

    Extracts two methods (and reformatted)

--- workspaces/telepath-queuing-service/source/server.js
+++ workspaces/telepath-queuing-service/source/server.js
@@ -11,48 +11,63 @@ function createServer () {
   server.use(cors())
   server.use(bodyParser.text({ type: '*/*' }))
 
-  setInterval(() => { state.prune() }, 60 * 1000)
+  setInterval(() => {
+    state.prune()
+  }, 60 * 1000)
 
-  server.post('/:queueId', wrap(async function (request, response) {
-    const queueId = request.params.queueId
-    const message = request.body
-    if (message.length > 100000) {
-      response
-        .status(400)
-        .send('Message too large. Only 100000 characters allowed.')
-      return
-    }
-    if (!state.get(queueId)) {
-      state.set(queueId, [])
-    }
-    const queue = state.get(queueId)
-    if (queue.length >= 10) {
-      response
-        .status(429)
-        .send('Too many requests, maximum queue size reached.')
-      return
-    }
-    queue.push(message)
-    response.status(200).end()
-  }))
-
-  server.get('/:queueId', wrap(async function (request, response) {
-    const queueId = request.params.queueId
-    const queue = state.get(queueId)
-    state.set(queueId, queue) // retain queue in cache
-    if (!queue) {
-      response.status(204).end()
-      return
-    }
-    const message = queue.shift()
-    if (!message) {
-      response.status(204).end()
-      return
-    }
-    response.status(200).send(message)
-  }))
+  registerSendMessageEndpoint({ server, state })
+  registerReadMessageEndpoint({ server, state })
 
   return server
+}
+
+function registerSendMessageEndpoint ({ server, state }) {
+  server.post(
+    '/:queueId',
+    wrap(async function (request, response) {
+      const queueId = request.params.queueId
+      const message = request.body
+      if (message.length > 100000) {
+        response
+          .status(400)
+          .send('Message too large. Only 100000 characters allowed.')
+        return
+      }
+      if (!state.get(queueId)) {
+        state.set(queueId, [])
+      }
+      const queue = state.get(queueId)
+      if (queue.length >= 10) {
+        response
+          .status(429)
+          .send('Too many requests, maximum queue size reached.')
+        return
+      }
+      queue.push(message)
+      response.status(200).end()
+    })
+  )
+}
+
+function registerReadMessageEndpoint ({ server, state }) {
+  server.get(
+    '/:queueId',
+    wrap(async function (request, response) {
+      const queueId = request.params.queueId
+      const queue = state.get(queueId)
+      state.set(queueId, queue) // retain queue in cache
+      if (!queue) {
+        response.status(204).end()
+        return
+      }
+      const message = queue.shift()
+      if (!message) {
+        response.status(204).end()
+        return
+      }
+      response.status(200).send(message)
+    })
+  )
 }
 
 export default createServer
