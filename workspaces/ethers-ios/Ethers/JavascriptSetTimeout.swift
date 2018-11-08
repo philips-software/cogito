aad13b2bc9e9e655987ce559a5b51c2bb3cbbbe6
Mark Spanbroek <mark@spanbroek.net>
2018-11-08 15:50:41 +0100

    ethers-ios: add support for setTimeout() in JavaScriptCore

---
+++ workspaces/ethers-ios/Ethers/JavascriptSetTimeout.swift
@@ -0,0 +1,18 @@
+import JavaScriptCore
+
+extension JSContext {
+    func addSetTimeout() -> JSContext {
+        self.globalObject.setValue(setTimeout, forProperty: "setTimeout")
+        return self
+    }
+}
+
+fileprivate let setTimeout: @convention(block) (JSValue?, JSValue?) -> Void = { callback, milliseconds in
+    var delay: Double = 0
+    if let milliseconds = milliseconds, milliseconds.isNumber {
+        delay = milliseconds.toDouble() / 1000
+    }
+    DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+        callback?.call(withArguments: [])
+    }
+}
