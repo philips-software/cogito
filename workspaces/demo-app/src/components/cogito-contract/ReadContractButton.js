2831a9855e7089f58dfeb62770dc6598a164d6bb
Marcin Czenko <marcin@everydayproductive.com>
2018-11-09 16:06:48 +0100

    Extracts ReadContractButton to a separate component

---
+++ workspaces/demo-app/src/components/cogito-contract/ReadContractButton.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+import { WithStore } from '@react-frontend-developer/react-redux-render-prop'
+
+const ReadContractButton = ({onClick}) => {
+  return (
+    <WithStore selector={state => ({
+      telepathInProgress: state.appEvents.telepathInProgress
+    })}
+    >
+      {
+        ({telepathInProgress}) => (
+          <Button
+            secondary
+            color='black'
+            disabled={telepathInProgress}
+            onClick={(e) => onClick(e)}
+          >
+          Read...
+          </Button>
+        )
+      }
+    </WithStore>
+  )
+}
+
+export { ReadContractButton }
