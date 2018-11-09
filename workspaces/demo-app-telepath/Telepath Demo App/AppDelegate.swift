4f2eb52e859a00f37ccef174f9474ae6ed9da645
Stefan van den Oord <stefan.van.den.oord@philips.com>
2018-11-09 15:13:00 +0100

    [#47] Adds new iOS project

---
+++ workspaces/demo-app-telepath/Telepath Demo App/AppDelegate.swift
@@ -0,0 +1,10 @@
+//  Copyright © 2018 Koninklijke Philips Nederland B.V. All rights reserved.
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
