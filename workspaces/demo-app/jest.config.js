3f20015b63921ec1c872fb51dfb4fcfdc971557c
Marcin Czenko <marcin@everydayproductive.com>
2018-11-09 18:19:21 +0100

    Moves setting NODE_PATH for tests to the jest.config.js

--- workspaces/demo-app/jest.config.js
+++ workspaces/demo-app/jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
   setupTestFrameworkScriptFile: require.resolve('./setup-tests.js'),
   snapshotSerializers: [
     'jest-glamor-react'
+  ],
+  modulePaths: [
+    '<rootDir>/src/'
   ]
 }
