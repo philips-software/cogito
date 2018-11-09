9025dbf00700bed2f0ac66585a802955cf3958e4
Marcin Czenko <marcin@everydayproductive.com>
2018-11-09 18:20:26 +0100

    Moves jest configuration to jest.config.js for the distribution app and fixes tests

--- workspaces/cogito-ios-app-distribution/src/app/Main.test.js
+++ workspaces/cogito-ios-app-distribution/src/app/Main.test.js
@@ -1,15 +1,15 @@
+import React from 'react'
 import ReactDOM from 'react-dom'
 import renderer from 'react-test-renderer'
 import { Main } from './Main'
-import { inRouter } from 'test-helpers/router'
 
 it('renders without crashing', async () => {
   const div = document.createElement('div')
-  await ReactDOM.render(inRouter(Main, '/'), div)
+  await ReactDOM.render(<Main />, div)
 })
 
 it('renders correctly', async () => {
   const app = await renderer
-    .create(inRouter(Main, '/'))
+    .create(<Main />)
   expect(app.toJSON()).toMatchSnapshot()
 })
