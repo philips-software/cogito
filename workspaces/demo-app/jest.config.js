7fa693969f624562bdfc5837ac9d0cb94ceb606a
Marcin Czenko <marcin@everydayproductive.com>
2018-11-14 14:39:22 +0100

    Adds jest-canvas mock to let jest run when canvas is to be rendered

--- workspaces/demo-app/jest.config.js
+++ workspaces/demo-app/jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  setupFiles: [
+    'jest-canvas-mock'
+  ],
   setupTestFrameworkScriptFile: require.resolve('./setup-tests.js'),
   snapshotSerializers: [
     'jest-glamor-react'
