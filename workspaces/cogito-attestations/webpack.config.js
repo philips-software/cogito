fdde08744636ca459e7ed47cb2ff904057ba734a
Marcin Czenko <marcin@everydayproductive.com>
2018-11-23 19:50:18 +0100

    Updates build script to get more infor from relevant package.json and automatically sense webpack.config.js

--- workspaces/cogito-attestations/webpack.config.js
+++ workspaces/cogito-attestations/webpack.config.js
@@ -1,8 +1,17 @@
-module.exports = {
-  entry: './source/index.js',
-  output: {
-    library: 'cogitoAttestations',
-    libraryTarget: 'umd',
-    globalObject: 'this'
+const path = require('path')
+
+module.exports = function (env = {}) {
+  const { production } = env
+  return {
+    mode: production ? 'production' : 'development',
+    devtool: 'source-maps',
+    entry: './source/index.js',
+    output: {
+      path: path.join(__dirname, 'umd'),
+      filename: `cogito-attestations.${production ? 'min.js' : 'js'}`,
+      library: 'cogitoAttestations',
+      libraryTarget: 'umd',
+      globalObject: 'this'
+    }
   }
 }
