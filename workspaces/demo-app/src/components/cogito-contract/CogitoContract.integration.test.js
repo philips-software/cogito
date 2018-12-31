d4a5ec4c39f4c2f74c174ce520e095815d98d5df
Marcin Czenko <marcin@everydayproductive.com>
2018-12-31 01:25:15 +0100

    [demo-app]: gets rid of mocking to make CogitoContract integration test more an integration test

--- workspaces/demo-app/src/components/cogito-contract/CogitoContract.integration.test.js
+++ workspaces/demo-app/src/components/cogito-contract/CogitoContract.integration.test.js
@@ -1,51 +1,66 @@
 import React from 'react'
+import { ethers } from 'ethers'
 import { render, fireEvent, wait, waitForElement } from 'test-helpers/render-props'
-import {
-  TelepathChannelMock,
-  GanacheTestNetwork
-} from 'test-helpers'
+import { GanacheTestNetwork } from 'test-helpers'
 import nock from 'nock'
 import { CogitoContract } from './CogitoContract'
 
 import { UserDataActions } from 'user-data'
-import { SimpleStorage as simpleStorageDef } from '@cogitojs/demo-app-contracts'
+import { SimpleStorage } from '@cogitojs/demo-app-contracts'
+import { CogitoEthereum } from '@cogitojs/cogito'
 
 jest.unmock('@react-frontend-developer/react-redux-render-prop')
 
 describe('CogitoContract Integration test', () => {
-  let channel
-  let simpleStorage
-  let simpleStorageProxy
-  let from
+  const appName = 'Cogito Demo App'
+  let cogitoEthereum
+  let cogitoContext
   let ganacheTestNetwork
+  const mnemonic = 'hair snack volcano shift tragic wrong wreck release vibrant gossip ugly debate'
+  let wallet
 
   const setActiveTelepathChannel = dispatch => {
-    dispatch(UserDataActions.setIdentityInfo(channel.identities[0]))
+    dispatch(UserDataActions.setIdentityInfo({
+      ethereumAddress: wallet.address,
+      username: 'Test User'
+    }))
     dispatch(UserDataActions.connectionEstablished())
   }
 
-  const initSimpleStorageProxy = () => {
-    return {
-      deployed: jest.fn().mockResolvedValueOnce(simpleStorage)
+  const makeTransactionEthersCompatible = transaction => {
+    let ethersTransaction = {
+      ...transaction,
+      gasLimit: transaction.gas
     }
+    delete ethersTransaction.from
+    delete ethersTransaction.gas
+    return ethersTransaction
+  }
+
+  const mockTelepathChannel = telepathChannel => {
+    telepathChannel.send = jest.fn().mockImplementationOnce(async signRequest => {
+      const transaction = makeTransactionEthersCompatible(signRequest.params[0])
+      const signedTransaction = await wallet.sign(transaction)
+      return Promise.resolve({
+        result: signedTransaction
+      })
+    })
   }
 
   beforeEach(async () => {
+    wallet = ethers.Wallet.fromMnemonic(mnemonic)
     ganacheTestNetwork = new GanacheTestNetwork()
-    from = (await ganacheTestNetwork.getAccounts())[0]
-    const { contract } = await ganacheTestNetwork.deploy(simpleStorageDef, { from })
-    simpleStorage = await contract.deployed()
-    simpleStorageProxy = initSimpleStorageProxy()
-    channel = new TelepathChannelMock({
-      identities: [
-        {
-          ethereumAddress: from,
-          username: 'Test User'
-        }
-      ]
-    })
-    process.env.FAUCET_URL = 'https://faucet.url/donate'
-    nock(process.env.FAUCET_URL).post(`/${from}`, '').reply(200)
+    window.web3 = ganacheTestNetwork.web3
+    process.env.REACT_APP_USE_INJECTED_WEB3 = 'YES'
+    console.log = jest.fn()
+
+    const { deployedJSON } = await ganacheTestNetwork.deploy(SimpleStorage, { from: wallet.address })
+    cogitoEthereum = new CogitoEthereum([ deployedJSON ])
+    cogitoContext = await cogitoEthereum.getContext({ appName })
+
+    mockTelepathChannel(cogitoContext.telepathChannel)
+    process.env.REACT_APP_FAUCET_URL = 'https://faucet.url/donate'
+    nock(process.env.REACT_APP_FAUCET_URL).post(`/${wallet.address}`, '').reply(200)
   })
 
   afterEach(() => {
@@ -53,9 +68,8 @@ describe('CogitoContract Integration test', () => {
   })
 
   it('can increase contract value', async () => {
-    console.log = jest.fn()
     const { getByText, getByTestId, store: { dispatch } } = render(
-      <CogitoContract channel={channel} simpleStorageProxy={simpleStorageProxy} />
+      <CogitoContract channel={cogitoContext.telepathChannel} simpleStorageProxy={cogitoContext.contractsProxies.SimpleStorage} />
     )
     const currentValue = await waitForElement(() => getByTestId(/current-value/i))
     expect(currentValue).toHaveTextContent('0')
