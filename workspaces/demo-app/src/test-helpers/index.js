7c9d0d168fdb28bf8b19ec5c7c891d7a2dfe5fee
Marcin Czenko <marcin@everydayproductive.com>
2019-01-01 20:50:27 +0100

    [demo-app]: removes CogitoProviderForTesting as it has been replaced with better setup in EthereumForSimpleStorage

--- workspaces/demo-app/src/test-helpers/index.js
+++ workspaces/demo-app/src/test-helpers/index.js
@@ -1,7 +1,6 @@
 export { inRouter, renderInRouter } from './router'
 export { EventWaiter } from './EventWaiter'
 export { GanacheTestNetwork } from './GanacheTestNetwork'
-export { CogitoProviderForTesting } from './CogitoProviderForTesting'
 export { TelepathChannelMock } from './TelepathChannelMock'
 export { SimpleStorageMock } from './SimpleStorageMock'
 export { InteractivePromise } from './InteractivePromise'
