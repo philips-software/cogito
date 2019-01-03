4031605064c737f691ec5888192bc69fb6d2e400
Marcin Czenko <marcin@everydayproductive.com>
2019-01-03 20:03:49 +0100

    [demo-app]: updates cogito-react manual mock

--- workspaces/demo-app/src/__mocks__/@cogitojs/cogito-react.js
+++ workspaces/demo-app/src/__mocks__/@cogitojs/cogito-react.js
@@ -1,37 +1,17 @@
 import React from 'react'
 
-const contract = {
-  increase: (increment, obj) => {
-    return new Promise(resolve => {
-      resolve()
-    })
-  },
-  read: obj => {
-    return new Promise(resolve => {
-      resolve({ toNumber: () => 15 })
-    })
-  }
-}
-
-const channel = {
-  id: 'channelId',
-  key: 'channelKey',
-  createConnectUrl: jest.fn().mockReturnValue('connecturl')
-}
-
 export class CogitoReact extends React.Component {
   state = {
-    web3: {
-      eth: {
-        personal: {
-          unlockAccount: () => true
-        }
-      }
+    cogitoWeb3: {},
+    telepathChannel: {
+      id: 'telepathId',
+      key: Uint8Array.from([1, 2, 3]),
+      appName: 'Cogito Demo App'
     },
-    channel: channel,
-    contracts: {
-      simpleStorage: contract
-    }
+    contractsProxies: {
+      SimpleStorage: 'SimpleStorageProxy'
+    },
+    newChannel: jest.fn()
   }
 
   render () {
