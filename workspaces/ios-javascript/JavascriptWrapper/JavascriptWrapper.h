8fa091bf556a873547a911f64e826660d44b6ddf
Mark Spanbroek <mark@spanbroek.net>
2019-02-06 13:56:35 +0100

    [ios-javascript] Introduce Javascript Wrapper for iOS

---
+++ workspaces/ios-javascript/JavascriptWrapper/JavascriptWrapper.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for JavascriptWrapper.
+FOUNDATION_EXPORT double JavascriptWrapperVersionNumber;
+
+//! Project version string for JavascriptWrapper.
+FOUNDATION_EXPORT const unsigned char JavascriptWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JavascriptWrapper/PublicHeader.h>
+
+
