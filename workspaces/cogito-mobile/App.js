5791c5f43bb638931622d61675260e35d9053fc0
Mark Spanbroek <mark@spanbroek.net>
2019-02-14 16:13:16 +0100

    [cogito-mobile] rewrite to stateless functional component

--- workspaces/cogito-mobile/App.js
+++ workspaces/cogito-mobile/App.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
-export default class App extends React.Component {
-  render () {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    )
-  }
-}
+const App = () => (
+  <View style={styles.container}>
+    <Text>Open up App.js to start working on your app!</Text>
+  </View>
+)
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +15,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   }
 })
+
+export default App
