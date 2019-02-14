f30b353f84e6c36748ac7481a7a62457b235ac68
Mark Spanbroek <mark@spanbroek.net>
2019-02-14 17:00:18 +0100

    [cogito-mobile] Two screens: home & identity manager

--- workspaces/cogito-mobile/App.js
+++ workspaces/cogito-mobile/App.js
@@ -1,9 +1,31 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { NavigatorIOS, StyleSheet, Text, View } from 'react-native'
 
 const App = () => (
+  <NavigatorIOS
+    initialRoute={{ component: Home, title: 'Home' }}
+    style={{ flex: 1 }}
+  />
+)
+
+class Home extends React.Component {
+  render () {
+    return (
+      <View style={styles.container}>
+        <Text onPress={() => this.openIdentityManager()}>I am.</Text>
+      </View>
+    )
+  }
+
+  openIdentityManager () {
+    const { navigator } = this.props
+    navigator.push({ component: IdentityManager, title: 'Me, Myself and I' })
+  }
+}
+
+const IdentityManager = () => (
   <View style={styles.container}>
-    <Text>Open up App.js to start working on your app!</Text>
+    <Text>Identity Manager</Text>
   </View>
 )
 
