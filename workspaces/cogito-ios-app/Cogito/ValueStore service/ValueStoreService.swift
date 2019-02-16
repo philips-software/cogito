67c6b3f17ef34d180ccaea56e9bf74e55815595f
Marcin Czenko <marcin@everydayproductive.com>
2019-02-16 01:55:23 +0100

    [#73][cogito-ios-app] refactors ValueStoreService lower complexity

--- workspaces/cogito-ios-app/Cogito/ValueStore service/ValueStoreService.swift
+++ workspaces/cogito-ios-app/Cogito/ValueStore service/ValueStoreService.swift
@@ -9,38 +9,47 @@ struct ValueStoreService: TelepathService {
         self.store = store
     }
 
+    func addKeyValuePair(key: String, request: JsonRpcRequest, channel: TelepathChannel) {
+        if let value = request.params["value"].string {
+            store.dispatch(ValueStoreActions.Add(key: key, value: value))
+            store.dispatch(TelepathActions.Send(id: request.id, result: "success", on: channel))
+        } else {
+            store.dispatch(TelepathActions.Send(
+                id: request.id,
+                error: ValueStoreError.valueNotFound,
+                on: channel))
+        }
+    }
+
+    func getValueForKey(key: String, request: JsonRpcRequest, channel: TelepathChannel) {
+        if let value = store.state.valueStore.valueForKey(key: key) {
+            store.dispatch(TelepathActions.Send(id: request.id, result: value, on: channel))
+        } else {
+            store.dispatch(TelepathActions.Send(id: request.id, result: JSON.null, on: channel))
+        }
+    }
+
+    func deleteKey(key: String, request: JsonRpcRequest, channel: TelepathChannel) {
+        if store.state.valueStore.store[key] != nil {
+            store.dispatch(ValueStoreActions.Delete(key: key))
+            store.dispatch(TelepathActions.Send(id: request.id, result: "success", on: channel))
+        } else {
+            store.dispatch(TelepathActions.Send(id: request.id,
+                                                error: ValueStoreError.noKeyInStore,
+                                                on: channel))
+        }
+    }
+
     func onRequest(_ request: JsonRpcRequest, on channel: TelepathChannel) {
-        guard request.method == "addKeyValuePair" ||
-            request.method == "getValueForKey" ||
-            request.method == "deleteKey" else { return }
+        guard ["addKeyValuePair", "getValueForKey", "deleteKey"].contains(request.method) else { return }
         if let key = request.params["key"].string {
             switch request.method {
             case "addKeyValuePair":
-                if let value = request.params["value"].string {
-                    store.dispatch(ValueStoreActions.Add(key: key, value: value))
-                    store.dispatch(TelepathActions.Send(id: request.id, result: "success", on: channel))
-                } else {
-                    store.dispatch(TelepathActions.Send(
-                        id: request.id,
-                        error: ValueStoreError.valueNotFound,
-                        on: channel))
-                }
-
+                addKeyValuePair(key: key, request: request, channel: channel)
             case "getValueForKey":
-                if let value = store.state.valueStore.valueForKey(key: key) {
-                    store.dispatch(TelepathActions.Send(id: request.id, result: value, on: channel))
-                } else {
-                    store.dispatch(TelepathActions.Send(id: request.id, result: JSON.null, on: channel))
-                }
+                getValueForKey(key: key, request: request, channel: channel)
             case "deleteKey":
-                if store.state.valueStore.store[key] != nil {
-                    store.dispatch(ValueStoreActions.Delete(key: key))
-                    store.dispatch(TelepathActions.Send(id: request.id, result: "success", on: channel))
-                } else {
-                    store.dispatch(TelepathActions.Send(id: request.id,
-                        error: ValueStoreError.noKeyInStore,
-                        on: channel))
-                }
+                deleteKey(key: key, request: request, channel: channel)
             default:
                 break
             }
