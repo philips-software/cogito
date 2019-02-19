68dfee5bd583521fd92ad66001a629fcbb2d9a0c
Mark Spanbroek <mark@spanbroek.net>
2019-02-19 10:41:49 +0100

    [cogito-mobile] fix indentation

--- workspaces/cogito-mobile/Home.test.js
+++ workspaces/cogito-mobile/Home.test.js
@@ -4,8 +4,8 @@ import { Home } from './Home'
 import { IdentityManager } from './IdentityManager'
 
 it('navigates to the identity manager', () => {
-    const navigator = { push: jest.fn() }
-    const { getByText } = render(<Home navigator={navigator}/>)
-    fireEvent.press(getByText('I am.'))
-    expect(navigator.push).toBeCalledWith(IdentityManager.route)
+  const navigator = { push: jest.fn() }
+  const { getByText } = render(<Home navigator={navigator} />)
+  fireEvent.press(getByText('I am.'))
+  expect(navigator.push).toBeCalledWith(IdentityManager.route)
 })
