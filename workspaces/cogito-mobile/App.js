d881fb32778f602b5c2e18aa59a50c14ffd18c60
Mark Spanbroek <mark@spanbroek.net>
2019-02-20 11:27:45 +0100

    [cogito-mobile] Use react-native-screens

--- workspaces/cogito-mobile/App.js
+++ workspaces/cogito-mobile/App.js
@@ -1,6 +1,9 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation'
+import { useScreens } from 'react-native-screens'
 import { Home } from './Home'
 import { IdentityManager } from './IdentityManager'
+
+useScreens()
 
 const AppNavigator = createStackNavigator({ Home, IdentityManager }, {
   headerMode: 'float',
