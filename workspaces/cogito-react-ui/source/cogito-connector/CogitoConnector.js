2c4ec4a4a9af3f2ffb6db1f53ad8b4e9452ac6a0
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-02-22 15:48:45 +0100

    [#47] CogitoConnector automatically triggers onDone when notified through Telepath

--- workspaces/cogito-react-ui/source/cogito-connector/CogitoConnector.js
+++ workspaces/cogito-react-ui/source/cogito-connector/CogitoConnector.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types'
 
 const CogitoConnector = ({
   connectUrl,
+  telepathChannel,
   buttonStyling,
   buttonDisabled,
   buttonText,
@@ -13,31 +14,33 @@ const CogitoConnector = ({
   onDone,
   onCancel
 }) => (
-  (
-    <Modal open={open}
-      trigger={
-        <Button {...buttonStyling}
-          disabled={buttonDisabled}
-          onClick={onOpen}>
-          {buttonText || 'Show QR code'}
-        </Button>
-      }
-      onClose={onCancel}
-      closeIcon >
-      <Modal.Header>Scan QRCode</Modal.Header>
-      <Modal.Content>
-        <Modal.Description>
-          <ConnectorBody connectUrl={connectUrl}
-            buttonStyling={buttonStyling}
-            onDone={onDone} />
-        </Modal.Description>
-      </Modal.Content>
-    </Modal>
-  )
+  <Modal
+    open={open}
+    trigger={
+      <Button {...buttonStyling} disabled={buttonDisabled} onClick={onOpen}>
+        {buttonText || 'Show QR code'}
+      </Button>
+    }
+    onClose={onCancel}
+    closeIcon
+  >
+    <Modal.Header>Scan QRCode</Modal.Header>
+    <Modal.Content>
+      <Modal.Description>
+        <ConnectorBody
+          connectUrl={connectUrl}
+          telepathChannel={telepathChannel}
+          buttonStyling={buttonStyling}
+          onDone={onDone}
+        />
+      </Modal.Description>
+    </Modal.Content>
+  </Modal>
 )
 
 CogitoConnector.propTypes = {
   connectUrl: PropTypes.string.isRequired,
+  telepathChannel: PropTypes.object.isRequired,
   buttonStyling: PropTypes.object,
   buttonDisabled: PropTypes.bool,
   buttonText: PropTypes.string,
--- workspaces/cogito-react-ui/source/cogito-connector/CogitoConnector.test.js
+++ workspaces/cogito-react-ui/source/cogito-connector/CogitoConnector.test.js
@@ -1,15 +1,88 @@
 import React from 'react'
-import { render } from 'react-testing-library'
+import { render, fireEvent } from 'react-testing-library'
 import { CogitoConnector } from './CogitoConnector'
 
 describe('Cogito Connector', () => {
-  it('does not show the dialog initially', () => {
-    const { queryByText } = render(<CogitoConnector connectUrl='connectUrl' />)
-    expect(queryByText(/scan the qr code/i)).toBeNull()
+  let telepathChannel
+
+  beforeEach(() => {
+    telepathChannel = {
+      subscribeForNotifications: jest.fn(),
+      unsubscribeForNotifications: jest.fn()
+    }
   })
 
-  it('does show the dialog when opened', () => {
-    const { queryByText } = render(<CogitoConnector open connectUrl='connectUrl' />)
-    expect(queryByText(/scan the qr code/i)).not.toBeNull()
+  describe('when closed', () => {
+    let queryByText
+
+    beforeEach(() => {
+      queryByText = render(
+        <CogitoConnector
+          connectUrl='connectUrl'
+          telepathChannel={telepathChannel}
+        />
+      ).queryByText
+    })
+
+    it('does not show the dialog', () => {
+      expect(queryByText(/scan the qr code/i)).toBeNull()
+    })
+
+    it('does not subscribe', () => {
+      expect(telepathChannel.subscribeForNotifications).not.toHaveBeenCalled()
+    })
+  })
+
+  describe('when open', () => {
+    let queryByText
+    let getAllByText
+    let unmount
+    let onDone
+
+    beforeEach(() => {
+      onDone = jest.fn()
+      const renderUtils = render(
+        <CogitoConnector
+          open
+          connectUrl='connectUrl'
+          telepathChannel={telepathChannel}
+          onDone={onDone}
+        />
+      )
+      queryByText = renderUtils.queryByText
+      getAllByText = renderUtils.getAllByText
+      unmount = renderUtils.unmount
+    })
+
+    it('does show the dialog when opened', () => {
+      expect(queryByText(/scan the qr code/i)).not.toBeNull()
+    })
+
+    it('fires onDone when close button is pressed', async () => {
+      const buttons = getAllByText(/done/i)
+      expect(buttons.length).toBe(2)
+      // TODO: why are there TWO 'done' buttons? How do I select the right one?
+      buttons.forEach(b => {
+        fireEvent.click(b)
+      })
+      expect(onDone).toHaveBeenCalled()
+    })
+
+    it('subscribes for notifications', () => {
+      expect(telepathChannel.subscribeForNotifications).toHaveBeenCalled()
+    })
+
+    it('unsubscribes un unmount', () => {
+      unmount()
+      expect(telepathChannel.unsubscribeForNotifications).toHaveBeenCalled()
+    })
+
+    it('fires onDone when telepath notification is received', () => {
+      const notification = { jsonrpc: '2.0', method: 'didScanQRCode' }
+      const onNotification =
+        telepathChannel.subscribeForNotifications.mock.calls[0][0]
+      onNotification(notification)
+      expect(onDone).toHaveBeenCalled()
+    })
   })
 })
