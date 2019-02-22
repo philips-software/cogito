bb0cc3c91b0f29d46e07574d3666b42fc003bc8c
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-02-22 15:48:45 +0100

    [#47] CogitoConnector automatically triggers onDone when notified through Telepath

--- workspaces/cogito-react-ui/source/cogito-connector/ConnectorBody.js
+++ workspaces/cogito-react-ui/source/cogito-connector/ConnectorBody.js
@@ -1,18 +1,40 @@
 import React from 'react'
 import { Button, Header } from 'semantic-ui-react'
-import { Centered, Spacer } from '@react-frontend-developer/react-layout-helpers'
+import {
+  Centered,
+  Spacer
+} from '@react-frontend-developer/react-layout-helpers'
 import { CogitoQRCode } from '../CogitoQRCode'
 
-const ConnectorBody = ({ connectUrl, buttonStyling, onDone }) => (
-  <Centered>
-    <Header>Please scan the QR code below with your mobile device.</Header>
-    <Spacer
-      margin='20px 0 50px 0'
-      render={() =>
-        <CogitoQRCode key={connectUrl} connectUrl={connectUrl} />
-      } />
-    <Button {...buttonStyling} onClick={onDone}>Done</Button>
-  </Centered>
-)
+class ConnectorBody extends React.Component {
+  componentDidMount () {
+    this.subscription = this.props.telepathChannel.subscribeForNotifications(
+      notification => {
+        if (notification.method === 'didScanQRCode') {
+          this.props.onDone()
+        }
+      }
+    )
+  }
+
+  componentWillUnmount () {
+    this.props.telepathChannel.unsubscribeForNotifications(this.subscription)
+  }
+
+  render () {
+    const { connectUrl, buttonStyling, onDone } = this.props
+    return (
+      <Centered>
+        <Header>Please scan the QR code below with your mobile device.</Header>
+        <Spacer
+          margin='20px 0 50px 0'
+          render={() =>
+            <CogitoQRCode key={connectUrl} connectUrl={connectUrl} />
+          } />
+        <Button {...buttonStyling} onClick={onDone}>Done</Button>
+      </Centered>
+    )
+  }
+}
 
 export { ConnectorBody }
