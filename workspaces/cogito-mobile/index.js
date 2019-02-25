75a6c435a94da07c4fb7523e42de4492473e501b
Mark Spanbroek <mark@spanbroek.net>
2019-02-25 16:12:04 +0100

    [cogito-mobile] Use react-native-navigation

--- workspaces/cogito-mobile/index.js
+++ workspaces/cogito-mobile/index.js
@@ -1,5 +1,14 @@
-import { AppRegistry } from 'react-native'
+import { Navigation } from 'react-native-navigation'
 import App from './App'
-import { name as appName } from './app.json'
 
-AppRegistry.registerComponent(appName, () => App)
+Navigation.registerComponent('App', () => App)
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: "App"
+      }
+    }
+  })
+})
