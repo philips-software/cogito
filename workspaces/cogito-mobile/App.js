5f0c1f3f54f420e805d116baac8cfd143b26cd29
Mark Spanbroek <mark@spanbroek.net>
2019-02-25 15:02:04 +0100

    [cogito-mobile] rewrite to stateless functional component

--- workspaces/cogito-mobile/App.js
+++ workspaces/cogito-mobile/App.js
@@ -1,49 +1,19 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
-import React, { Component } from 'react'
-import { Platform, StyleSheet, Text, View } from 'react-native'
+const App = () => (
+  <View style={styles.container}>
+    <Text>Open up App.js to start working on your app!</Text>
+  </View>
+)
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu'
-})
-
-export default class App extends Component {
-  render () {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    )
-  }
-}
+export default App
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#fff',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
+    justifyContent: 'center'
   }
 })
