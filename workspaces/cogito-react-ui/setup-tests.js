db2c80e86883d51351efe6fc244470c5dbfa2573
Mark Spanbroek <mark@spanbroek.net>
2019-02-28 15:51:57 +0100

    [cogito-react-ui] Ensure that react-testing-library cleans up after itself

---
+++ workspaces/cogito-react-ui/setup-tests.js
@@ -0,0 +1,6 @@
+import 'jest-dom/extend-expect'
+import 'react-testing-library/cleanup-after-each'
+import { createSerializer } from 'jest-emotion'
+import * as emotion from 'emotion'
+
+expect.addSnapshotSerializer(createSerializer(emotion))
