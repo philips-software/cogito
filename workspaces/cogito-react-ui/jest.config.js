4ffb626421e38e811abeed554d67b2d0e820a774
Mark Spanbroek <mark@spanbroek.net>
2019-02-28 15:51:57 +0100

    [cogito-react-ui] Ensure that react-testing-library cleans up after itself

--- workspaces/cogito-react-ui/jest.config.js
+++ workspaces/cogito-react-ui/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   setupFiles: [
     'jest-canvas-mock'
   ],
+  setupTestFrameworkScriptFile: require.resolve('./setup-tests.js'),
   modulePaths: [
     '<rootDir>/../demo-app/src/'
   ]
