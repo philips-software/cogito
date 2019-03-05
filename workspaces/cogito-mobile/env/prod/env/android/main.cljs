cf16eef6bb5322a7bc47ca82d066944c0176c573
Mark Spanbroek <mark@spanbroek.net>
2019-03-05 10:00:00 +0100

    [cogito-mobile] Empty ClojureScript React Native project
    re-natal init

---
+++ workspaces/cogito-mobile/env/prod/env/android/main.cljs
@@ -0,0 +1,6 @@
+ (ns env.android.main
+  (:require [cogito.android.core :as core]))
+
+ (core/init)
+
+
---
+++ workspaces/cogito-mobile/env/prod/env/ios/main.cljs
@@ -0,0 +1,6 @@
+ (ns env.ios.main
+  (:require [cogito.ios.core :as core]))
+
+ (core/init)
+
+
