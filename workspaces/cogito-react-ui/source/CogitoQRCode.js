db75815b4925bfb747b32c50d9193b0158c1c227
Nicole Bekkers <hackybekkers@hacklor.com>
2019-03-05 12:09:47 +0100

    Simplified Cogito QR Code

--- workspaces/cogito-react-ui/source/CogitoQRCode.js
+++ workspaces/cogito-react-ui/source/CogitoQRCode.js
@@ -1,33 +1,10 @@
 import React from 'react'
 import QRCode from 'qrcode.react'
 
-class CogitoQRCode extends React.Component {
-  state = {}
-
-  componentDidMount () {
-    const canvas = document.querySelector('canvas')
-    if (canvas) {
-      this.setState({
-        canvas: {
-          url: canvas.toDataURL(),
-          width: canvas.width / 2,
-          height: canvas.height / 2
-        }
-      })
-    }
-  }
-
-  render () {
-    const { canvas } = this.state
-    return (
-      <a href={this.props.value} title={this.props.title}>
-        { !canvas && <QRCode
-          value={this.props.value}
-        /> }
-        { canvas && <img alt='qr-code' src={canvas.url} width={canvas.width} height={canvas.height} /> }
-      </a>
-    )
-  }
-}
+const CogitoQRCode = ({ url, title = 'Cogito QR Code' }) => (
+  <a href={url} title={title}>
+    <QRCode value={url} />
+  </a>
+)
 
 export { CogitoQRCode }
