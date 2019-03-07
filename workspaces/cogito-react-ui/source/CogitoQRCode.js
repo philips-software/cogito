f40e57e834d34e235057d0dbeec3f6d8c7c58e11
Nicole Bekkers <hackybekkers@hacklor.com>
2019-03-07 12:35:18 +0100

    Renaming url to connectUrl

--- workspaces/cogito-react-ui/source/CogitoQRCode.js
+++ workspaces/cogito-react-ui/source/CogitoQRCode.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import QRCode from 'qrcode.react'
 
-const CogitoQRCode = ({ url, title = 'Cogito QR Code' }) => (
-  <a href={url} title={title}>
-    <QRCode value={url} />
+const CogitoQRCode = ({ connectUrl, title = 'Cogito QR Code' }) => (
+  <a href={connectUrl} title={title}>
+    <QRCode value={connectUrl} />
   </a>
 )
 
