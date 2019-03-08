d1176a94918fbc830217f7763e98d57bbfb210e3
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-03-08 16:51:58 +0100

    [cogito-mobile] Moves home screen to separate namespace

--- workspaces/cogito-mobile/src/main/cogito/app.cljs
+++ workspaces/cogito-mobile/src/main/cogito/app.cljs
@@ -1,32 +1,14 @@
 (ns cogito.app
   (:require [reagent.core :as r :refer [atom]]
-            [cogito.identity-manager :as identity-manager]))
+            [cogito.identity-manager :as identity-manager]
+            [cogito.home :as home]))
 
-
-(def ReactNative (js/require "react-native"))
 (def ReactNativeNavigation (js/require "react-native-navigation"))
-
-(def app-registry (.-AppRegistry ReactNative))
 (def navigation (.-Navigation ReactNativeNavigation))
-(def text (r/adapt-react-class (.-Text ReactNative)))
-(def view (r/adapt-react-class (.-View ReactNative)))
-(def button (r/adapt-react-class (.-Button ReactNative)))
-
-(defn show-identity-manager [componentId]
-  (.push
-   navigation
-   componentId
-   #js {:component #js {:name "IdentityManager"
-                        :options identity-manager/push-options}}))
-
-(defn app-root [props]
-  [view {:style {:flex-direction "column" :margin 40 :align-items "center" :background-color "white"}}
-   [text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} "Hi Shadow CLJS!"]
-   [button {:title "Press me" :on-press #(show-identity-manager (:componentId props))}]])
 
 (defn init []
   (println "hello shadow")
-  (.registerComponent navigation "Cogito" #(r/reactify-component app-root))
+  (.registerComponent navigation "Home" #(r/reactify-component home/screen))
   (.registerComponent navigation "IdentityManager" #(r/reactify-component identity-manager/screen))
   (let [events (.events navigation)]
     (.registerAppLaunchedListener
@@ -37,5 +19,5 @@
                    {:stack #js
                             {:children #js [#js
                                              {:component #js
-                                                          {:name "Cogito"}}]
+                                                          {:name "Home"}}]
                              :options #js {:topBar #js {:visible "false"}}}}}))))
---
+++ workspaces/cogito-mobile/src/main/cogito/home.cljs
@@ -0,0 +1,23 @@
+(ns cogito.home
+  (:require [reagent.core :as r :refer [atom]]
+            [cogito.identity-manager :as identity-manager]))
+
+(def ReactNative (js/require "react-native"))
+(def ReactNativeNavigation (js/require "react-native-navigation"))
+(def navigation (.-Navigation ReactNativeNavigation))
+
+(def text (r/adapt-react-class (.-Text ReactNative)))
+(def view (r/adapt-react-class (.-View ReactNative)))
+(def button (r/adapt-react-class (.-Button ReactNative)))
+
+(defn show-identity-manager [componentId]
+  (.push
+   navigation
+   componentId
+   #js {:component #js {:name "IdentityManager"
+                        :options identity-manager/push-options}}))
+
+(defn screen [props]
+  [view {:style {:flex-direction "column" :margin 40 :align-items "center" :background-color "white"}}
+   [text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} "Hi Shadow CLJS!"]
+   [button {:title "Press me" :on-press #(show-identity-manager (:componentId props))}]])
