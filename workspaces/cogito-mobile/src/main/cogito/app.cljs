68a41ed57c581e20c426f1cfdd1faa5236b73915
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-03-08 15:57:26 +0100

    [cogito-mobile] Adds ability to push new screen

--- workspaces/cogito-mobile/src/main/cogito/app.cljs
+++ workspaces/cogito-mobile/src/main/cogito/app.cljs
@@ -10,20 +10,35 @@
 (def view (r/adapt-react-class (.-View ReactNative)))
 (def button (r/adapt-react-class (.-Button ReactNative)))
 
-(defn alert [title]
-  (.alert (.-Alert ReactNative) title))
+(defn identity-manager []
+  [view {:style {:flex-direction "column" :margin 40 :align-items "center" :background-color "white"}}
+   [text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} "Hi Shadow CLJS!"]])
 
-(defn app-root []
+(defn push [componentId]
+  (.push
+   navigation
+   componentId
+   #js {:component #js {:name "IdentityManager"
+                        :options #js {:topBar #js {:visible "true"
+                                                   :title #js {:text "Me, myself and I"}}}}}))
+
+(defn app-root [props]
   [view {:style {:flex-direction "column" :margin 40 :align-items "center" :background-color "white"}}
    [text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} "Hi Shadow CLJS!"]
-   [button {:title "Press me" :on-press #(alert "Hello!")}]])
+   [button {:title "Press me" :on-press #(push (:componentId props))}]])
 
 (defn init []
   (println "hello shadow")
   (.registerComponent navigation "Cogito" #(r/reactify-component app-root))
+  (.registerComponent navigation "IdentityManager" #(r/reactify-component identity-manager))
   (let [events (.events navigation)]
     (.registerAppLaunchedListener
      events
      #(.setRoot
        navigation
-       #js {:root #js {:stack #js {:children #js [#js {:component #js {:name "Cogito" :options #js {:topbar #js {:title #js {:text "Cogito"}}}}}]}}}))))
+       #js {:root #js
+                   {:stack #js
+                            {:children #js [#js
+                                             {:component #js
+                                                          {:name "Cogito"}}]
+                             :options #js {:topBar #js {:visible "false"}}}}}))))
