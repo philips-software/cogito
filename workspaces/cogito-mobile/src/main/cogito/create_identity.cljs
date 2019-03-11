81d4fc6672e96f56da2dac0f6e7e30f2c81a7e33
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-03-11 10:07:15 +0100

    [cogito-mobile] Renames file to match convention and reformats

--- workspaces/cogito-mobile/src/main/cogito/create_identity.cljs
+++ workspaces/cogito-mobile/src/main/cogito/create_identity.cljs
@@ -4,13 +4,22 @@
 (def ReactNative (js/require "react-native"))
 (def view (r/adapt-react-class (.-View ReactNative)))
 (def text (r/adapt-react-class (.-Text ReactNative)))
+(def platform (.-Platform ReactNative))
 
 (def ReactNativeNavigation (js/require "react-native-navigation"))
 (def navigation (.-Navigation ReactNativeNavigation))
 
 (defn screen-layout []
-  [view {:style {:flex-direction "column" :margin 40 :align-items "center" :background-color "white"}}
-   [text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} "Create Identity"]])
+  [view {:style {:flex-direction "column"
+                 :margin 40
+                 :align-items "center"
+                 :background-color "white"}}
+
+   [text {:style {:font-size 30
+                  :font-weight "100"
+                  :margin-bottom 20
+                  :text-align "center"}}
+    "Create Identity"]])
 
 (defn screen []
   (r/create-class
@@ -24,7 +33,9 @@
     :reagent-render screen-layout
 
     :navigation-button-pressed
-    (fn [props] (.dismissModal navigation (.-componentId props)))}))
+    (fn [props]
+      (println (.-OS platform))
+      (.dismissModal navigation (.-componentId props)))}))
 
 (defn toolbar-button [id] {:id id :systemItem id})
 
