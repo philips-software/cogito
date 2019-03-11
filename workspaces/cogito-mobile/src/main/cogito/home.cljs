fd8daf118cb1ade7ae0cc27e64899535c8adc759
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-03-11 10:07:21 +0100

    [cogito-mobile] Reformat

--- workspaces/cogito-mobile/src/main/cogito/home.cljs
+++ workspaces/cogito-mobile/src/main/cogito/home.cljs
@@ -18,6 +18,16 @@
                         :options identity-manager/push-options}}))
 
 (defn screen [props]
-  [view {:style {:flex-direction "column" :margin 40 :align-items "center" :background-color "white"}}
-   [text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} "Hi Shadow CLJS!"]
-   [button {:title "Press me" :on-press #(show-identity-manager (:componentId props))}]])
+  [view {:style {:flex-direction "column"
+                 :margin 40
+                 :align-items "center"
+                 :background-color "white"}}
+
+   [text {:style {:font-size 30
+                  :font-weight "100"
+                  :margin-bottom 20
+                  :text-align "center"}}
+    "Hi Shadow CLJS!"]
+
+   [button {:title "Press me"
+            :on-press #(show-identity-manager (:componentId props))}]])
