d7c20eae7494fd4af155ed5a34e15f5f17a76101
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-03-11 16:22:20 +0100

    [cogito-mobile] Adds react-native-navigation support to Android build

--- workspaces/cogito-mobile/android/app/src/main/java/com/cogito/MainApplication.java
+++ workspaces/cogito-mobile/android/app/src/main/java/com/cogito/MainApplication.java
@@ -1,45 +1,40 @@
 package com.cogito;
 
-import android.app.Application;
-
-import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.reactnativenavigation.NavigationApplication;
+import com.reactnativenavigation.react.NavigationReactNativeHost;
+import com.reactnativenavigation.react.ReactGateway;
+
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+import static java.util.Collections.emptyList;
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
-
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
-      );
-    }
-
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
-  };
+public class MainApplication extends NavigationApplication {
 
   @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
+  protected ReactGateway createReactGateway() {
+    ReactNativeHost host = new NavigationReactNativeHost(this, isDebug(), createAdditionalReactPackages()) {
+      @Override
+      protected String getJSMainModuleName() {
+        return "index";
+      }
+    };
+    return new ReactGateway(this, isDebug(), host);
   }
 
   @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+  public boolean isDebug() {
+    return BuildConfig.DEBUG;
+  }
+
+  @Override
+  public List<ReactPackage> createAdditionalReactPackages() {
+    return emptyList();
   }
 }
