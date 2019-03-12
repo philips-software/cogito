523ba27a93269470dc2ed7eddf0891ca0e77f12b
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-03-12 10:22:12 +0100

    [cogito-mobile] Puts "Home" in a simple wrapper

--- workspaces/cogito-mobile/src/main/cogito/app.cljs
+++ workspaces/cogito-mobile/src/main/cogito/app.cljs
@@ -3,12 +3,26 @@
    [reagent.core :as r :refer [atom]]
    ["react-native" :as ReactNative]
    ["react-native-navigation" :as ReactNativeNavigation :refer (Navigation)]
+   ["create-react-class" :as crc]
    [cogito.home :as home]
    [cogito.identity-manager :as identity-manager]
    [cogito.create-identity :as create-identity]))
 
+(defn reload-wrapper [component-name root]
+  (let [wrapper (r/create-class
+                 {:display-name "reload-wrapper"
+
+                  :get-initial-state
+                  (fn [] (print "new wrapper"))
+
+                  :reagent-render root})]
+    (.registerComponent Navigation
+                        component-name
+                        (fn [] wrapper)
+                        #(r/reactify-component root))))
+
 (defn init {:dev/after-load true} []
-  (.registerComponent Navigation "Home" #(r/reactify-component home/screen))
+  (reload-wrapper "Home" home/screen)
   (.registerComponent Navigation "IdentityManager" identity-manager/screen)
   (.registerComponent Navigation "CreateIdentity" create-identity/screen)
 
