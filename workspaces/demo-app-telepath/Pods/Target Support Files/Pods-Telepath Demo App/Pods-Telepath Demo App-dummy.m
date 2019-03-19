994d36f0d339c0193e323848cacc46c556399ff5
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-03-19 10:33:09 +0100

    [#47] [demo-app-telepath] This demo app is no longer needed; removed

--- workspaces/demo-app-telepath/Pods/Target Support Files/Pods-Telepath Demo App/Pods-Telepath Demo App-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Telepath_Demo_App : NSObject
-@end
-@implementation PodsDummy_Pods_Telepath_Demo_App
-@end
--- workspaces/demo-app-telepath/Pods/Target Support Files/base64url/base64url-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_base64url : NSObject
-@end
-@implementation PodsDummy_base64url
-@end
