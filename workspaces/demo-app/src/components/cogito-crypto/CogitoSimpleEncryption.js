6d9663b6cd5b7cda7bdbc0d510de13e47588b6b3
Nicole Bekkers <hackybekkers@hacklor.com>
2019-03-21 10:48:59 +0100

    [demo-app] Extract CogitoSimpleEncryptionView without WithStore

--- workspaces/demo-app/src/components/cogito-crypto/CogitoSimpleEncryption.js
+++ workspaces/demo-app/src/components/cogito-crypto/CogitoSimpleEncryption.js
@@ -8,67 +8,72 @@ import {
 import { EncryptionActions } from 'encryption-state'
 import { WithStore } from '@react-frontend-developer/react-redux-render-prop'
 
-class CogitoSimpleEncryption extends React.Component {
-  render () {
-    return (
-      <WithStore selector={state => state.encryption}>
-        {
-          ({ plainText, cipherText }, dispatch) => (
-            <Centered>
-              <EncryptionGrid>
-                <PlainTextGridItem>
-                  <TextInput
-                    placeholder='Enter some text'
-                    value={plainText}
-                    onChange={(event) => dispatch(
-                      EncryptionActions.setPlainText(event.target.value)
-                    )}
-                  />
-                </PlainTextGridItem>
-                <EncryptGridItem>
-                  <Button
-                    secondary color='black'
-                    onClick={() => dispatch(
-                      EncryptionActions.encrypt({
-                        telepathChannel: this.props.telepathChannel
-                      })
-                    )}
-                  >
-                    ―Encrypt→
-                  </Button>
-                </EncryptGridItem>
-                <DecryptGridItem>
-                  <Button
-                    secondary color='black'
-                    onClick={() => dispatch(
-                      EncryptionActions.decrypt({
-                        telepathChannel: this.props.telepathChannel
-                      })
-                    )}
-                  >
-                   ←Decrypt―
-                  </Button>
-                </DecryptGridItem>
-                <CipherTextGridItem>
-                  <TextInput
-                    placeholder='Encrypted text appears here'
-                    value={cipherText}
-                    onChange={(event) => dispatch(
-                      EncryptionActions.setCipherText(event.target.value)
-                    )}
-                  />
-                </CipherTextGridItem>
-              </EncryptionGrid>
-            </Centered>
-          )
-        }
-      </WithStore>
-    )
-  }
-}
+const CogitoSimpleEncryption = ({ telepathChannel }) => (
+  <WithStore selector={state => state.encryption}>
+    {
+      ({ plainText, cipherText }, dispatch) => (
+        <CogitoSimpleEncryptionView
+          telepathChannel={telepathChannel}
+          plainText={plainText}
+          cipherText={cipherText}
+          dispatch={dispatch}
+        />
+      )
+    }
+  </WithStore>
+)
+
+const CogitoSimpleEncryptionView = ({ telepathChannel, plainText, cipherText, dispatch }) => (
+  <Centered>
+    <EncryptionGrid>
+      <PlainTextGridItem>
+        <TextInput
+          placeholder='Enter some text'
+          value={plainText}
+          onChange={(event) => dispatch(
+            EncryptionActions.setPlainText(event.target.value)
+          )}
+        />
+      </PlainTextGridItem>
+      <EncryptGridItem>
+        <Button
+          secondary color='black'
+          onClick={() => dispatch(
+            EncryptionActions.encrypt({
+              telepathChannel: telepathChannel
+            })
+          )}
+        >
+          ―Encrypt→
+        </Button>
+      </EncryptGridItem>
+      <DecryptGridItem>
+        <Button
+          secondary color='black'
+          onClick={() => dispatch(
+            EncryptionActions.decrypt({
+              telepathChannel: telepathChannel
+            })
+          )}
+        >
+         ←Decrypt―
+        </Button>
+      </DecryptGridItem>
+      <CipherTextGridItem>
+        <TextInput
+          placeholder='Encrypted text appears here'
+          value={cipherText}
+          onChange={(event) => dispatch(
+            EncryptionActions.setCipherText(event.target.value)
+          )}
+        />
+      </CipherTextGridItem>
+    </EncryptionGrid>
+  </Centered>
+)
 
 const TextInput = ({ ...args }) => (
   <TextArea {...args} style={{ width: '13rem', height: '10rem' }} />
 )
 
-export { CogitoSimpleEncryption }
+export { CogitoSimpleEncryption, CogitoSimpleEncryptionView }
