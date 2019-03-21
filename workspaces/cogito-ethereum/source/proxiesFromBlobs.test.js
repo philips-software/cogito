0baeb2f0ad2b79e1778240b3b716def5d937d10c
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-03-21 15:23:45 +0100

    [#47] [cogito-ethereum] Rewrites tests not to go through all layers of production code

--- workspaces/cogito-ethereum/source/proxiesFromBlobs.test.js
+++ workspaces/cogito-ethereum/source/proxiesFromBlobs.test.js
@@ -1,34 +1,32 @@
-import { SimpleStorage } from '@cogitojs/demo-app-contracts'
-import { EthereumForSimpleStorage } from 'test-helpers'
 import { proxiesFromBlobs } from './proxiesFromBlobs'
+import initContract from 'truffle-contract'
+
+jest.mock('truffle-contract')
 
 describe('proxiesFromBlobs', () => {
   let blobs
   let web3
+  let contractProxy
 
-  const setupContractsBlobs = () => {
-    const baseName = SimpleStorage().contractName
-    blobs = [1, 2, 3].map(i => {
-      return { ...SimpleStorage(), contractName: `${baseName}-${i}` }
-    })
-  }
+  const SimpleStorage = () => ({
+    contractName: 'SimpleStorage',
+    deployed: jest.fn()
+  })
 
-  const stubWeb3 = () => {
+  beforeEach(() => {
+    blobs = [SimpleStorage()]
+    contractProxy = {
+      setProvider: jest.fn()
+    }
     web3 = {
       currentProvider: {}
     }
-  }
-
-  beforeEach(() => {
-    setupContractsBlobs()
-    stubWeb3()
+    initContract.mockReturnValue(contractProxy)
   })
 
   it('each proxy is keyed by the contract name', async () => {
     const proxies = proxiesFromBlobs(blobs, web3)
-
     const expectedKeys = blobs.map(b => b.contractName)
-
     expect(Object.keys(proxies)).toEqual(expectedKeys)
   })
 
@@ -44,36 +42,8 @@ describe('proxiesFromBlobs', () => {
     expect(proxiesFromBlobs(blobs)).toEqual({})
   })
 
-  describe('given deployed contracts', () => {
-    let ethereum
-
-    beforeEach(async () => {
-      console.log = jest.fn()
-      ethereum = await EthereumForSimpleStorage.setup({ contractsBlobs: blobs })
-    }, 10000)
-
-    afterEach(() => {
-      console.log.mockRestore()
-    })
-
-    const increment = async (proxy, index) => {
-      const contractInstance = await proxy.deployed()
-      await contractInstance.increase(index + 1, { from: ethereum.address })
-      return (await contractInstance.read()).toNumber()
-    }
-
-    it('takes a list with contract JSON blobs and returns object with proxies', async () => {
-      const proxies = proxiesFromBlobs(ethereum.deployedJSONs, ethereum.cogitoWeb3)
-
-      // this is a bit tricky - cogito-web3-provider requires that that transactions from the same address
-      // are submitted synchronously - otherwise we will get problems with nonces
-      // TODO: we should document it in the cogito-web3-provider documentation
-      const increments = await Object.values(proxies).reduce(async (promise, proxy, index) => {
-        const increments = await promise
-        return [ ...increments, await increment(proxy, index) ]
-      }, Promise.resolve([]))
-
-      expect(increments).toEqual([1, 2, 3])
-    }, 10000)
+  it('takes a list with contract JSONs and returns proxies', async () => {
+    const proxies = proxiesFromBlobs(blobs, web3)
+    expect(proxies['SimpleStorage']).toEqual(contractProxy)
   })
 })
