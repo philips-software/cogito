cae682657fc9d04173ecfcba88234c4229b863ac
Nicole Bekkers <hackybekkers@hacklor.com>
2019-03-21 11:41:37 +0100

    [demo-app] Test that the encrypt action is called

---
+++ workspaces/demo-app/src/components/cogito-crypto/CogitoSimpleEncryption.test.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { render, fireEvent } from 'react-testing-library'
+import { EncryptionActions } from 'encryption-state'
+import { CogitoSimpleEncryptionView } from './CogitoSimpleEncryption'
+
+describe('encryption', () => {
+  const telepathChannel = 'Some telepath Channel'
+  let original, dispatch
+
+  beforeEach(() => {
+    dispatch = jest.fn()
+    original = EncryptionActions.encrypt
+
+    EncryptionActions.encrypt = jest.fn(
+      ({ telepathChannel }) => ({ type: 'ENCRYPT', telepathChannel })
+    )
+  })
+
+  afterEach(() => {
+    EncryptionActions.encrypt = original
+  })
+
+  it('calls the encrypt action', () => {
+    const { getByText } = render(
+      <CogitoSimpleEncryptionView dispatch={dispatch} telepathChannel={telepathChannel} />
+    )
+    fireEvent.click(getByText('―Encrypt→'))
+
+    expect(dispatch).toBeCalledWith(EncryptionActions.encrypt({ telepathChannel }))
+  })
+})
