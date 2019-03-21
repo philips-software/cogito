66f4439f4ae5995c4559929ee0b1bdd564e30fa4
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-03-21 15:00:54 +0100

    [#47] [cogito-ethereum] Uses telepath mock and rewrites tests not to go through all layers of production code

--- workspaces/cogito-ethereum/source/CogitoEthereum.test.js
+++ workspaces/cogito-ethereum/source/CogitoEthereum.test.js
@@ -1,81 +1,41 @@
-import nock from 'nock'
-import base64url from 'base64url'
-import { encrypt, decrypt, nonceSize as getNonceSize } from '@cogitojs/crypto'
-import { EthereumForSimpleStorage } from 'test-helpers'
-import { SimpleStorage } from '@cogitojs/demo-app-contracts'
 import { CogitoEthereum } from './'
+import { proxiesFromBlobs } from './proxiesFromBlobs'
+
+jest.mock('./proxiesFromBlobs')
 
 describe('CogitoEthereum', () => {
   const exampleTelepathId = 'IDN3oO-6rGSyqpMFDC6EfCQC'
-  const exampleTelepathKey = new Uint8Array([176, 8, 86, 89, 0, 33, 4, 124, 240, 249, 253, 251, 147, 56, 138, 54, 84, 144, 150, 125, 89, 4, 6, 6, 217, 246, 16, 163, 188, 247, 113, 134])
-  const blobs = [ SimpleStorage() ]
+  // prettier-ignore
+  const exampleTelepathKey = new Uint8Array([
+    176, 8, 86, 89, 0, 33, 4, 124, 240, 249, 253, 251, 147, 56, 138, 54,
+    84, 144, 150, 125, 89, 4, 6, 6, 217, 246, 16, 163, 188, 247, 113, 134
+  ])
+
+  const SimpleStorage = () => ({
+    contractName: jest.fn(),
+    deployed: jest.fn()
+  })
+
+  const blobs = [SimpleStorage()]
   const appName = 'Cogito Demo App'
   let ethereum
-  let channelResponseCallback
+  let cogitoEthereum
 
-  const decodeChannelRequest = async (body, telepathChannel) => {
-    const nonceSize = await getNonceSize()
-    const nonceAndCypherText = new Uint8Array(base64url.toBuffer(body))
-    const nonce = nonceAndCypherText.slice(0, nonceSize)
-    const cypherText = nonceAndCypherText.slice(nonceSize)
-    const requestString = await decrypt(
-      cypherText,
-      nonce,
-      telepathChannel.key,
-      'text'
-    )
-    return {
-      request: JSON.parse(requestString),
-      nonce
-    }
-  }
-
-  const encodeChannelResponse = async ({ request, nonce }, telepathChannel) => {
-    const response = {
+  const prepareChannel = telepathChannel => {
+    telepathChannel.setMockedReceive({
       jsonrpc: '2.0',
-      id: request.id,
+      id: 42,
       result: [ethereum.address]
-    }
-    const responseString = JSON.stringify(response)
-    const plainText = new Uint8Array(Buffer.from(responseString))
-    const cypherText = await encrypt(plainText, nonce, telepathChannel.key)
-    const nonceAndCypherText = Buffer.concat([
-      Buffer.from(nonce),
-      Buffer.from(cypherText)
-    ])
-    return base64url.encode(Buffer.from(nonceAndCypherText))
-  }
-
-  const prepareChannelRequest = (telepathChannel, baseUrl) => {
-    return nock(baseUrl)
-      .post(`/${telepathChannel.id}.red`, async body => {
-        const request = await decodeChannelRequest(body, telepathChannel)
-        const encodedResponse = await encodeChannelResponse(
-          request,
-          telepathChannel
-        )
-        expect(channelResponseCallback).toBeDefined()
-        channelResponseCallback(null, [200, encodedResponse])
-      })
-      .reply(200)
-  }
-
-  const prepareChannelResponse = (telepathChannel, baseUrl) => {
-    nock(baseUrl)
-      .get(`/${telepathChannel.id}.blue`)
-      .reply(function (uri, requestBody, cb) {
-        channelResponseCallback = cb
-      })
-  }
-
-  const prepareChannel = (telepathChannel, baseUrl) => {
-    prepareChannelResponse(telepathChannel, baseUrl)
-    return prepareChannelRequest(telepathChannel, baseUrl)
+    })
   }
 
   beforeEach(() => {
-    ethereum = new EthereumForSimpleStorage({ appName })
+    ethereum = {
+      address: '0x9C66B52bb71DD7E146944DFdad5784Bf7217A6B6',
+      useTelepathChannel: jest.fn()
+    }
     console.log = jest.fn()
+    cogitoEthereum = new CogitoEthereum(blobs)
   })
 
   afterEach(() => {
@@ -90,43 +50,38 @@ describe('CogitoEthereum', () => {
       appName
     })
 
-    const post = prepareChannel(telepathChannel, baseUrl)
+    prepareChannel(telepathChannel)
 
-    const accounts = await cogitoWeb3.eth.getAccounts()
-    expect(post.isDone()).toBeTruthy()
-    expect(accounts).toEqual([ethereum.address])
+    await cogitoWeb3.eth.getAccounts()
+    expect(telepathChannel.sent.method).toBe('accounts')
   })
 
   it('uses default telepath queuing service url if not provided', async () => {
     const baseUrl = 'https://telepath.cogito.mobi'
-    const cogitoEthereum = new CogitoEthereum(blobs)
-
-    const { cogitoWeb3, telepathChannel } = await cogitoEthereum.getContext({
-      appName
-    })
-
-    const post = prepareChannel(telepathChannel, baseUrl)
-
-    const accounts = await cogitoWeb3.eth.getAccounts()
-    expect(post.isDone()).toBeTruthy()
-    expect(accounts).toEqual([ethereum.address])
+    expect(cogitoEthereum.telepath.serviceUrl).toBe(baseUrl)
   })
 
   it('provides Web3 object with CogitoProvider', async () => {
-    const cogitoEthereum = new CogitoEthereum(blobs)
-
     const { cogitoWeb3, telepathChannel } = await cogitoEthereum.getContext({
       appName
     })
 
-    ethereum.useTelepathChannel(telepathChannel)
+    prepareChannel(telepathChannel)
 
     expect(await cogitoWeb3.eth.getAccounts()).toEqual([ethereum.address])
   })
 
   it('provides a new telepath channel when no channel id and key are provided', async () => {
-    const cogitoEthereum = new CogitoEthereum(blobs)
+    const { telepathChannel } = await cogitoEthereum.getContext({
+      appName
+    })
 
+    expect(telepathChannel.id).toBeDefined()
+    expect(telepathChannel.key).toBeDefined()
+    expect(telepathChannel.appName).toBe(appName)
+  })
+
+  it('provides a telepath channel with provided channel id and key', async () => {
     const { telepathChannel } = await cogitoEthereum.getContext({
       channelId: exampleTelepathId,
       channelKey: exampleTelepathKey,
@@ -138,42 +93,14 @@ describe('CogitoEthereum', () => {
     expect(telepathChannel.appName).toBe(appName)
   })
 
-  it('provides a telepath channel with provided channel id and key', async () => {
-    const cogitoEthereum = new CogitoEthereum(blobs)
-
-    const { telepathChannel } = await cogitoEthereum.getContext({
-      appName
-    })
-
-    expect(telepathChannel.id).toBeDefined()
-    expect(telepathChannel.key).toBeDefined()
-    expect(telepathChannel.appName).toBe(appName)
-  })
-
   describe('when contracts are deployed', () => {
-    const increment = 5
-    let cogitoEthereum
-
-    beforeEach(async () => {
-      ethereum = await EthereumForSimpleStorage.setup()
-      cogitoEthereum = new CogitoEthereum([ethereum.simpleStorageBlob])
-    })
-
     it('can execute contract using provided proxy and cogito provider', async () => {
-      const {
-        contractsProxies,
-        telepathChannel
-      } = await cogitoEthereum.getContext({
+      const { telepathChannel, cogitoWeb3 } = await cogitoEthereum.getContext({
         appName
       })
 
       ethereum.useTelepathChannel(telepathChannel)
-
-      const simpleStorage = await contractsProxies.SimpleStorage.deployed()
-      await simpleStorage.increase(increment, { from: ethereum.address })
-      const value = (await simpleStorage.read()).toNumber()
-
-      expect(value).toBe(5)
+      expect(proxiesFromBlobs).toBeCalledWith(expect.anything(), cogitoWeb3)
     })
   })
 })
