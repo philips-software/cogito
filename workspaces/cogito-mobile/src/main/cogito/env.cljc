85f9ae7824411913a476fca3bd0689b3f9126f6c
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-03-22 16:05:29 +0100

    [cogito-mobile] [wip] Starts building `env`

--- workspaces/cogito-mobile/src/main/cogito/env.cljc
+++ workspaces/cogito-mobile/src/main/cogito/env.cljc
@@ -1,11 +1,24 @@
 (ns cogito.env
-  (:require #?@(:test []
-                :cljs [[cogito.react-native-navigation-bridge :as rnnbridge]])))
+  (:require
+   ["create-react-class" :as crc]
+   #?@(:test []
+       :cljs [[cogito.react-native-navigation-bridge :as rnnbridge]])))
 
 (declare register-component)
+(declare create-wrapper)
 
 (defn register [key]
-  (register-component key nil))
+  (register-component key (create-wrapper key)))
+
+(defn create-wrapper [key]
+  (crc #js
+        {:getInitialState
+         (fn []
+           #js {:key key
+                :id 1})
+
+         :render
+         (fn [] (println "foo"))}))
 
 #?(:test
    (defn register-component [key component])
--- workspaces/cogito-mobile/src/test/cogito/env_test.cljs
+++ workspaces/cogito-mobile/src/test/cogito/env_test.cljs
@@ -1,12 +1,43 @@
 (ns cogito.env-test
   (:require [cljs.test :refer (deftest is testing)]
-            [cogito.env :refer (register register-component)]))
+            ["create-react-class" :as crc]
+            [cogito.env :refer (register register-component create-wrapper)]))
 
 (deftest register-test
-  (testing "it calls register-component with the right key"
-    (let [registered-key (atom nil)]
+  (testing "it calls register-component and create-wrapper"
+    (let [registered-key (atom nil)
+          create-wrapper-count (atom 0)]
       (with-redefs [register-component
                     (fn [key component]
-                      (reset! registered-key key))]
+                      (reset! registered-key key))
+
+                    create-wrapper
+                    (fn [key]
+                      (swap! create-wrapper-count inc))]
         (register "Home")
-        (is (= "Home" @registered-key))))))
+        (is (= "Home" @registered-key))
+        (is (= 1 @create-wrapper-count))))))
+
+(deftest create-wrapper-test
+  (testing "creates a react component"
+    (let [crc-arg (atom nil)]
+      (with-redefs [crc
+                    (fn [js-struct] (reset! crc-arg js-struct))]
+        (create-wrapper "MyComponent")
+        (is (fn? (-> @crc-arg .-render)))))) ;; has a 'render' method
+
+  (testing "stores key in initial state"
+    (let [crc-arg (atom nil)]
+      (with-redefs [crc
+                    (fn [js-struct] (reset! crc-arg js-struct))]
+        (create-wrapper "MyComponent")
+        (let [initialState (.getInitialState @crc-arg)]
+          (is (= (-> initialState .-key) "MyComponent"))))))
+
+  (testing "stores incrementing id in initial state"
+    (let [crc-arg (atom nil)]
+      (with-redefs [crc
+                    (fn [js-struct] (reset! crc-arg js-struct))]
+        (create-wrapper "MyComponent")
+        (let [initialState (.getInitialState @crc-arg)]
+          (is (= (-> initialState .-id) 1)))))))
