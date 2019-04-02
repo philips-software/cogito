0b6ff173e9eee496f222c391747a9a64761026d7
Nicole Bekkers <hackybekkers@hacklor.com>
2019-04-02 11:58:40 +0200

    [demo-app] Reintroduce setCipherText action
    It was used in the onChange

--- workspaces/demo-app/src/encryption-state/reducer.js
+++ workspaces/demo-app/src/encryption-state/reducer.js
@@ -2,6 +2,8 @@ const encryptionReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'ENCRYPTION_SET_PLAINTEXT':
       return { ...state, plainText: action.plainText }
+    case 'ENCRYPTION_SET_CIPHERTEXT':
+      return { ...state, cipherText: action.cipherText }
     case 'ENCRYPTION_SET_KEY_TAG':
       return { ...state, keyTag: action.tag }
     case 'ENCRYPTION_PENDING':
