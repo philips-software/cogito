6c223f3dcb2017c5f1ac23f6ed29957ea1d35073
Nicole Bekkers <nicole.bekkers@philips.com>
2019-04-04 11:13:32 +0200

    [telepath-js] Propagate error message from server

--- workspaces/telepath-js/source/queuing-service.js
+++ workspaces/telepath-js/source/queuing-service.js
@@ -13,14 +13,18 @@ class QueuingService {
     const body = base64url.encode(Buffer.from(message))
     const response = await fetch(new Request(url, { method: 'POST', body }))
     if (!response.ok) {
-      throw new Error(`sending failed (${response.status})`)
+      const status = response.status
+      const message = await response.text()
+      throw new Error(`Failed to send message (${status}): ${message}`)
     }
   }
 
   async receive (queueId) {
     const response = await fetch(`${this.baseUrl}/${queueId}`)
     if (!response.ok) {
-      throw new Error(`receiving failed (${response.status})`)
+      const status = response.status
+      const message = await response.text()
+      throw new Error(`Failed to receive message (${status}): ${message}`)
     }
     if (response.status === 204) {
       return null
