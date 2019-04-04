29ca752f517d57646929f6540e92fffb0af07d42
Jeroen Knoops <jeroen.knoops@gmail.com>
2019-04-04 12:04:09 +0200

    Reverts change in jest timeout settings

--- workspaces/demo-app/setup-tests.js
+++ workspaces/demo-app/setup-tests.js
@@ -4,4 +4,4 @@ import { createSerializer } from 'jest-emotion'
 import * as emotion from 'emotion'
 
 expect.addSnapshotSerializer(createSerializer(emotion))
-jest.setTimeout(9000)
+jest.setTimeout(7000)
