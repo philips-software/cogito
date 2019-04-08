fbd370a02ebbe37cf590a29520dea05066fab557
Nicole Bekkers <hackybekkers@hacklor.com>
2019-04-08 11:39:32 +0200

    [demo-app] WIP: Refactoring TelepathError

--- workspaces/demo-app/src/components/telepath/TelepathError.test.js
+++ workspaces/demo-app/src/components/telepath/TelepathError.test.js
@@ -2,9 +2,39 @@ import React from 'react'
 import { render } from 'react-testing-library'
 import { TelepathError } from './TelepathError'
 import { EventWaiter } from 'test-helpers'
+import MockDate from 'mockdate'
+
+// Test empty ontimeout, default function
+// no timeout, should show error 'forever'
+describe('timed test', () => {
+  const now = Date.now()
+
+  beforeEach(() => {
+    jest.useFakeTimers()
+    MockDate.set(now)
+  })
+
+  afterEach(() => {
+    MockDate.reset()
+    jest.restoreAllMocks()
+  })
+
+  it("doesn't require onTimeout, takes default", () => {
+    //   const { debug } = render(
+    //     <TelepathError>{errorDescription}</TelepathError>
+    //   )
+
+    //   const threeSecondsFromNow = now + 3000
+    //   MockDate.set(new Date(threeSecondsFromNow))
+    jest.runOnlyPendingTimers()
+
+    //   debug()
+  })
+})
 
 describe('TelepathError', () => {
   const errorDescription = 'error description'
+
   it('displays provied error description', () => {
     const { getByText } = render(<TelepathError error={errorDescription} />)
     expect(getByText(errorDescription)).toBeInTheDocument()
