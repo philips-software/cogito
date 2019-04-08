fa8e61da00e7eb4853e572b6f0c05a4cfb4ede47
Nicole Bekkers <hackybekkers@hacklor.com>
2019-04-08 11:50:27 +0200

    [telepath-js] Remove duplication

--- workspaces/telepath-js/source/queuing-service.js
+++ workspaces/telepath-js/source/queuing-service.js
@@ -12,24 +12,24 @@ class QueuingService {
     const url = `${this.baseUrl}/${queueId}`
     const body = base64url.encode(Buffer.from(message))
     const response = await fetch(new Request(url, { method: 'POST', body }))
-    if (!response.ok) {
-      const status = response.status
-      const message = await response.text()
-      throw new Error(`Failed to send message (${status}): ${message}`)
-    }
+    await verifyResponseOk({ response, message: 'Failed to send message' })
   }
 
   async receive (queueId) {
     const response = await fetch(`${this.baseUrl}/${queueId}`)
-    if (!response.ok) {
-      const status = response.status
-      const message = await response.text()
-      throw new Error(`Failed to receive message (${status}): ${message}`)
-    }
+    await verifyResponseOk({ response, message: 'Failed to receive message' })
     if (response.status === 204) {
       return null
     }
     return base64url.toBuffer(await response.text())
+  }
+}
+
+const verifyResponseOk = async ({ response, message }) => {
+  if (!response.ok) {
+    const status = response.status
+    const text = await response.text()
+    throw new Error(`${message} (${status}): ${text}`)
   }
 }
 
