2764ebd54d05ed2772cb457bdfb7571da69c9bbb
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-05-11 14:30:41 +0200

    [cogito-mobile] Adds redux store

---
+++ workspaces/cogito-mobile/store/store.js
@@ -0,0 +1,26 @@
+import { configureStore, getDefaultMiddleware } from 'redux-starter-kit'
+import { identityReducer } from '../identity-manager/reducer'
+import logger from 'redux-logger'
+
+function configureAppStore (preloadedState) {
+  const middleware = getDefaultMiddleware()
+  if (process.env.NODE_ENV === 'development') {
+    console.log('add redux logger middleware')
+    middleware.push(logger)
+  }
+  const store = configureStore({
+    reducer: { identity: identityReducer },
+    middleware,
+    preloadedState
+  })
+
+  // TODO: find out how to do this when you don't have a rootReducer explicitly
+  // if (process.env.NODE_ENV === 'development' && module.hot) {
+  //   module.hot.accept('./reducers', () => store.replaceReducer(rootReducer))
+  // }
+
+  return store
+}
+
+const preloadedState = {}
+export const store = configureAppStore(preloadedState)
