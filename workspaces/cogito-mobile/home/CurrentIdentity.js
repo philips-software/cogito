7685ea5c315c195f345d41320dcaef0edd87e231
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-05-11 15:08:10 +0200

    [cogito-mobile] Displays identity from store if available

--- workspaces/cogito-mobile/home/CurrentIdentity.js
+++ workspaces/cogito-mobile/home/CurrentIdentity.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import { Text } from 'react-native'
 import { Navigation } from 'react-native-navigation'
+import { connect } from 'react-redux'
 import { CreateIdentity } from '../identity-manager/CreateIdentity'
 
-export const CurrentIdentity = () => (
-  <Text onPress={() => openCreateIdentityScreen()}>Who am I?</Text>
+export const CurrentIdentityComponent = props => (
+  <Text onPress={() => openCreateIdentityScreen()}>
+    {props.identityName || 'Who am I?'}
+  </Text>
+)
+
+const mapStateToProps = state => ({
+  identityName:
+    state.identity.identities.length > 0 ? state.identity.identities[0] : null
+})
+export const CurrentIdentity = connect(mapStateToProps)(
+  CurrentIdentityComponent
 )
 
 function openCreateIdentityScreen () {
