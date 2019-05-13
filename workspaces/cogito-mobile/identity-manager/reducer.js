74f3ca61ca7498bf547daeace3a4792582d4e61b
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-05-13 08:54:32 +0200

    [cogito-mobile] Uses named parameter for action

--- workspaces/cogito-mobile/identity-manager/reducer.js
+++ workspaces/cogito-mobile/identity-manager/reducer.js
@@ -4,6 +4,6 @@ import { add } from './actions'
 export const initialState = { identities: [] }
 export const identityReducer = createReducer(initialState, {
   [add]: (state, action) => {
-    state.identities.push(action.payload)
+    state.identities.push(action.payload.name)
   }
 })
