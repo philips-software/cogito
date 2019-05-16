982cae6a6cb1073e55517a80af6864e86d68b84e
Nicole Bekkers <hackybekkers@hacklor.com>
2019-05-16 13:22:00 +0200

    [cogito-mobile] Make identity name not pressable

--- workspaces/cogito-mobile/home/CurrentIdentity.test.js
+++ workspaces/cogito-mobile/home/CurrentIdentity.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render, fireEvent } from 'react-native-testing-library'
-import { CurrentIdentityComponent as CurrentIdentity } from './CurrentIdentity'
 import { Navigation } from 'react-native-navigation'
+import { CurrentIdentityComponent as CurrentIdentity } from './CurrentIdentity'
 import { CreateIdentity } from '../identity-manager/CreateIdentity'
 
 describe('CurrentIdentity', () => {
@@ -14,6 +14,7 @@ describe('CurrentIdentity', () => {
 
   it('shows the identity name in props if available', () => {
     const { queryByText } = render(<CurrentIdentity identityName='superman' />)
+
     expect(queryByText(welcomeText)).toBeNull()
     expect(queryByText('superman')).not.toBeNull()
   })
@@ -24,5 +25,13 @@ describe('CurrentIdentity', () => {
 
     const layout = Navigation.showModal.mock.calls[0][0]
     expect(layout).toEqual(CreateIdentity.modalPresentationLayout)
+  })
+
+  it('does not navigate to create identity screen when identity is present', () => {
+    const identityName = 'my identity'
+    const { getByText } = render(<CurrentIdentity identityName={identityName} />)
+
+    const element = getByText(identityName)
+    expect(() => fireEvent.press(element)).toThrow('No handler function found for event: press')
   })
 })
