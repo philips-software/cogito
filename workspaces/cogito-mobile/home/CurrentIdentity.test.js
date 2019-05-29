11968c3647c5f594c9b08b7b0c5670725f689da1
Nicole Bekkers <hackybekkers@hacklor.com>
2019-05-29 13:50:01 +0200

    [cogito-mobile] Rename describe and test

--- workspaces/cogito-mobile/home/CurrentIdentity.test.js
+++ workspaces/cogito-mobile/home/CurrentIdentity.test.js
@@ -1,26 +1,26 @@
 import React from 'react'
 import { render, fireEvent } from 'react-native-testing-library'
 import { Navigation } from 'react-native-navigation'
-import { CurrentIdentityComponent as CurrentIdentity } from './CurrentIdentity'
+import { CurrentIdentityComponent } from './CurrentIdentity'
 import { CreateIdentity } from '../identity-manager/CreateIdentity'
 
-describe('CurrentIdentity', () => {
+describe('CurrentIdentityComponent', () => {
   const welcomeText = 'Who am I?'
 
   it('shows the welcome text', () => {
-    const { queryByText } = render(<CurrentIdentity />)
+    const { queryByText } = render(<CurrentIdentityComponent />)
     expect(queryByText(welcomeText)).not.toBeNull()
   })
 
   it('shows the identity name in props if available', () => {
-    const { queryByText } = render(<CurrentIdentity identityName='superman' />)
+    const { queryByText } = render(<CurrentIdentityComponent identityName='superman' />)
 
     expect(queryByText(welcomeText)).toBeNull()
     expect(queryByText('superman')).not.toBeNull()
   })
 
   it('navigates to the create identity screen', () => {
-    const { getByText } = render(<CurrentIdentity />)
+    const { getByText } = render(<CurrentIdentityComponent />)
     fireEvent.press(getByText(welcomeText))
 
     const layout = Navigation.showModal.mock.calls[0][0]
@@ -29,7 +29,7 @@ describe('CurrentIdentity', () => {
 
   it('does not navigate to create identity screen when identity is present', () => {
     const identityName = 'my identity'
-    const { getByText } = render(<CurrentIdentity identityName={identityName} />)
+    const { getByText } = render(<CurrentIdentityComponent identityName={identityName} />)
 
     const element = getByText(identityName)
     expect(() => fireEvent.press(element)).toThrow('No handler function found for event: press')
