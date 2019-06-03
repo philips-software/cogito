692d470867808d601f45b70e5c6a447e74106ce9
Marcin Czenko <marcin@everydayproductive.com>
2019-06-03 12:22:05 +0200

    updates compute-churn script to only run on the master branch

--- tools/compute-churn.sh
+++ tools/compute-churn.sh
@@ -4,7 +4,7 @@ LAST_WEEK_SUNDAY=`date -v-monday -v-8d +%Y-%m-%d`
 LAST_WEEK_MONDAY=`date -v-monday -v-7d +%Y-%m-%d`
 LAST_SUNDAY=`date -v-monday -v-1d +%Y-%m-%d`
 
-git log --all --numstat --date=short --pretty=format:'--%h--%ad--%aN' --no-renames --after=$LAST_WEEK_SUNDAY --until=$LAST_SUNDAY > codemaat.txt
+git log --branches=[m]aster --numstat --date=short --pretty=format:'--%h--%ad--%aN' --no-renames --after=$LAST_WEEK_SUNDAY --until=$LAST_SUNDAY > codemaat.txt
 
 if [ -s codemaat.txt ]
 then
