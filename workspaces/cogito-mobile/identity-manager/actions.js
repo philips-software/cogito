00a9741422af4bda0ab224173c2972ed72d59074
Mark Spanbroek <mark@spanbroek.net>
2019-06-11 17:08:48 +0200

    📱 Add actions for 'pending' and 'finished' when adding an identity

--- workspaces/cogito-mobile/identity-manager/actions.js
+++ workspaces/cogito-mobile/identity-manager/actions.js
@@ -2,8 +2,12 @@ import { createAction } from 'redux-starter-kit'
 import { Wallet } from 'ethers'
 
 export const add = name => async dispatch => {
+  dispatch(addIsPending())
   const wallet = await Wallet.createRandom()
   dispatch(store({ name, wallet }))
+  dispatch(addIsFinished())
 }
 
+export const addIsPending = createAction('identity/add/pending')
+export const addIsFinished = createAction('identity/add/finished')
 export const store = createAction('identity/store')
--- workspaces/cogito-mobile/identity-manager/actions.test.js
+++ workspaces/cogito-mobile/identity-manager/actions.test.js
@@ -1,7 +1,8 @@
-import { add, store } from './actions'
+import { add, addIsPending, addIsFinished, store } from './actions'
 import { Wallet } from 'ethers'
 
 describe('when adding a new identity', () => {
+  const name = 'some name'
   const wallet = { some: 'wallet' }
 
   beforeEach(() => {
@@ -10,12 +11,21 @@ describe('when adding a new identity', () => {
       .mockResolvedValue(wallet)
   })
 
-  it('stores a new cryptographic identity', async () => {
-    const name = 'some name'
+  it('signals that it is adding an identity', () => {
     const dispatch = jest.fn()
+    add(name)(dispatch)
+    expect(dispatch).toBeCalledWith(addIsPending())
+  })
 
+  it('stores a new cryptographic identity', async () => {
+    const dispatch = jest.fn()
     await add(name)(dispatch)
-
     expect(dispatch).toBeCalledWith(store({ name, wallet }))
+  })
+
+  it('signals that it finished adding an identity', async () => {
+    const dispatch = jest.fn()
+    await add(name)(dispatch)
+    expect(dispatch).toBeCalledWith(addIsFinished())
   })
 })
