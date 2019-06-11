546ca377af5cd549d9dea7d89d8ba4f96fae40e8
Nicole Bekkers <hackybekkers@hacklor.com>
2019-06-11 10:31:21 +0200

    📱 Fix linter warning on duplicate exports

--- workspaces/cogito-mobile/__mocks__/react-native-elements/index.js
+++ workspaces/cogito-mobile/__mocks__/react-native-elements/index.js
@@ -1,3 +1,7 @@
-export * from 'react-native-elements'
-export { Button } from './Button'
-export { Input } from './Input'
+// Use old-style module.exports instead of new-style 'export', otherwise the
+// linter will error on exporting some elements twice.
+module.exports = {
+  ...require('react-native-elements'),
+  ...require('./Button'),
+  ...require('./Input')
+}
