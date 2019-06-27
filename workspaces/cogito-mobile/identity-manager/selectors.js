fccadfaa8ec4f466f7aaa30c008ba4a13274c1b9
Mark Spanbroek <mark@spanbroek.net>
2019-06-27 08:21:17 +0200

    Ensure that isCreatingIdentity returns a boolean

--- workspaces/cogito-mobile/identity-manager/selectors.js
+++ workspaces/cogito-mobile/identity-manager/selectors.js
@@ -1,3 +1,3 @@
 export const getIdentityName = state => state.identity.name
-export const isCreatingIdentity = state => state.identity.creating
+export const isCreatingIdentity = state => !!state.identity.creating
 export const wasIdentityCreated = state => !!state.identity.wallet
