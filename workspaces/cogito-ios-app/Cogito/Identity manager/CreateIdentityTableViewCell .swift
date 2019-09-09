517c99fdf6937709d7a9406fbaf87887f66b0042
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-09-09 18:10:49 +0200

    Adds activity indicator and tweaks margins

--- workspaces/cogito-ios-app/Cogito/Identity manager/CreateIdentityTableViewCell .swift
+++ workspaces/cogito-ios-app/Cogito/Identity manager/CreateIdentityTableViewCell .swift
@@ -4,6 +4,7 @@ class CreateIdentityTableViewCell: UITableViewCell {
     @IBOutlet weak var nameEntryField: NameEntryField!
     @IBOutlet weak var createButton: Button!
     @IBOutlet weak var createButtonTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var activityView: UIActivityIndicatorView!
 
     override func awakeFromNib() {
         createButtonTopConstraint.isActive = false
