49db38aaf9b3efc731c5e846e9b5fcd14ecfd715
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-09-10 08:45:25 +0200

    Replaces buttons in cells with labels

--- workspaces/cogito-ios-app/Cogito/Identity manager/CreateIdentityTableViewCell .swift
+++ workspaces/cogito-ios-app/Cogito/Identity manager/CreateIdentityTableViewCell .swift
@@ -5,8 +5,8 @@ class CreateIdentityTableViewCell: UITableViewCell {
     @IBOutlet weak var createButton: Button!
     @IBOutlet weak var createButtonTopConstraint: NSLayoutConstraint!
     @IBOutlet weak var activityView: UIActivityIndicatorView!
-    @IBOutlet weak var tapToCreateButton: UIButton!
-    
+    @IBOutlet weak var tapToCreateLabel: UILabel!
+
     override func awakeFromNib() {
         createButtonTopConstraint.isActive = false
     }
