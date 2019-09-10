bd6d9ae07666edbabfd4a7f0a0cb9f1409bbc0d2
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-09-10 08:14:44 +0200

    Adds outlet for "tap to create [...]" button

--- workspaces/cogito-ios-app/Cogito/Identity manager/CreateIdentityTableViewCell .swift
+++ workspaces/cogito-ios-app/Cogito/Identity manager/CreateIdentityTableViewCell .swift
@@ -5,7 +5,8 @@ class CreateIdentityTableViewCell: UITableViewCell {
     @IBOutlet weak var createButton: Button!
     @IBOutlet weak var createButtonTopConstraint: NSLayoutConstraint!
     @IBOutlet weak var activityView: UIActivityIndicatorView!
-
+    @IBOutlet weak var tapToCreateButton: UIButton!
+    
     override func awakeFromNib() {
         createButtonTopConstraint.isActive = false
     }
