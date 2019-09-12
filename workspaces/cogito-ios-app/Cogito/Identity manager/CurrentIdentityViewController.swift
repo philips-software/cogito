4f60e919ace8fa33cc4370d697aa7bab588e58b6
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-09-12 20:04:18 +0200

    Adds QR code reader to current identity screen

--- workspaces/cogito-ios-app/Cogito/Identity manager/CurrentIdentityViewController.swift
+++ workspaces/cogito-ios-app/Cogito/Identity manager/CurrentIdentityViewController.swift
@@ -3,6 +3,8 @@ import ReSwift
 import RxSwift
 import ReRxSwift
 import RichString
+import QRCodeReader
+import FontAwesome_swift
 
 private let typewriter = UIFont(name: "American Typewriter", size: 17)!
 private let boldTypewriter: UIFont = {
@@ -10,7 +12,7 @@ private let boldTypewriter: UIFont = {
     return UIFont(descriptor: bold, size: 17)
 }()
 
-class CurrentIdentityViewController: UIViewController, Connectable {
+class CurrentIdentityViewController: UIViewController, QRCodeReaderViewControllerDelegate, Connectable {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var cameraButton: UIButton!
     @IBOutlet weak var previewContainer: UIView!
@@ -38,6 +40,95 @@ class CurrentIdentityViewController: UIViewController, Connectable {
         }
     }
 
+    // MARK: - QR Code Reading
+
+    lazy var readerVC: QRCodeReaderViewController = {
+        let builder = QRCodeReaderViewControllerBuilder {
+            $0.reader = QRCodeReader(metadataObjectTypes: [.qr], captureDevicePosition: .back)
+            $0.showCancelButton = false
+            $0.showOverlayView = false
+            $0.showSwitchCameraButton = false
+            $0.startScanningAtLoad = false
+        }
+
+        let readerVC = QRCodeReaderViewController(builder: builder)
+        addChild(readerVC)
+        previewContainer.insertSubview(readerVC.view, at: 0)
+        let frame = CGRect(x: 0, y: 0,
+                           width: previewContainer.frame.size.width,
+                           height: previewContainer.frame.size.height)
+        readerVC.view.frame = frame
+        let cameraIconLabel = UILabel(frame: frame)
+        cameraIconLabel.textAlignment = .center
+        let icon = String
+            .fontAwesomeIcon(name: .video)
+            .font(Font.fontAwesome(ofSize: frame.size.width/2, style: .solid))
+        cameraIconLabel.attributedText = icon
+        cameraIconLabel.textColor = .white
+        readerVC.view.insertSubview(cameraIconLabel, at: 0)
+        return readerVC
+    }()
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        readerVC.delegate = self
+    }
+
+    @IBAction func scanButtonDown() {
+        if props.selectedFacet != nil {
+            startScanning()
+        }
+    }
+
+    @IBAction func scanButtonUpInside() {
+        if props.selectedFacet != nil {
+            stopScanning()
+        }
+    }
+
+    @IBAction func scanButtonUpOutside() {
+        if props.selectedFacet != nil {
+            stopScanning()
+        }
+    }
+
+    private func startScanning() {
+        readerVC.startScanning()
+        UIView.animate(withDuration: 0.1,
+                       delay: 0,
+                       usingSpringWithDamping: 1,
+                       initialSpringVelocity: 0,
+                       options: .beginFromCurrentState,
+                       animations: {
+                        self.leftShutter.frame.origin.x = -self.leftShutter.frame.size.width
+                        self.rightShutter.frame.origin.x = self.rightShutter.superview!.frame.size.width
+        })
+    }
+
+    private func stopScanning() {
+        readerVC.stopScanning()
+        UIView.animate(withDuration: 1,
+                       delay: 0,
+                       usingSpringWithDamping: 1,
+                       initialSpringVelocity: 0,
+                       options: .beginFromCurrentState,
+                       animations: {
+                        self.leftShutter.frame.origin.x = 0
+                        self.rightShutter.frame.origin.x = self.rightShutter.superview!.frame.size.width / 2
+        })
+    }
+
+    func reader(_ reader: QRCodeReaderViewController, didScanResult result: QRCodeReaderResult) {
+        actions.handleScannedQRCode(result.value)
+        stopScanning()
+    }
+
+    func readerDidCancel(_ reader: QRCodeReaderViewController) {
+        stopScanning()
+    }
+
+    // MARK: - Connection
+
     let connection = Connection(store: appStore,
                                 mapStateToProps: mapStateToProps,
                                 mapDispatchToActions: mapDispatchToActions)
@@ -45,7 +136,9 @@ class CurrentIdentityViewController: UIViewController, Connectable {
         let selectedFacet: Identity?
     }
 
-    struct Actions {}
+    struct Actions {
+        let handleScannedQRCode: (String) -> Void
+    }
 }
 
 private func mapStateToProps(state: AppState) -> CurrentIdentityViewController.Props {
@@ -56,5 +149,11 @@ private func mapStateToProps(state: AppState) -> CurrentIdentityViewController.P
 
 private func mapDispatchToActions(dispatch: @escaping DispatchFunction)
     -> CurrentIdentityViewController.Actions {
-        return CurrentIdentityViewController.Actions()
+        return CurrentIdentityViewController.Actions(
+            handleScannedQRCode: { qrCodeString in
+                if let url = URL(string: qrCodeString) {
+                    dispatch(URLActions.HandleIncomingURL(url: url))
+                }
+            }
+        )
 }
