65e34bb647065682419447bb97413e314fb927af
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-09-13 14:13:51 +0200

    Preserves colors of identity labels in main view

--- workspaces/cogito-ios-app/Cogito/Identity manager/FacetTableViewCell.swift
+++ workspaces/cogito-ios-app/Cogito/Identity manager/FacetTableViewCell.swift
@@ -1,13 +1,21 @@
 import UIKit
 
 class FacetTableViewCell: UITableViewCell {
+    var enabledFacetText: NSAttributedString?
     var facet: Identity?
     var enabled: Bool = true {
         didSet {
-            facetLabel.textColor = enabled ? .black : .disabled
             iamLabel.textColor = enabled ? .black : .disabled
             tapToEnterLabel.textColor = enabled ? .tinted : .disabled
             selectionStyle = enabled ? .default : .none
+
+            if let previousText = enabledFacetText, enabled {
+                facetLabel.attributedText = previousText
+                enabledFacetText = nil
+            } else if !enabled && enabledFacetText == nil {
+                enabledFacetText = facetLabel.attributedText
+                facetLabel.attributedText = facetLabel.attributedText?.color(.disabled)
+            }
         }
     }
     @IBOutlet weak var facetLabel: UILabel!
