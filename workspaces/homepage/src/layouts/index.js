4deb268a442e687dd1f526899b90c4b9b26e9b41
Marcin Czenko <marcin@everydayproductive.com>
2020-03-21 12:29:15 +0100

    Updates homepage to use new confluenza theme

--- workspaces/homepage/src/layouts/index.js
+++ workspaces/homepage/src/layouts/index.js
@@ -1,16 +1,3 @@
-import { React } from 'react'
-import { DocumentationLayout } from './documentation'
-
-const Layout = ({ location, children }) => {
-  if (location.pathname === '' || location.pathname === '/') {
-    return (
-      <div>{children}</div>
-    )
-  } else {
-    return (
-      <DocumentationLayout location={location}>{children}</DocumentationLayout>
-    )
-  }
-}
+import Layout from '@confluenza/gatsby-theme-confluenza/src/layouts'
 
 export default Layout
