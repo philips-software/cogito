23a016485946726c7fb99f2d4d08105f78016461
Stefan van den Oord <stefan.van.den.oord@philips.com>
2020-06-08 14:28:21 +0200

    Work-around to override mkdirp version, to solve minimist security issue

--- workspaces/cogito-mobile/CONFLUENZA.md
+++
@@ -1,6 +0,0 @@
----
-path: /components/cogito-mobile
-title: Cogito Mobile
-tag: component
-content: Readme.md
----
--- workspaces/cogito-mobile/ios/Cogito-Bridging-Header.h
+++
@@ -1,3 +0,0 @@
-#import <React/RCTBundleURLProvider.h>
-#import <React/RCTRootView.h>
-#import <ReactNativeNavigation/ReactNativeNavigation.h>
